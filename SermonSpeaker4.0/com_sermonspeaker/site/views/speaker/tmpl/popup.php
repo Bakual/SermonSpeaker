7c7aa2425e44a6009d5cdc1d0c955390d03f32af
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2010-12-16 14:47:06 +0000

    SS4.0: Speaker popup layout works
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@230 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/com_sermonspeaker/site/views/speaker/tmpl/popup.php
+++ SermonSpeaker4.0/com_sermonspeaker/site/views/speaker/tmpl/popup.php
@@ -1,20 +1,20 @@
 <?php
 defined( '_JEXEC' ) or die( 'Restricted access' );
-if ($this->row->pic == "") { $this->row->pic = JURI::root().'components/com_sermonspeaker/images/nopict.jpg'; }
+if ($this->speaker->pic == "") { $this->speaker->pic = JURI::root().'components/com_sermonspeaker/images/nopict.jpg'; }
 ?>
 <!-- Begin Data -->
-<div class="ss-speaker-container" style="padding:20px;">
-<a href="<?php echo JRoute::_('index.php?view=speaker&id='.$this->row->id); ?>">
-	<img class="speaker" style="float:right;" src="<?php echo $this->row->pic; ?>" title="<?php echo $this->row->name; ?>" alt="<?php echo $this->row->name; ?>" />
+<div id="ss-speaker-container" style="padding:20px;">
+<a href="<?php echo JRoute::_('index.php?view=speaker&id='.$this->speaker->slug); ?>" target="_parent">
+	<img class="speaker" style="float:right;" src="<?php echo $this->speaker->pic; ?>" title="<?php echo $this->speaker->name; ?>" alt="<?php echo $this->speaker->name; ?>" />
 </a>
-<h1 class="componentheading"><?php echo $this->row->name ?></h1>
-<?php if ($this->row->bio || ($this->row->intro && $this->params->get('speaker_intro'))) { ?>
+<h1 class="componentheading"><?php echo $this->speaker->name ?></h1>
+<?php if ($this->speaker->bio || ($this->speaker->intro && $this->params->get('speaker_intro'))) { ?>
 	<h3 class="contentheading"><?php echo JText::_('COM_SERMONSPEAKER_SPEAKER_BIO'); ?></h3>
 	<?php
-	echo $this->row->intro;
-	echo $this->row->bio; ?>
+	echo $this->speaker->intro;
+	echo $this->speaker->bio; ?>
 <?php }
-if ($this->row->website && $this->row->website != "http://") { ?>
-	<a href="<?php echo $this->row->website; ?>" target="_blank" title="<?php echo JText::_('COM_SERMONSPEAKER_SPEAKER_WEBLINK_HOOVER'); ?>"><?php echo JText::sprintf('COM_SERMONSPEAKER_SPEAKER_WEBLINK', $this->row->name); ?></a>
+if ($this->speaker->website && $this->speaker->website != 'http://') { ?>
+	<a href="<?php echo $this->speaker->website; ?>" target="_blank" title="<?php echo JText::_('COM_SERMONSPEAKER_SPEAKER_WEBLINK_HOOVER'); ?>"><?php echo JText::sprintf('COM_SERMONSPEAKER_SPEAKER_WEBLINK', $this->speaker->name); ?></a>
 <?php } ?>
 </div>
