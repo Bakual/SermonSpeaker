64f26df09d943697f2a0720fdf58c9e5f9d76b7a
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2010-12-21 14:32:46 +0000

    SS4.0: Added alias to the example sermons
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@240 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/com_sermonspeaker/file.script.php
+++ SermonSpeaker4.0/com_sermonspeaker/file.script.php
@@ -18,22 +18,22 @@ class Com_SermonspeakerInstallerScript {
 		if ($demo == 1) {
 			//Speaker
 			$query = "INSERT INTO `#__sermon_speakers` "
-				."(`id`,`name`,`website`,`intro`,`bio`,`pic`,`state`,`ordering`,`created_by`,`created`) VALUES"
-				."(1,'Billy Sunday','http://joomlacode.org/gf/project/sermon_speaker/','Billy Sunday died in Chicago, November 6, 1935; services were held in the Moody Memorial Church with 4,400 present. Take 15 minutes each day to listen to God talking to you; take 15 minutes each day to talk to God; take 15 minutes each day to talk to others about God.','This young convert was deeply impressed and determined to make these the rules of his life. From that day onward throughout his life he made it a rule to spend the first moments of his day alone with God and God\'s Word. Before he read a letter, looked at a paper or even read a telegram, he went first to the Bible, that the first impression of the day might be what he got directly from God.','components/com_sermonspeaker/media/default_speaker.jpg',1,1,62,'2006-03-28')";
+				."(`id`,`name`,`alias`,`website`,`intro`,`bio`,`pic`,`state`,`ordering`,`created_by`,`created`) VALUES"
+				."(1,'Billy Sunday','billy-sunday','http://joomlacode.org/gf/project/sermon_speaker/','Billy Sunday died in Chicago, November 6, 1935; services were held in the Moody Memorial Church with 4,400 present. Take 15 minutes each day to listen to God talking to you; take 15 minutes each day to talk to God; take 15 minutes each day to talk to others about God.','This young convert was deeply impressed and determined to make these the rules of his life. From that day onward throughout his life he made it a rule to spend the first moments of his day alone with God and God\'s Word. Before he read a letter, looked at a paper or even read a telegram, he went first to the Bible, that the first impression of the day might be what he got directly from God.','components/com_sermonspeaker/media/default_speaker.jpg',1,1,62,'2006-03-28')";
 			$database->setQuery($query);
 			$database->Query();
 	  
 			//Series
 			$query = "INSERT INTO `#__sermon_series` "
-				."(`id`, `series_title`, `series_description`, `state`, `ordering`, `created_by`, `created`) VALUES"
-				."(1, 'General Topics', 'Topics of general interest.', 1, 1, 62, '2006-03-28')";
+				."(`id`,`series_title`,`alias`,`series_description`,`state`,`ordering`,`created_by`,`created`) VALUES"
+				."(1,'General Topics','general-topics','Topics of general interest.',1,1,62,'2006-03-28')";
 			$database->setQuery($query);
 			$database->Query();
 		
 			//Sermon
 			$query = "INSERT INTO `#__sermon_sermons` "
-			."(`id`, `speaker_id`, `series_id`, `sermon_path`, `sermon_title`, `sermon_number`, `sermon_date`, `sermon_time`, `notes`, `ordering`, `created_by`, `created`, `state`) VALUES"
-			."(1, 1, 1, '/components/com_sermonspeaker/media/default_sermon.mp3', 'The Sin of Booze', '1', '2006-03-28', '00:00:05', 'Borrowed from sermonaudio.com', 1, 62, '2006-03-28', 1)";
+			."(`id`,`speaker_id`,`series_id`,`sermon_path`,`sermon_title`,`alias`,`sermon_number`,`sermon_date`,`sermon_time`,`notes`,`ordering`,`created_by`,`created`,`state`) VALUES"
+			."(1,1,1,'/components/com_sermonspeaker/media/default_sermon.mp3','The Sin of Booze','the-sin-of-booze','1','2006-03-28','00:00:05','Borrowed from sermonaudio.com',1,62,'2006-03-28',1)";
 			$database->setQuery($query);
 			$database->Query();
 		}
