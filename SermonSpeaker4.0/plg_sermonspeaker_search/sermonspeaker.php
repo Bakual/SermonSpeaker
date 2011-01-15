47813de99c9af2baa4cda716c9ae724995699007
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2011-01-15 11:05:10 +0000

    SS4.0: Rewrote search plugin, doesn't work yet with Root category as I believe there is a bug in Joomla with this.
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@307 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/plg_sermonspeaker_search/sermonspeaker.php
+++ SermonSpeaker4.0/plg_sermonspeaker_search/sermonspeaker.php
@@ -1,263 +1,284 @@
 <?php
 /**
- * @version 0.9 $Id: sermonspeaker.php 825 2008-12-03 16:12:24Z sermonspeaker $
- * @package Joomla
- * @subpackage SermonSpeaker
- * @copyright (C) 2010 Phil Boucher
- * @license GNU/GPL, see LICENCE.php
- * SermonSpeaker is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License 2
- * as published by the Free Software Foundation.
-
- * SermonSpeaker is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with SermonSpeaker; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * SermonSpeaker search plugin
+ * based on Weblinks Search plugin
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
  */
-
+ 
+ 
 // no direct access
-defined('_JEXEC') or die('Restricted access');
+defined('_JEXEC') or die;
 
-$app = JFactory::getApplication();
-$app->registerEvent('onSearch', 'plgSearchSermonspeaker');
-$app->registerEvent('onSearchAreas', 'plgSearchSermonspeakerAreas');
+jimport('joomla.plugin.plugin');
 
-//Load the Plugin language file out of the administration
-JPlugin::loadLanguage('plg_search_sermonspeaker', JPATH_ADMINISTRATOR);
+require_once JPATH_SITE.'/components/com_sermonspeaker/helpers/route.php';
 
 /**
- * @return array An array of search areas
- */
-function &plgSearchSermonspeakerAreas() {
-	static $areas = array(
-		'spsermons' => 'PLG_SEARCH_SERMONSPEAKER_SERMONS',
-		'spseries' => 'PLG_SEARCH_SERMONSPEAKER_SERIES',
-	);
-	return $areas;
-}
-
-/**
- * Categories Search method
+ * SermonSpeaker Search plugin
  *
- * The sql must return the following fields that are
- * used in a common display routine: href, title, section, created, text,
- * browsernav
- * @param string Target search string
- * @param string mathcing option, exact|any|all
- * @param string ordering option, newest|oldest|popular|alpha|category
- * @param mixed An array if restricted to areas, null if search all
+ * @package		Joomla
+ * @subpackage	Search
+ * @since		1.6
  */
-function plgSearchSermonspeaker($text, $phrase='', $ordering='', $areas=null)
+class plgSearchSermonspeaker extends JPlugin
 {
-	$db		=& JFactory::getDBO();
-	$user	=& JFactory::getUser();
-	
-	if (is_array( $areas )) {
-		if (!array_intersect($areas, array_keys(plgSearchSermonspeakerAreas()))) {
+	/**
+	 * Constructor
+	 *
+	 * @access      protected
+	 * @param       object  $subject The object to observe
+	 * @param       array   $config  An array that holds the plugin configuration
+	 * @since       1.5
+	 */
+	public function __construct(& $subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	/**
+	 * @return array An array of search areas
+	 */
+	function onContentSearchAreas() {
+		static $areas = array(
+			'spsermons' => 'PLG_SEARCH_SERMONSPEAKER_SERMONS',
+			'spseries' => 'PLG_SEARCH_SERMONSPEAKER_SERIES',
+			);
+			return $areas;
+	}
+
+	/**
+	 * SermonSpeaker Search method
+	 *
+	 * The sql must return the following fields that are used in a common display
+	 * routine: href, title, section, created, text, browsernav
+	 * @param string Target search string
+	 * @param string mathcing option, exact|any|all
+	 * @param string ordering option, newest|oldest|popular|alpha|category
+	 * @param mixed An array if the search it to be restricted to areas, null if search all
+	 */
+	function onContentSearch($text, $phrase='', $ordering='', $areas=null)
+	{
+		$db		= JFactory::getDbo();
+		$app	= JFactory::getApplication();
+		$user	= JFactory::getUser();
+		$groups	= implode(',', $user->getAuthorisedViewLevels());
+
+		$searchText = $text;
+
+		if (is_array($areas)) {
+			if (!array_intersect($areas, array_keys($this->onContentSearchAreas()))) {
+				return array();
+			}
+		} else {
+			$areas = array_keys($this->onContentSearchAreas());
+		}
+
+		$sContent		= $this->params->get('search_content',		1);
+		$sArchived		= $this->params->get('search_archived',		1);
+		$limit			= $this->params->def('search_limit',		50);
+		$state = array();
+		if ($sContent) {
+			$state[]=1;
+		}
+		if ($sArchived) {
+			$state[]=2;
+		}
+
+		$text = trim($text);
+		if ($text == '') {
 			return array();
 		}
-	} else {
-		$areas = array_keys( plgSearchSermonspeakerAreas() );
-	}
+		$section	= JText::_('PLG_SEARCH_SERMONSPEAKER_SERMONS');
 
-	// load plugin params info
-	$plugin =& JPluginHelper::getPlugin('search', 'sermonspeaker');
-	$pluginParams = new JParameter($plugin->params);
+		$rows = array();
+		if(in_array('spsermons', $areas)) {
 
-	$limit = $pluginParams->def('search_limit', 50);
+			$wheres	= array();
+			switch ($phrase)
+			{
+				case 'exact':
+					$text		= $db->Quote('%'.$db->getEscaped($text, true).'%', false);
+					$wheres2	= array();
+					$wheres2[]	= 'a.sermon_title LIKE '.$text;
+					$wheres2[]	= 'a.sermon_scripture LIKE '.$text;
+					$wheres2[]	= 'a.notes LIKE '.$text;
+					$where		= '(' . implode(') OR (', $wheres2) . ')';
+					break;
 
-	$text = trim($text);
-	if ( $text == '' ) {
-		return array();
-	}
+				case 'all':
+				case 'any':
+				default:
+					$words	= explode(' ', $text);
+					$wheres = array();
+					foreach ($words as $word)
+					{
+						$word		= $db->Quote('%'.$db->getEscaped($word, true).'%', false);
+						$wheres2	= array();
+						$wheres2[]	= 'a.sermon_title LIKE '.$word;
+						$wheres2[]	= 'a.sermon_scripture LIKE '.$word;
+						$wheres2[]	= 'a.notes LIKE '.$word;
+						$wheres[]	= implode(' OR ', $wheres2);
+					}
+					$where	= '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
+					break;
+			}
 
-	$searchSermonSpeaker = $db->Quote(JText::_('PLG_SEARCH_SERMONSPEAKER_SERMONS'));
+			switch ($ordering)
+			{
+				case 'oldest':
+					$order = 'a.created ASC';
+					break;
 
-	$rows = array();
+				case 'popular':
+					$order = 'a.hits DESC';
+					break;
 
-	if(in_array('spsermons', $areas)) {
+				case 'alpha':
+					$order = 'a.sermon_title ASC';
+					break;
 
-		switch ($phrase) {
-			case 'exact':
-				$search 	= $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
-				$wheres2 	= array();
-				$wheres2[] 	= 'LOWER(a.sermon_title) LIKE '.$search;
-				$wheres2[] 	= 'LOWER(a.sermon_scripture) LIKE '.$search;
-				$wheres2[] 	= 'LOWER(a.notes) LIKE '.$search;
-				$where 		= '(' . implode( ') OR (', $wheres2 ) . ')';
-				break;
+				case 'category':
+					$order = 'c.title ASC, a.sermon_title ASC';
+					$morder = 'a.sermon_title ASC';
+					break;
 
-			case 'all':
-			case 'any':
-			default:
-				$words = explode( ' ', $text );
-				$wheres = array();
-				foreach ($words as $word) {
-					$word 		= $db->Quote( '%'.$db->getEscaped( $word, true ).'%', false );
-					$wheres2 	= array();
-					$wheres2[] 	= 'LOWER(a.sermon_title) LIKE '.$word;
-					$wheres2[] 	= 'LOWER(a.sermon_scripture) LIKE '.$word;
-					$wheres2[] 	= 'LOWER(a.notes) LIKE '.$word;
-					$wheres[] 	= implode( ' OR ', $wheres2 );
+				case 'newest':
+				default:
+					$order = 'a.created DESC';
+			}
+
+			if (!empty($state)) {
+				$query	= $db->getQuery(true);
+				$query->select('a.sermon_title AS title, a.notes AS text, a.created AS created, '
+							.'CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug, '
+							.'CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as catslug, '
+							.'CONCAT_WS(" / ", '.$db->Quote($section).', c.title) AS section, "1" AS browsernav');
+				$query->from('#__sermon_sermons AS a');
+				$query->innerJoin('#__categories AS c ON c.id = a.catid');
+				$query->where('('.$where.')' . ' AND a.state in ('.implode(',',$state).') AND  c.published=1 AND  c.access IN ('.$groups.')');
+				$query->order($order);
+
+				// Filter by language
+				if ($app->isSite() && $app->getLanguageFilter()) {
+					$tag = JFactory::getLanguage()->getTag();
+					$query->where('c.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
 				}
-				$where = '(' . implode( ($phrase == 'all' ? ') AND (' : ') OR ('), $wheres ) . ')';
-				break;
-		}
-		
-		switch ($ordering) {
-			case 'oldest':
-				$order = 'a.sermon_date ASC';
-				break;
 
-			case 'alpha':
-				$order = 'a.sermon_title ASC';
-				break;
+				$db->setQuery($query, 0, $limit);
+				$list = $db->loadObjectList();
 
-			case 'category':
-				$order = 'c.series_name ASC, a.sermon_title ASC';
-				break;
-      
-			case 'popular':
-				$order = 'a.hits DESC, a.sermon_title ASC';
-				break;
-					
-			case 'newest':
-			default:
-				$order = 'a.sermon_date DESC';
-		}
-
-		$query = 'SELECT a.sermon_title AS title,'
-		. ' a.notes AS text,'
-		. ' a.sermon_date AS created,'
-		. ' "2" AS browsernav,'
-		. " CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(':', a.id, a.alias) ELSE a.id END as slug, \n"
-		. ' CONCAT_WS( " / ", '. $searchSermonSpeaker .', c.series_title, a.sermon_title ) AS section'
-		. ' FROM #__sermon_sermons AS a'
-		. ' INNER JOIN #__sermon_series AS c ON c.id = a.series_id'
-		. ' WHERE ( '.$where.' )'
-		. ' AND a.published = 1'
-		. ' AND c.published = 1'
-		. ' ORDER BY '. $order
-		;
-		$db->setQuery( $query, 0, $limit );
-		$list = $db->loadObjectList();
-
-		// set a menu item
-		$menu = &JSite::getMenu();
-		$menuitems = $menu->getItems('link', 'index.php?option=com_sermonspeaker&view=sermons');
-		if ($menuitems == '') {
-			$menuitems = $menu->getItems('component', 'com_sermonspeaker');
-		}
-		$ss_itemid = $menuitems[0]->id;
-
-		foreach($list as $key => $row) {
-			$list[$key]->href = 'index.php?option=com_sermonspeaker&view=sermon&id='.$row->slug.'&Itemid='.$ss_itemid;
-		}
-
-		$rows[] = $list;
-	}
-
-	if(in_array('spseries', $areas)) {
-
-		switch ($phrase) {
-			case 'exact':
-				$search 		= $db->Quote( '%'.$db->getEscaped( $text, true ).'%', false );
-				$wheres2 	= array();
-				$wheres2[] 	= 'LOWER(series_title) LIKE '.$search;
-				$wheres2[] 	= 'LOWER(series_description) LIKE '.$search;
-				$where 		= '(' . implode( ') OR (', $wheres2 ) . ')';
-				break;
-
-			case 'all':
-			case 'any':
-			default:
-				$words = explode( ' ', $text );
-				$wheres = array();
-				foreach ($words as $word) {
-					$word 		= $db->Quote( '%'.$db->getEscaped( $word, true ).'%', false );
-					$wheres2 	= array();
-					$wheres2[] 	= 'LOWER(series_title) LIKE '.$word;
-					$wheres2[] 	= 'LOWER(series_description) LIKE '.$word;
-					$wheres[] 	= implode( ' OR ', $wheres2 );
+				if (isset($list))
+				{
+					foreach($list as $key => $item)
+					{
+						$list[$key]->href = SermonspeakerHelperRoute::getSermonRoute($item->slug, $item->catslug);
+					}
 				}
-				$where = '(' . implode( ($phrase == 'all' ? ') AND (' : ') OR ('), $wheres ) . ')';
-				break;
+				$rows[] = $list;
+			}
 		}
 
-		switch ( $ordering ) {
-			case 'oldest':
-				$order = 'created_on DESC';
-				break;
+		if(in_array('spseries', $areas)) {
 
-			case 'alpha':
-				$order = 'series_title ASC';
-				break;
+			$wheres	= array();
+			switch ($phrase)
+			{
+				case 'exact':
+					$text		= $db->Quote('%'.$db->getEscaped($text, true).'%', false);
+					$wheres2	= array();
+					$wheres2[]	= 'a.series_title LIKE '.$text;
+					$wheres2[]	= 'a.series_description LIKE '.$text;
+					$where		= '(' . implode(') OR (', $wheres2) . ')';
+					break;
 
-			case 'popular':
-				$order = 'hits DESC, series_title ASC';
-				break;
-					
-			case 'newest':
-				$order = 'created_on ASC';
-				break;
-			default:
-				$order = 'series_title ASC';
+				case 'all':
+				case 'any':
+				default:
+					$words	= explode(' ', $text);
+					$wheres = array();
+					foreach ($words as $word)
+					{
+						$word		= $db->Quote('%'.$db->getEscaped($word, true).'%', false);
+						$wheres2	= array();
+						$wheres2[]	= 'a.series_title LIKE '.$word;
+						$wheres2[]	= 'a.series_description LIKE '.$word;
+						$wheres[]	= implode(' OR ', $wheres2);
+					}
+					$where	= '(' . implode(($phrase == 'all' ? ') AND (' : ') OR ('), $wheres) . ')';
+					break;
+			}
+
+			switch ($ordering)
+			{
+				case 'oldest':
+					$order = 'a.created ASC';
+					break;
+
+				case 'popular':
+					$order = 'a.hits DESC';
+					break;
+
+				case 'alpha':
+					$order = 'a.series_title ASC';
+					break;
+
+				case 'category':
+					$order = 'c.title ASC, a.series_title ASC';
+					$morder = 'a.series_title ASC';
+					break;
+
+				case 'newest':
+				default:
+					$order = 'a.created DESC';
+			}
+
+			if (!empty($state)) {
+				$query	= $db->getQuery(true);
+				$query->select('a.series_title AS title, a.series_description AS text, a.created AS created, '
+							.'CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(\':\', a.id, a.alias) ELSE a.id END as slug, '
+							.'CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(\':\', c.id, c.alias) ELSE c.id END as catslug, '
+							.'CONCAT_WS(" / ", '.$db->Quote($section).', c.title) AS section, "1" AS browsernav');
+				$query->from('#__sermon_series AS a');
+				$query->innerJoin('#__categories AS c ON c.id = a.catid');
+				$query->where('('.$where.')' . ' AND a.state in ('.implode(',',$state).') AND  c.published=1 AND  c.access IN ('.$groups.')');
+				$query->order($order);
+
+				// Filter by language
+				if ($app->isSite() && $app->getLanguageFilter()) {
+					$tag = JFactory::getLanguage()->getTag();
+					$query->where('c.language in (' . $db->Quote($tag) . ',' . $db->Quote('*') . ')');
+				}
+
+				$db->setQuery($query, 0, $limit);
+				$list = $db->loadObjectList();
+
+				if (isset($list))
+				{
+					foreach($list as $key => $item)
+					{
+						$list[$key]->href = SermonspeakerHelperRoute::getSerieRoute($item->slug, $item->catslug);
+					}
+				}
+				$rows[] = $list;
+			}
 		}
 
-		$query = 'SELECT series_title AS title,'
-		. ' series_description AS text,'
-		. ' created_on as created,'
-		. ' "2" AS browsernav,'
-		. ' id as slug, '
-		. ' CONCAT_WS( " / ", '. $searchSermonSpeaker .', series_title )AS section'
-		. ' FROM #__sermon_series'
-		. ' WHERE ( '.$where.')'
-		. ' AND published = 1'
-		. ' ORDER BY '. $order
-		;
-		$db->setQuery( $query, 0, $limit );
-		$list2 = $db->loadObjectList();
-    
-		// set a menu item
-		$menu = &JSite::getMenu();
-		$menuitems = $menu->getItems('link', 'index.php?option=com_sermonspeaker&view=series');
-		if ($menuitems == '') {
-			$menuitems = $menu->getItems('component', 'com_sermonspeaker');
-		}
-		$ss_itemid = $menuitems[0]->id;
-
-		foreach($list2 as $key => $row) {
-			$list2[$key]->href = 'index.php?option=com_sermonspeaker&view=serie&id='.$row->slug.'&Itemid='.$ss_itemid;
-		}
-
-		$rows[] = $list2;
-	}
-
-
-	$count = count( $rows );
-	if ( $count > 1 ) {
-		switch ( $count ) {
-			case 2:
-				$results = array_merge( (array) $rows[0], (array) $rows[1] );
-				break;
-
-			case 3:
-				$results = array_merge( (array) $rows[0], (array) $rows[1], (array) $rows[2] );
-				break;
-
-			case 4:
-			default:
-				$results = array_merge( (array) $rows[0], (array) $rows[1], (array) $rows[2], (array) $rows[3] );
-				break;
+		$results = array();
+		if (count($rows))
+		{
+			foreach($rows as $row)
+			{
+				$new_row = array();
+				foreach($row AS $key => $item) {
+					if (searchHelper::checkNoHTML($item, $searchText, array('text', 'title'))) {
+						$new_row[] = $item;
+					}
+				}
+				$results = array_merge($results, (array) $new_row);
+			}
 		}
 
 		return $results;
-	} else if ( $count == 1 ) {
-		return $rows[0];
 	}
 }
-?>
