22ae4e008589a007bafa925c6236bc973726d13a
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2011-08-19 14:27:32 +0000

    SS4.1: Added id3 Write to Tools.
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@505 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/com_sermonspeaker/admin/controllers/tools.php
+++ SermonSpeaker4.0/com_sermonspeaker/admin/controllers/tools.php
@@ -15,7 +15,7 @@ jimport('joomla.filesystem.folder');
  */
 class SermonspeakerControllerTools extends JController
 {
-	function order(){
+	public function order(){
 		// Check for request forgeries
 		JRequest::checkToken('request') or jexit(JText::_('JINVALID_TOKEN'));
 		$db		= JFactory::getDBO();
@@ -32,5 +32,71 @@ class SermonspeakerControllerTools extends JController
 			$this->setMessage('Successfully reordered the sermons');
 		}
 		$this->setRedirect('index.php?option=com_sermonspeaker&view=tools');
+	}
+
+	public function write_id3(){
+		// Check for request forgeries
+		JRequest::checkToken('request') or jexit(JText::_('JINVALID_TOKEN'));
+		$app	= JFactory::getApplication();
+		$db =& JFactory::getDBO();
+		$query	= "SELECT audiofile, videofile, sermons.created_by, sermons.catid, sermon_title, name, series_title, YEAR(sermon_date) AS date, notes, sermon_number \n"
+				. "FROM #__sermon_sermons AS sermons \n"
+				. "LEFT JOIN #__sermon_speakers AS speakers ON speaker_id = speakers.id \n"
+				. "LEFT JOIN #__sermon_series AS series ON series_id = series.id \n"
+				;
+		$db->setQuery($query);
+		$items	= $db->loadObjectList();
+		$user	= JFactory::getUser();
+		foreach($items as $item){
+			$canEdit	= $user->authorise('core.edit', 'com_sermonspeaker.category.'.$item->catid);
+			$canEditOwn	= $user->authorise('core.edit.own', 'com_sermonspeaker.category.'.$item->catid) && $item->created_by == $user->id;
+			if ($canEdit || $canEditOwn){
+				$files[]	= $item->audiofile;
+				$files[]	= $item->videofile;
+				require_once(JPATH_COMPONENT_SITE.DS.'id3'.DS.'getid3'.DS.'getid3.php');
+				$getID3		= new getID3;
+				$getID3->setOption(array('encoding'=>'UTF-8'));
+				require_once(JPATH_COMPONENT_SITE.DS.'id3'.DS.'getid3'.DS.'write.php');
+				$writer		= new getid3_writetags;
+				$writer->tagformats		= array('id3v2.3');
+				$writer->overwrite_tags	= true;
+				$writer->tag_encoding	= 'UTF-8';
+				$TagData = array(
+					'title'   => array($item->sermon_title),
+					'artist'  => array($item->name),
+					'album'   => array($item->series_title),
+					'year'    => array($item->date),
+					'comment' => array($item->notes),
+					'track'   => array($item->sermon_number),
+				);
+				$writer->tag_data = $TagData;
+				foreach ($files as $file){
+					if (!$file){
+						continue;
+					}
+					$path		= JPATH_SITE.str_replace('/', DS, $file);
+					$path		= str_replace(DS.DS, DS, $path);
+					if(!is_writable($path)){
+						continue;
+					}
+					$writer->filename	= $path;
+					if ($writer->WriteTags()) {
+						$app->enqueueMessage('Successfully wrote tags to "'.$file.'"');
+						if (!empty($writer->warnings)){
+							$app->enqueueMessage('There were some warnings: '.implode(', ', $writer->errors), 'notice');
+							$writer->warnings = array();
+						}
+					} else {
+						$app->enqueueMessage('Failed to write tags to "'.$file.'"! '.implode(', ', $writer->errors), 'error');
+						$writer->errors	= array();
+					}
+				}
+			} else {
+				$app->enqueueMessage(JText::_('JERROR_ALERTNOAUTHOR').' - '.$item->sermon_title, 'error');
+				continue;
+			}
+		}
+		$app->redirect('index.php?option=com_sermonspeaker&view=tools');
+		return;
 	}
 }
--- SermonSpeaker4.0/com_sermonspeaker/admin/views/tools/tmpl/default.php
+++ SermonSpeaker4.0/com_sermonspeaker/admin/views/tools/tmpl/default.php
@@ -15,6 +15,12 @@ $session	= JFactory::getSession();
 					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ORDER'); ?></span>
 				</a>
 			</div>
+			<div class="icon">
+				<a href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName().'='.$session->getId().'&'.JUtility::getToken(); ?>=1">
+					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/download.png"; ?>"/>
+					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></span>
+				</a>
+			</div>
 		</div>
 		<div style="clear: both;"> </div>
 	</td></tr></tbody>
