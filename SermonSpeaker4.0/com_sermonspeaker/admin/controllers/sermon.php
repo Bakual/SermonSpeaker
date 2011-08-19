e71f35f8783e3d87896c7cc0b83782aa9b05fc7a
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2011-08-19 13:13:19 +0000

    SS4.1: Added id3 write to sermon backend
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@503 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/com_sermonspeaker/admin/controllers/sermon.php
+++ SermonSpeaker4.0/com_sermonspeaker/admin/controllers/sermon.php
@@ -98,7 +98,7 @@ class SermonspeakerControllerSermon extends JControllerForm
 		$item 	= $model->getItem($id);
 		$user	= JFactory::getUser();
 		$canEdit	= $user->authorise('core.edit', 'com_sermonspeaker.category.'.$item->catid);
-		$canEditOwn	= $user->authorise('core.edit.own', 'com_sermonspeaker.category.'.$item->catid) && $item->created_by == $userId;
+		$canEditOwn	= $user->authorise('core.edit.own', 'com_sermonspeaker.category.'.$item->catid) && $item->created_by == $user->id;
 		if ($canEdit || $canEditOwn){
 			$query	= "UPDATE #__sermon_sermons \n"
 					. "SET hits='0' \n"
@@ -110,6 +110,72 @@ class SermonspeakerControllerSermon extends JControllerForm
 				exit();
 			}
 			$app->redirect('index.php?option=com_sermonspeaker&view=sermons', JText::sprintf('COM_SERMONSPEAKER_RESET_OK', JText::_('COM_SERMONSPEAKER_SERMON'), $item->sermon_title));
+			return;
+		} else {
+			$app->redirect('index.php?option=com_sermonspeaker&view=sermons', JText::_('JERROR_ALERTNOAUTHOR'), 'error');
+			return;
+		}
+	}
+	
+	public function write_id3(){
+		$app	= JFactory::getApplication();
+		$id		= JRequest::getInt('id', 0);
+		if (!$id){
+			$app->redirect('index.php?option=com_sermonspeaker&view=sermons', JText::_('JERROR_AN_ERROR_HAS_OCCURRED'), 'error');
+			return;
+		}
+		$db =& JFactory::getDBO();
+		$query	= "SELECT audiofile, videofile, sermons.created_by, sermons.catid, sermon_title, name, series_title, YEAR(sermon_date) AS date, notes, sermon_number \n"
+				. "FROM #__sermon_sermons AS sermons \n"
+				. "LEFT JOIN #__sermon_speakers AS speakers ON speaker_id = speakers.id \n"
+				. "LEFT JOIN #__sermon_series AS series ON series_id = series.id \n"
+				. "WHERE sermons.id='".$id."'"
+				;
+		$db->setQuery($query);
+		$item	= $db->loadObject();
+		$user	= JFactory::getUser();
+		$canEdit	= $user->authorise('core.edit', 'com_sermonspeaker.category.'.$item->catid);
+		$canEditOwn	= $user->authorise('core.edit.own', 'com_sermonspeaker.category.'.$item->catid) && $item->created_by == $user->id;
+		if ($canEdit || $canEditOwn){
+			$files[]	= $item->audiofile;
+			$files[]	= $item->videofile;
+			require_once(JPATH_COMPONENT_SITE.DS.'id3'.DS.'getid3'.DS.'getid3.php');
+			$getID3		= new getID3;
+			$getID3->setOption(array('encoding'=>'UTF-8'));
+			require_once(JPATH_COMPONENT_SITE.DS.'id3'.DS.'getid3'.DS.'write.php');
+			$writer		= new getid3_writetags;
+			$writer->tagformats		= array('id3v2.4');
+			$writer->overwrite_tags	= true;
+			$writer->tag_encoding	= 'UTF-8';
+			$TagData = array(
+				'title'   => array($item->sermon_title),
+				'artist'  => array($item->name),
+				'album'   => array($item->series_title),
+				'year'    => array($item->date),
+				'comment' => array($item->notes),
+				'track'   => array($item->sermon_number),
+			);
+			$tagwriter->tag_data = $TagData;
+			foreach ($files as $file){
+				if (!$file){
+					continue;
+				}
+				$path		= JPATH_SITE.str_replace('/', DS, $file);
+				$path		= str_replace(DS.DS, DS, $path);
+				if(!is_writable($path)){
+					continue;
+				}
+				$writer->filename	= $path;
+				if ($writer->WriteTags()) {
+					$app->enqueueMessage('Successfully wrote tags to "'.$file.'"');
+					if (!empty($writer->warnings)) {
+						JError::raiseNotice(100, 'There were some warnings:<br>'.implode(', ', $writer->warnings));
+					}
+				} else {
+					JError::raiseWarning(100, 'Failed to write tags to "'.$file.'"! '.implode(', ', $writer->errors));
+				}
+			}
+			$app->redirect('index.php?option=com_sermonspeaker&view=sermon&layout=edit&id='.$id);
 			return;
 		} else {
 			$app->redirect('index.php?option=com_sermonspeaker&view=sermons', JText::_('JERROR_ALERTNOAUTHOR'), 'error');
