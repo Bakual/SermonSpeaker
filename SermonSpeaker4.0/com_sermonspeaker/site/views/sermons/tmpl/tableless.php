07503b585ce8ea97827818a4d5ea36b2439b7f6d
bakual <bakual@5d6a53e0-5f49-0410-96cc-992822d07570>
2012-02-20 14:54:21 +0000

    SS4.3: Added pagination to the sermons tableless layout
    git-svn-id: http://joomlacode.org/svn/sermon_speaker@651 5d6a53e0-5f49-0410-96cc-992822d07570

--- SermonSpeaker4.0/com_sermonspeaker/site/views/sermons/tmpl/tableless.php
+++ SermonSpeaker4.0/com_sermonspeaker/site/views/sermons/tmpl/tableless.php
@@ -41,88 +41,121 @@ if (in_array('sermons:player', $this->columns) && count($this->items)) : ?>
 	</div>
 <?php endif; ?>
 <form action="<?php echo JFilterOutput::ampReplace(JFactory::getURI()->toString()); ?>" method="post" id="adminForm" name="adminForm">
-	<?php foreach($this->items as $i => $item) : ?>
-		<div id="sermon<?php echo $i; ?>" class="ss-entry" onclick="ss_play('<?php echo $i; ?>')">
-			<?php if ($item->picture): ?>
-				<div class="ss-picture"><img src="<?php echo SermonspeakerHelperSermonspeaker::makelink($item->picture); ?>"></div>
-			<?php elseif ($item->pic): ?>
-				<div class="ss-picture"><img src="<?php echo SermonspeakerHelperSermonspeaker::makelink($item->pic); ?>"></div>
-			<?php endif; ?>
-			<h3><a href="<?php echo JRoute::_(SermonspeakerHelperRoute::getSermonRoute($item->slug)); ?>"><?php echo $item->sermon_title; ?></a></h3>
-			<dl class="article-info sermon-info">
-			<dt class="article-info-term"><?php echo JText::_('JDETAILS'); ?></dt>
-			<?php if (in_array('sermons:series', $this->columns) && $item->series_title) : ?>
-				<dd class="category-name">
-					<?php echo JText::_('COM_SERMONSPEAKER_SERIE_TITLE'); ?>:
-					<?php if ($item->series_state) : ?>
-						<a href="<?php echo JRoute::_(SermonspeakerHelperRoute::getSerieRoute($item->series_slug)); ?>">
-					<?php echo $this->escape($item->series_title); ?></a>
-					<?php else :
-						echo $this->escape($item->series_title);
-					endif; ?>
-				</dd>
+	<?php if ($this->params->get('filter_field')) :?>
+		<fieldset class="filters">
+			<legend class="hidelabeltxt">
+				<?php echo JText::_('JGLOBAL_FILTER_LABEL'); ?>
+			</legend>
+			<div class="filter-search">
+				<label class="filter-search-lbl" for="filter-search"><?php echo JText::_('JGLOBAL_FILTER_LABEL').'&nbsp;'; ?></label>
+				<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_SERMONSPEAKER_FILTER_SEARCH_DESC'); ?>" />
+			</div>
+	<?php endif;
+	if ($this->params->get('show_pagination_limit')) : ?>
+			<div class="display-limit">
+				<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>&nbsp;
+				<?php echo $this->pagination->getLimitBox(); ?>
+			</div>
+	<?php endif;
+	if ($this->params->get('filter_field')) : ?>
+		</fieldset>
+	<?php endif;
+	if (!count($this->items)) : ?>
+		<div class="no_entries"><?php echo JText::sprintf('COM_SERMONSPEAKER_NO_ENTRIES', JText::_('COM_SERMONSPEAKER_SERMONS')); ?></div>
+	<?php else : ?>
+		<?php foreach($this->items as $i => $item) : ?>
+			<div id="sermon<?php echo $i; ?>" class="ss-entry" onclick="ss_play('<?php echo $i; ?>')">
+				<?php if ($item->picture): ?>
+					<div class="ss-picture"><img src="<?php echo SermonspeakerHelperSermonspeaker::makelink($item->picture); ?>"></div>
+				<?php elseif ($item->pic): ?>
+					<div class="ss-picture"><img src="<?php echo SermonspeakerHelperSermonspeaker::makelink($item->pic); ?>"></div>
+				<?php endif; ?>
+				<h3><a href="<?php echo JRoute::_(SermonspeakerHelperRoute::getSermonRoute($item->slug)); ?>"><?php echo $item->sermon_title; ?></a></h3>
+				<dl class="article-info sermon-info">
+				<dt class="article-info-term"><?php echo JText::_('JDETAILS'); ?></dt>
+				<?php if (in_array('sermons:series', $this->columns) && $item->series_title) : ?>
+					<dd class="category-name">
+						<?php echo JText::_('COM_SERMONSPEAKER_SERIE_TITLE'); ?>:
+						<?php if ($item->series_state) : ?>
+							<a href="<?php echo JRoute::_(SermonspeakerHelperRoute::getSerieRoute($item->series_slug)); ?>">
+						<?php echo $this->escape($item->series_title); ?></a>
+						<?php else :
+							echo $this->escape($item->series_title);
+						endif; ?>
+					</dd>
+				<?php endif;
+				if (in_array('sermons:date', $this->columns)) : ?>
+					<dd class="create">
+						<?php echo JText::_('COM_SERMONSPEAKER_FIELD_DATE_LABEL'); ?>:
+						<?php echo JHTML::Date($item->sermon_date, JText::_($this->params->get('date_format')), 'UTC'); ?>
+					</dd>
+				<?php endif;
+				if (in_array('sermons:speaker', $this->columns) && $item->name) : ?>
+					<dd class="createdby">
+						<?php echo JText::_('COM_SERMONSPEAKER_SPEAKER'); ?>:
+						<?php if ($item->speaker_state):
+							echo SermonspeakerHelperSermonSpeaker::SpeakerTooltip($item->speaker_slug, $item->pic, $item->name);
+						else: 
+							echo $item->name;
+						endif; ?>
+					</dd>
+				<?php endif;
+				if (in_array('sermons:hits', $this->columns)) : ?>
+					<dd class="hits">
+						<?php echo JText::_('JGLOBAL_HITS'); ?>: 
+						<?php echo $item->hits; ?>
+					</dd>
+				<?php endif;if (in_array('sermons:scripture', $this->columns) && $item->sermon_scripture) : ?>
+					<dd class="ss-sermondetail-info">
+						<?php echo JText::_('COM_SERMONSPEAKER_FIELD_SCRIPTURE_LABEL'); ?>:
+						<?php echo JHTML::_('content.prepare', $item->sermon_scripture); ?>
+					</dd>
+				<?php endif;
+				if ($this->params->get('custom1') && $item->custom1) : ?>
+					<dd class="ss-sermondetail-info">
+						<?php echo JText::_('COM_SERMONSPEAKER_CUSTOM1'); ?>:
+						<?php echo $item->custom1; ?>
+					</dd>
+				<?php endif;
+				if ($this->params->get('custom2') && $item->custom2) : ?>
+					<dd class="ss-sermondetail-info">
+						<?php echo JText::_('COM_SERMONSPEAKER_CUSTOM2'); ?>:
+						<?php echo $item->custom2; ?>
+					</dd>
+				<?php endif;
+				if (in_array('sermons:length', $this->columns)) : ?>
+					<dd class="ss-sermondetail-info">
+						<?php echo JText::_('COM_SERMONSPEAKER_FIELD_LENGTH_LABEL'); ?>:
+						<?php echo SermonspeakerHelperSermonspeaker::insertTime($item->sermon_time); ?>
+					</dd>
+				<?php endif;
+				if (in_array('sermons:addfile', $this->columns) && $item->addfile) : ?>
+					<dd class="ss-sermondetail-info">
+						<?php echo JText::_('COM_SERMONSPEAKER_ADDFILE'); ?>:
+						<?php echo SermonspeakerHelperSermonspeaker::insertAddfile($item->addfile, $item->addfileDesc); ?>
+					</dd>
+				<?php endif; ?>
+				</dl>
+				<div style="clear:left"></div>
+				<?php if (strlen($item->notes) > 0) : ?>
+					<div>
+						<?php echo JHTML::_('content.prepare', $item->notes); ?>
+					</div>
+				<?php endif; ?>
+			</div>
+			<hr class="ss-sermons-player" style="clear:both" />
+		<?php endforeach;
+	endif;
+	if ($this->params->get('show_pagination') && ($this->pagination->get('pages.total') > 1)) : ?>
+		<div class="pagination">
+			<?php if ($this->params->get('show_pagination_results', 1)) : ?>
+				<p class="counter">
+					<?php echo $this->pagination->getPagesCounter(); ?>
+				</p>
 			<?php endif;
-			if (in_array('sermons:date', $this->columns)) : ?>
-				<dd class="create">
-					<?php echo JText::_('COM_SERMONSPEAKER_FIELD_DATE_LABEL'); ?>:
-					<?php echo JHTML::Date($item->sermon_date, JText::_($this->params->get('date_format')), 'UTC'); ?>
-				</dd>
-			<?php endif;
-			if (in_array('sermons:speaker', $this->columns) && $item->name) : ?>
-				<dd class="createdby">
-					<?php echo JText::_('COM_SERMONSPEAKER_SPEAKER'); ?>:
-					<?php if ($item->speaker_state):
-						echo SermonspeakerHelperSermonSpeaker::SpeakerTooltip($item->speaker_slug, $item->pic, $item->name);
-					else: 
-						echo $item->name;
-					endif; ?>
-				</dd>
-			<?php endif;
-			if (in_array('sermons:hits', $this->columns)) : ?>
-				<dd class="hits">
-					<?php echo JText::_('JGLOBAL_HITS'); ?>: 
-					<?php echo $item->hits; ?>
-				</dd>
-			<?php endif;if (in_array('sermons:scripture', $this->columns) && $item->sermon_scripture) : ?>
-				<dd class="ss-sermondetail-info">
-					<?php echo JText::_('COM_SERMONSPEAKER_FIELD_SCRIPTURE_LABEL'); ?>:
-					<?php echo JHTML::_('content.prepare', $item->sermon_scripture); ?>
-				</dd>
-			<?php endif;
-			if ($this->params->get('custom1') && $item->custom1) : ?>
-				<dd class="ss-sermondetail-info">
-					<?php echo JText::_('COM_SERMONSPEAKER_CUSTOM1'); ?>:
-					<?php echo $item->custom1; ?>
-				</dd>
-			<?php endif;
-			if ($this->params->get('custom2') && $item->custom2) : ?>
-				<dd class="ss-sermondetail-info">
-					<?php echo JText::_('COM_SERMONSPEAKER_CUSTOM2'); ?>:
-					<?php echo $item->custom2; ?>
-				</dd>
-			<?php endif;
-			if (in_array('sermons:length', $this->columns)) : ?>
-				<dd class="ss-sermondetail-info">
-					<?php echo JText::_('COM_SERMONSPEAKER_FIELD_LENGTH_LABEL'); ?>:
-					<?php echo SermonspeakerHelperSermonspeaker::insertTime($item->sermon_time); ?>
-				</dd>
-			<?php endif;
-			if (in_array('sermons:addfile', $this->columns) && $item->addfile) : ?>
-				<dd class="ss-sermondetail-info">
-					<?php echo JText::_('COM_SERMONSPEAKER_ADDFILE'); ?>:
-					<?php echo SermonspeakerHelperSermonspeaker::insertAddfile($item->addfile, $item->addfileDesc); ?>
-				</dd>
-			<?php endif; ?>
-			</dl>
-			<div style="clear:left"></div>
-			<?php if (strlen($item->notes) > 0) : ?>
-				<div>
-					<?php echo JHTML::_('content.prepare', $item->notes); ?>
-				</div>
-			<?php endif; ?>
+			echo $this->pagination->getPagesLinks(); ?>
 		</div>
-		<hr class="ss-sermons-player" style="clear:both" />
-	<?php endforeach; ?>
+	<?php endif; ?>
 	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
 	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 </form>
