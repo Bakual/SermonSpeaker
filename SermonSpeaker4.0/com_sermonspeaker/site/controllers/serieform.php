4086819f74e3de91b1129d4efb749fd283499ca2
Bakual <werbemails@bakual.ch>
2012-11-14 22:16:29 +0100

    Fixing Frontend ACLs in Controller

--- SermonSpeaker4.0/com_sermonspeaker/site/controllers/serieform.php
+++ SermonSpeaker4.0/com_sermonspeaker/site/controllers/serieform.php
@@ -48,51 +48,66 @@ class SermonspeakerControllerSerieform extends JControllerForm
 	 *
 	 * @param	array	$data	An array of input data.
 	 * @return	boolean
-	 * @since	1.6
 	 */
 	protected function allowAdd($data = array())
 	{
-		// Initialise variables.
 		$user		= JFactory::getUser();
-		$categoryId	= JArrayHelper::getValue($data, 'catid', JFactory::getApplication()->input->get('id', 0, 'int'), 'int');
-		$allow		= null;
+		$categoryId	= JArrayHelper::getValue($data, 'catid', $this->input->getInt('filter_category_id'), 'int');
+		$allow = null;
 
-		if ($categoryId) {
-			// If the category has been passed in the URL check it.
-			$allow	= $user->authorise('core.create', $this->option.'.category.'.$categoryId);
+		if ($categoryId)
+		{
+			// If the category has been passed in the data or URL check it.
+			$allow = $user->authorise('core.create', $this->option.'.category.'.$categoryId);
 		}
 
-		if ($allow === null) {
+		if ($allow === null)
+		{
 			// In the absense of better information, revert to the component permissions.
 			return parent::allowAdd($data);
-		} else {
+		}
+		else
+		{
 			return $allow;
 		}
 	}
 
 	/**
-	 * Method to check if you can edit a record.
+	 * Method to check if you can add a new record.
 	 *
 	 * @param	array	$data	An array of input data.
 	 * @param	string	$key	The name of the key for the primary key.
 	 *
 	 * @return	boolean
-	 * @since	1.6
 	 */
 	protected function allowEdit($data = array(), $key = 'id')
 	{
-		// Initialise variables.
-		$recordId	= (int) isset($data[$key]) ? $data[$key] : 0;
+		$recordId = (int) isset($data[$key]) ? $data[$key] : 0;
 		$categoryId = 0;
 
-		if ($recordId) {
-			$categoryId = (int) $this->getModel()->getItem($recordId)->catid;
+		if ($recordId)
+		{
+			// Need to do a lookup from the model.
+			$record = $this->getModel()->getItem($recordId);
+			$categoryId = (int) $record->catid;
 		}
 
-		if ($categoryId) {
+		if ($categoryId)
+		{
+			$user = JFactory::getUser();
 			// The category has been set. Check the category permissions.
-			return JFactory::getUser()->authorise('core.edit', $this->option.'.category.'.$categoryId);
-		} else {
+			if ($user->authorise('core.edit', $this->option.'.category.'.$categoryId))
+			{
+				return true;
+			}
+			// Fallback on edit.own.
+			if ($user->authorise('core.edit.own', $this->option.'.category.'.$categoryId))
+			{
+				return ($record->created_by == $user->get('id'));
+			}
+		}
+		else
+		{
 			// Since there is no asset tracking, revert to the component permissions.
 			return parent::allowEdit($data, $key);
 		}
