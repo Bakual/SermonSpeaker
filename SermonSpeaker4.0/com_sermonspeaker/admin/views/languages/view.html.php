3ae6aaa51c62b91cb452a56d60de9e1c6e0977cd
Bakual <bakual@bakual.ch>
2013-01-10 08:19:33 +0100

    Improved language view

--- SermonSpeaker4.0/com_sermonspeaker/admin/views/languages/view.html.php
+++ SermonSpeaker4.0/com_sermonspeaker/admin/views/languages/view.html.php
@@ -14,9 +14,51 @@ class SermonspeakerViewLanguages extends JViewLegacy
 			$this->setLayout($this->getLayout().'30');
 		}
 
+		/* Settings for XML parsing */
 		$url				= 'http://www.sermonspeaker.net/languages.raw';
+		$this->site			= 'http://www.sermonspeaker.net';
+		$this->prefix		= strtoupper(JApplicationHelper::getComponentName());
+
+		/* Loading XML and installed languages */
 		$this->xml			= simplexml_load_file($url);
 		$this->languages	= JFactory::getLanguage()->getKnownLanguages();
+
+		// Get extension info
+		$db		= JFactory::getDbo();
+		$db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "'.$this->prefix.'"');
+		$this->manifest		= json_decode($db->loadResult(), true);
+
+		// Get installed language packs
+		$query	= $db->getQuery(true);
+		$query->select('ext.name, ext.manifest_cache, ext.element');
+		$query->from('`#__extensions` AS ext');
+		$query->where('`element` LIKE "'.$this->xml->extension_name.'%"');
+		$db->setQuery($query);
+		$this->installed	= $db->loadObjectList('element');
+		foreach ($this->installed as $item)
+		{
+			$data = json_decode($item->manifest_cache);
+			if ($data)
+			{
+				foreach ($data as $key => $value)
+				{
+					if ($key == 'type')
+					{
+						// Ignore the type field
+						continue;
+					}
+					elseif ($key == 'creationDate')
+					{
+						$date = explode('.', $value);
+						if (count($date) == 3)
+						{
+							$value = $date[2].'-'.$date[1].'-'.$date[0];
+						}
+					}
+					$item->$key = $value;
+				}
+			}
+		}
 
 		$this->addToolbar();
 		if ($this->joomla30)
