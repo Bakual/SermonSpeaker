c9030ec1cbeb613f16999a47f584034c859837d2
Bakual <bakual@bakual.ch>
2013-01-10 16:09:32 +0100

    Adding languages layout for J2.5

--- SermonSpeaker4.0/com_sermonspeaker/admin/views/languages/tmpl/default.php
+++ SermonSpeaker4.0/com_sermonspeaker/admin/views/languages/tmpl/default.php
@@ -1,60 +1,77 @@
 <?php
 defined('_JEXEC') or die;
-JHTML::_('behavior.tooltip');
-JHTML::_('behavior.modal');
-$session	= JFactory::getSession();
-$user	= JFactory::getUser();
 ?>
-<table class="adminform">
-	<tbody><tr><td valign="top">
-		<div id="cpanel">
-			<div class="icon hasTip" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3').'::'.JText::_('COM_SERMONSPEAKER_TOOLS_ID3_DESC'); ?>">
-				<a href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName().'='.$session->getId().'&'.JSession::getFormToken(); ?>=1">
-					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/download.png"; ?>"/>
-					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></span>
-				</a>
-			</div>
-			<div class="icon hasTip" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME').'::'.JText::_('COM_SERMONSPEAKER_TOOLS_TIME_DESC'); ?>">
-				<a class="modal" href="index.php?option=com_sermonspeaker&view=tools&layout=time&tmpl=component" rel="{handler: 'iframe', size: {x: 350, y: 170}}">
-					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/clock.png"; ?>"/>
-					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?></span>
-				</a>
-			</div>
-			<div class="icon hasTip" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND').'::'.JText::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?>">
-				<a class="modal" href="index.php?option=com_sermonspeaker&view=files&layout=modal&tmpl=component" rel="{handler: 'iframe', size: {x: 700, y: 500}}">
-					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/find.png"; ?>"/>
-					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND'); ?></span>
-				</a>
-			</div>
-			<?php // Check Access
-				if (!$user->authorise('com_sermonspeaker.script', 'com_sermonspeaker')):
-					$link = 'href="#" onclick="alert(\''.JText::_('JERROR_ALERTNOAUTHOR').'\')"';
-					$class = ' inactive';
-				else:
-					$link = 'href="index.php?option=com_sermonspeaker&task=tools.createAutomatic"';
-					$class = '';
-				endif; ?>
-			<div class="icon <?php echo $class; ?> hasTip" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC').'::'.JText::sprintf('COM_SERMONSPEAKER_TOOLS_AUTOMATIC_DESC', JURI::root()); ?>">
-				<a <?php echo $link; ?>>
-					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/run.png"; ?>"/>
-					<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?></span>
-				</a>
-			</div>
-			<?php if($this->pi): ?>
-				<div class="icon hasTip" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT').'::'.JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_DESC'); ?>">
-					<a href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName().'='.$session->getId().'&'.JSession::getFormToken(); ?>=1">
-						<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/import.png"; ?>"/>
-						<span><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?></span>
-					</a>
-				</div>
-			<?php endif; ?>
-			<div class="icon">
-				<a href="index.php?option=com_sermonspeaker&view=statistics&format=raw">
-					<img border="0" align="middle" alt="<?php echo JText::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?>" src="<?php echo JURI::base()."components/com_sermonspeaker/images/stats.png"; ?>"/>
-					<span><?php echo JText::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?></span>
-				</a>
-			</div>
+<form enctype="multipart/form-data"  action="<?php echo JRoute::_('index.php?option=com_installer&view=install'); ?>" method="post" name="adminForm" id="adminForm">
+<?php if ($this->xml) : ?>
+		<div class="well">
+			<h3><?php echo $this->xml->title; ?></h3>
+			<?php echo (string)$this->xml->description; ?>
 		</div>
-		<div style="clear: both;"> </div>
-	</td></tr></tbody>
-</table>	
+		<table class="table adminlist">
+			<?php foreach ($this->xml->language as $i => $language) :
+				$class = 'btn';
+				if ((int)$language->completed && isset($this->installed[$this->xml->extension_name.'-'.$language->lang_name])) :
+					// language pack is installed
+					$creationDate	= $this->installed[$this->xml->extension_name.'-'.$language->lang_name]->creationDate;
+					if (strtotime($language->created) > strtotime($creationDate)) :
+						$class .= ' btn-warning';
+						$text	= JText::_($this->prefix.'_NEWER_LANGUAGEPACK_THAN_INSTALLED');
+					else :
+						$class .= ' btn-success';
+						$text	= JText::_($this->prefix.'_NEWEST_LANGUAGE_INSTALLED');
+					endif;
+				elseif ((int)$language->completed && isset($this->languages[str_replace('_', '-', $language->lang_name)])) :
+					// site language is installed
+					if (strtotime($language->created) > strtotime($this->manifest['creationDate'])) :
+						$class .= ' btn-primary';
+						$text	= JText::_($this->prefix.'_NEWER_LANGUAGEPACK_THAN_EXTENSION');
+					else :
+						$class .= ' btn-info';
+						$text	= JText::_($this->prefix.'_NEWEST_LANGUAGE_INSTALLED');
+					endif;
+				else :
+					// language pack available
+					$text	= JText::_($this->prefix.'_SITELANGUAGE_NOT_INSTALLED');
+				endif; ?>
+				<tr class="row<?php echo $i % 2; ?>">
+					<td>
+						<input type="button" class="<?php echo $class; ?>" value="<?php echo JText::_($this->prefix.'_INSTALL_LANGUAGEPACK'); ?>" onclick="document.getElementById('install_url').value = '<?php echo $language->link; ?>'; Joomla.submitbutton();" title="<?php echo $text; ?>" />
+						<a href="<?php echo $language->link; ?>" title="<?php echo JText::sprintf($this->prefix.'_DOWNLOAD_LANGUAGEPACK', $this->site); ?>">
+							<?php if (isset($language->iso_lang_name)) : ?>
+							<?php echo $language->iso_lang_name; ?>
+							<?php if (isset($language->iso_country_name) && $language->iso_country_name != '') : ?>
+								(<?php echo $language->iso_country_name; ?>)
+							<?php endif; ?>
+							<?php else : ?>
+								<?php echo $language->lang_name; ?>
+							<?php endif; ?>
+						</a>
+					</td>
+					<td width="40%">
+						<div class="progress progress-striped">
+							<div class="bar" style="width:<?php echo $language->completed; ?>%;"><?php echo $language->completed; ?>%</div>
+						</div>
+					</td>
+					<td>
+						<?php echo JHtml::Date($language->created, JText::_('DATE_FORMAT_LC4')); ?>
+						&nbsp;
+						<?php if($this->xml->contribute && $language->completed != 100) : ?>
+							<button><a href="http://transifex.com/projects/p/<?php echo $this->xml->transifex_slug; ?>/language/<?php echo $language->lang_name; ?>" class="btn" target="_blank">
+								<?php echo JText::_($this->prefix.'_CONTRIBUTE_NOW'); ?>
+							</a></button>
+						<?php endif; ?>
+					</td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+<?php else : ?>
+		<div class="well">
+			<?php echo JText::_($this->prefix.'_XML_ERROR'); ?>
+		</div>
+<?php endif; ?>
+	</div>
+	<input type="hidden" id="install_url" name="install_url" />
+	<input type="hidden" name="installtype" value="url" />
+	<input type="hidden" name="task" value="install.install" />
+	<?php echo JHtml::_('form.token'); ?>
+</form>
