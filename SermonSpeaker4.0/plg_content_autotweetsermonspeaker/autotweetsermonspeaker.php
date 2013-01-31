cff226d951df62ac7dbf7a070990c9793ba92971
Bakual <werbemails@bakual.ch>
2013-01-31 22:19:25 +0100

    Adding Autotweet plugin

---
+++ SermonSpeaker4.0/plg_content_autotweetsermonspeaker/autotweetsermonspeaker.php
@@ -0,0 +1,278 @@
+<?php
+
+/**
+ * @package  autotweetsermonspeaker - Plugin AutoTweetNG SermonSpeaker-Extension
+ *
+ * @author      Thomas Hunziker
+ * @copyright   Copyright (C) 2012 SermonSpeaker.net. All rights reserved.
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ * @link        http://www.sermonspeaker.net
+ */
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+// Check for component
+if (!JComponentHelper::getComponent('com_autotweet', true)->enabled)
+{
+	JError::raiseWarning('5', 'AutoTweet NG Component is not installed or not enabled. - ' . __FILE__);
+	return;
+}
+
+require_once JPATH_ROOT . '/administrator/components/com_autotweet/helpers/autotweetbase.php';
+
+/**
+ * PlgContentAutotweetSermonspeaker
+ *
+ * @package     Extly.Components
+ * @subpackage  com_autotweet
+ * @since       1.0
+ */
+class PlgContentAutotweetSermonspeaker extends plgAutotweetBase
+{
+	// Typeinfo
+	const TYPE_SERMON = 1;
+
+	// Plugin params
+
+	protected $categories = '';
+
+	protected $excluded_categories = '';
+
+	protected $post_modified = 0;
+
+	protected $show_catsec = 0;
+
+	protected $show_hash = 0;
+
+	protected $use_text = 0;
+
+	protected $use_text_count = 100;
+
+	protected $static_text = '';
+
+	protected $static_text_pos = 1;
+
+	protected $static_text_source = 0;
+
+	protected $metakey_count = 1;
+
+	protected $interval = 60;
+
+	// -1 means: nothing special to do
+	private $_post_modified_as_new = -1;
+
+	/**
+	 * plgContentAutotweetSermonspeaker
+	 *
+	 * @param   string  &$subject  Param
+	 * @param   object  $params    Param
+	 *
+	 * @since	1.5
+	 */
+	public function plgContentAutotweetSermonspeaker(&$subject, $params)
+	{
+		parent::__construct($subject, $params);
+
+		defined('JPATH_AUTOTWEET') || define('JPATH_AUTOTWEET', JPATH_ADMINISTRATOR . '/components/com_autotweet');
+		defined('JPATH_AUTOTWEET_HELPERS') || define('JPATH_AUTOTWEET_HELPERS', JPATH_AUTOTWEET . '/helpers');
+
+		JLoader::register('CparamsHelper', JPATH_AUTOTWEET_HELPERS . '/cparams.php');
+		JLoader::register('AutotweetLog', JPATH_AUTOTWEET_HELPERS . '/autotweetlog.php');
+		JLoader::register('AutotweetPostHelper', JPATH_AUTOTWEET_HELPERS . '/autotweetposthelper.php');
+
+		JLoader::import('components.com_sermonspeaker.helpers.route', JPATH_ROOT);
+		JLoader::import('components.com_sermonspeaker.helpers.sermonspeaker', JPATH_ROOT);
+
+		$pluginParams = $this->pluginParams;
+
+		// Joomla article specific params
+		$this->categories = $pluginParams->get('categories', '');
+		$this->excluded_categories = $pluginParams->get('excluded_categories', '');
+		$this->post_modified = (int) $pluginParams->get('post_modified', 0);
+		$this->show_catsec = (int) $pluginParams->get('show_catsec', 0);
+		$this->show_hash = (int) $pluginParams->get('show_hash', 0);
+		$this->use_text = (int) $pluginParams->get('use_text', 0);
+		$this->use_text_count = $pluginParams->get('use_text_count', 100);
+		$this->static_text = strip_tags($pluginParams->get('static_text', ''));
+		$this->static_text_pos = (int) $pluginParams->get('static_text_pos', 1);
+		$this->static_text_source = (int) $pluginParams->get('static_text_source', 0);
+		$this->metakey_count = (int) $pluginParams->get('metakey_count', 1);
+		$this->interval = (int) $pluginParams->get('interval', 60);
+
+		// Correct value if value is under the minimum
+		if ($this->interval < 60)
+		{
+			$this->interval = 60;
+		}
+
+		// Check type and range, and correct if needed
+		$this->use_text_count = $this->getTextcount($this->use_text_count);
+	}
+
+	/**
+	 * onContentAfterSave
+	 *
+	 * @param   object  $context  The context of the content passed to the plugin.
+	 * @param   object  $item  A JTableContent object
+	 * @param   bool    $isNew    If the content is just about to be created
+	 *
+	 * @return	boolean
+	 *
+	 * @since	1.5
+	 */
+	public function onContentAfterSave($context, $item, $isNew)
+	{
+		/*
+		 request from backend:
+		 - com_sermonspeaker.sermon
+		 requests form frontend: com_sermonspeaker.form ->TODO: ?
+		 */
+		// Sermon
+		if ((($context == 'com_sermonspeaker.sermon') || ($context == 'com_sermonspeaker.form'))
+			&& ($isNew || $this->post_modified) && (1 == $item->state))
+		{
+			$this->postSermon($item);
+		}
+
+		return true;
+	}
+
+	/**
+	 * onContentChangeState
+	 *
+	 * @param   object  $context  The context of the content passed to the plugin.
+	 * @param   array   $pks      A list of primary key ids of the content that has changed state.
+	 * @param   int     $value    The value of the state that the content has been changed to.
+	 *
+	 * @return	boolean
+	 *
+	 * @since	1.5
+	 */
+	public function onContentChangeState($context, $pks, $value)
+	{
+		// Sermon
+		if ((($context == 'com_sermonspeaker.sermon') || ($context == 'com_sermonspeaker.form'))
+			&& ($value == 1))
+		{
+			$item = JTable::getInstance('Sermon', 'SermonspeakerTable');
+
+			foreach ($pks as $id)
+			{
+				$item->load($id);
+				$this->postSermon($item);
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * postSermon
+	 *
+	 * @param   object  $item  The item object.
+	 *
+	 * @return	boolean
+	 *
+	 * @since	1.5
+	 */
+	protected function postSermon($item)
+	{
+		$publish_up	= JFactory::getDate()->toSql();
+
+		$cats		= $this->getContentCategories($item->catid);
+		$cat_alias	= $cats[2];
+
+		// Use main category for item url
+		$cat_slug	= $catids[0] . ':' . JFilterOutput::stringURLSafe($cat_alias[0]);
+		$id_slug	= $item->id . ':' . JFilterOutput::stringURLSafe($item->alias);
+
+		// Create internal url for sermon
+		$url		= SermonspeakerHelperRoute::getSermonRoute($id_slug, $cat_slug);
+
+		// Get the image
+		$image_url	= SermonspeakerHelperSermonspeaker::insertPicture($item, 1);
+
+		$native_object = json_encode($item);
+		$this->postStatusMessage($item->id, $publish_up, $item->sermon_title, self::TYPE_SERMON, $url, $image_url, $native_object);
+	}
+
+	/**
+	 * getExtendedData
+	 *
+	 * @param   string  $id              Param.
+	 * @param   string  $typeinfo        Param.
+	 * @param   string  &$native_object  Param.
+	 *
+	 * @return	array
+	 *
+	 * @since	1.5
+	 */
+	public function getExtendedData($id, $typeinfo, &$native_object)
+	{
+		$item = json_decode($native_object);
+
+		// Get category path for item
+		$cats = $this->getContentCategories($item->catid);
+		$catids = $cats[0];
+		$cat_names = $cats[1];
+
+		// Needed for url only
+		$cat_alias = $cats[2];
+
+		// Use item title or text as message
+		$title = $item->sermon_title;
+		$item_text = $item->notes;
+		$text = $this->getMessagetext($this->use_text, $this->use_text_count, $title, $item_text);
+		$hashtags = '';
+
+		// Use metakey or static text or nothing
+		if ((2 == $this->static_text_source) || ((1 == $this->static_text_source) && (empty($item->metakey))))
+		{
+			$title = $this->addStatictext($this->static_text_pos, $title, $this->static_text);
+			$text = $this->addStatictext($this->static_text_pos, $text, $this->static_text);
+		}
+		elseif (1 == $this->static_text_source)
+		{
+			$hashtags .= $this->getHashtags($item->metakey, $this->metakey_count);
+		}
+
+		// Title
+		$catsec_result = $this->addCategories($this->show_catsec, $cat_names, $title, 0);
+		$title = $catsec_result['text'];
+
+		// Text
+		$catsec_result = $this->addCategories($this->show_catsec, $cat_names, $text, $this->show_hash);
+		$text = $catsec_result['text'];
+		if ('' != $catsec_result['hashtags'])
+		{
+			$hashtags .= ' ';
+			$hashtags .= $catsec_result['hashtags'];
+		}
+
+		$data = array(
+			'title' => $title,
+			'text' => $text,
+			'hashtags' => $hashtags,
+
+			// Already done when msg is inserted in queue
+			'url' => '',
+
+			// Already done when msg is inserted in queue
+			'image_url' => '',
+
+			'fulltext' => $item_text,
+			'catids' => $catids,
+			'cat_names' => $cat_names,
+			'author' => $this->getArticleAuthor($item),
+			'language' => $item->language,
+
+			// Sermons don't have an access field, maybe take category access instead
+//			'access' => $item->access,
+
+			'is_valid' => true
+		);
+
+		return $data;
+	}
+
+}
