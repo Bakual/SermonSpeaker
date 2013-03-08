8a0ef329271c81f06af8d607caa2d2709c93b046
Bakual <werbemails@bakual.ch>
2013-03-09 00:02:30 +0100

    Rewrote feed view.

--- SermonSpeaker4.0/com_sermonspeaker/site/views/feed/tmpl/default.php
+++ SermonSpeaker4.0/com_sermonspeaker/site/views/feed/tmpl/default.php
@@ -1,126 +1,105 @@
-<?php
-defined('_JEXEC') or die;
+<?php defined('_JEXEC') or die;
+$params	= $this->params;
+
 // required channel elements
-echo '<?xml version="1.0" encoding="utf-8" ?>
+echo '<?xml version="1.0" encoding="utf-8" ?>'; ?>
 <rss version="2.0" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-	<title>'.$this->channel->title.'</title>
-	<link>'.$this->channel->link.'</link>
-	<atom:link href="http://'.$this->channel->atomlink.'" rel="self" type="application/rss+xml" />
-	<description>'.$this->channel->description.'</description>';
-// optional channel elements
-echo '
-	<generator>SermonSpeaker 4.4</generator>
-	<lastBuildDate>'.gmdate("r").'</lastBuildDate>';
-if ($this->channel->language)
-{
-	echo '
-	<language>'.$this->channel->language.'</language>';
-}
-if ($this->channel->manEditor)
-{
-	echo '
-	'.$this->channel->manEditor;
-}
-if ($this->channel->copyright)
-{
-	echo '
-	<copyright>'.$this->channel->copyright.'</copyright>';
-}
+	<title><?php echo $this->make_xml_safe($params->get('sc_title')); ?></title>
+	<link><?php echo JURI::root(); ?></link>
+	<atom:link href="<?php echo JURI::getInstance()->toString(); ?>" rel="self" type="application/rss+xml" />
+	<description><?php echo $this->make_xml_safe($params->get('description')); ?></description>
+<?php // optional channel elements ?>
+	<generator>SermonSpeaker 4.5</generator>
+	<lastBuildDate><?php echo gmdate("r"); ?></lastBuildDate>
+<?php if ($params->get('itLanguage')) : ?>
+	<language><?php echo $this->make_xml_safe($params->get('itLanguage')); ?></language>
+<?php endif;
+if ($params->get('editor')) :
+	if ($params->get('editorEmail')) : ?>
+	<managingEditor><?php echo $this->make_xml_safe($params->get('editorEmail')); ?> (<?php echo $this->make_xml_safe($params->get('editor')); ?>)</managingEditor>
+<?php else : ?>
+	<dc:creator><?php echo $this->make_xml_safe($params->get('editor')); ?></dc:creator>
+<?php endif;
+endif;
+if ($params->get('copyright')) : ?>
+	<copyright><?php echo $this->make_xml_safe($params->get('copyright')); ?></copyright>
+<?php endif;
 // Custom Code
-if ($this->channel->custom)
-{
-	echo '
-	'.$this->channel->custom;
-}
-// iTunes tags
-echo '
-	<itunes:summary>'.$this->channel->itSummary.'</itunes:summary>';
-if ($this->channel->itAuthor)
-{
-	echo '
-	<itunes:author>'.$this->channel->itAuthor.'</itunes:author>';
-}
-if($this->channel->itCategories)
-{
-	echo $this->channel->itCategories;
-}
-if ($this->channel->itSubtitle)
-{
-	echo '
-	<itunes:subtitle>'.$this->channel->itSubtitle.'</itunes:subtitle>';
-}
-if (is_array($this->channel->itOwner))
-{
-	echo '
-	<itunes:owner>';
-	if($this->channel->itOwner['name'])
-	{
-		echo '
-		<itunes:name>'.$this->channel->itOwner['name'].'</itunes:name>';
-	}
-	if($this->channel->itOwner['email'])
-	{
-		echo '
-		<itunes:email>'.$this->channel->itOwner['email'].'</itunes:email>';
-	}
-	echo '
-	</itunes:owner>';
-}
-if ($this->channel->itImage)
-{
-	echo '
-	<itunes:image href="'.$this->channel->itImage.'" />
+	echo $params->get('sc_custom');
+// iTunes tags ?>
+	<itunes:summary><?php echo $this->make_xml_safe($params->get('description')); ?></itunes:summary>
+<?php if ($params->get('editor')) : ?>
+	<itunes:author><?php echo $this->make_xml_safe($params->get('editor')); ?></itunes:author>
+<?php endif;
+if ($params->get('itCategory1')) : ?>
+	<itunes:category text="<?php echo $this->make_itCat($params->get('itCategory1'));
+endif;
+if ($params->get('itCategory2')) : ?>
+	<itunes:category text="<?php echo $this->make_itCat($params->get('itCategory2'));
+endif;
+if ($params->get('itCategory3')) : ?>
+	<itunes:category text="<?php echo $this->make_itCat($params->get('itCategory3'));
+endif;
+if ($params->get('itSubtitle')) : ?>
+	<itunes:subtitle><?php echo $this->make_xml_safe($params->get('itSubtitle')); ?></itunes:subtitle>
+<?php endif;
+if ($params->get('itOwnerName') or $params->get('itOwnerEmail')) : ?>
+	<itunes:owner>
+	<?php if ($params->get('itOwnerName')) : ?>
+		<itunes:name><?php echo $params->get('itOwnerName'); ?></itunes:name>
+	<?php endif;
+	if ($params->get('itOwnerEmail')) : ?>
+		<itunes:email><?php echo $params->get('itOwnerEmail'); ?></itunes:email>
+	<?php endif; ?>
+	</itunes:owner>
+<?php endif;
+if ($params->get('itImage')) : ?>
+	<itunes:image href="<?php echo SermonspeakerHelperSermonspeaker::makeLink($params->get('itImage'), true); ?>" />
+<?php endif;
+if ($params->get('rssImage')) : ?>
 	<image>
-		<url>'.$this->channel->itImage.'</url>
-		<title>'.$this->channel->title.'</title>
-		<link>'.$this->channel->link.'</link>
-		<description>'.$this->channel->description.'</description>
-	</image>';
-}
-	echo '
-	<itunes:explicit>no</itunes:explicit>';
-if ($this->channel->itKeywords)
-{
-	echo '
-	<itunes:keywords>'.$this->channel->itKeywords.'</itunes:keywords>';
-}
-if ($this->channel->itNewfeedurl)
-{
-	echo '
-	<itunes:new-feed-url>'.$this->channel->itNewfeedurl.'</itunes:new-feed-url>';
-}
+		<url><?php echo SermonspeakerHelperSermonspeaker::makeLink($params->get('rssImage'), true); ?></url>
+		<title><?php echo $this->make_xml_safe($params->get('sc_title')); ?></title>
+		<link><?php echo JURI::root(); ?></link>
+		<description><?php echo $this->make_xml_safe($params->get('description')); ?></description>
+	</image>
+<?php endif; ?>
+	<itunes:explicit>no</itunes:explicit>
+<?php if ($params->get('itKeywords')) : ?>
+	<itunes:keywords><?php echo $this->make_xml_safe($params->get('itKeywords')); ?></itunes:keywords>
+<?php endif;
+if ($params->get('itRedirect')) : ?>
+	<itunes:new-feed-url><?php echo $this->make_xml_safe($params->get('itRedirect')); ?></itunes:new-feed-url>
+<?php endif;
 // starting with items
-foreach ($this->items as $item)
-{
-	echo '
+foreach ($this->items as $item) :
+	$item_link	= SermonspeakerHelperRoute::getSermonRoute($item->id);
+	$notes		= $this->getNotes($item->notes); ?>
 	<item>
-		<title>'.$item->title.'</title>
-		<link>'.$item->link.'</link>
-		<guid>'.$item->guid.'</guid>
-		'.$item->author.'
-		<description>'.$item->description.'</description>
-		<pubDate>'.$item->date.'</pubDate>';
-	if ($item->enclosure)
-	{
-		echo '
-		<enclosure url="'.$item->enclosure['url'].'" length="'.$item->enclosure['length'].'" type="'.$item->enclosure['type'].'"></enclosure>';
-	}
-	if ($item->itImage)
-	{
-		echo '
-		<itunes:image href="'.$item->itImage.'" />';
-	}
-	// iTunes specific: per item
-		echo '
-		<itunes:author>'.$item->itAuthor.'</itunes:author>
-		<itunes:duration>'.$item->itDuration.'</itunes:duration>
+		<title><?php echo $this->make_xml_safe($item->sermon_title); ?></title>
+		<link><?php echo $this->make_xml_safe(JURI::root().JRoute::_($item_link)); ?></link>
+		<guid><?php echo $this->make_xml_safe(JURI::root().$item_link); ?></guid>
+<?php // todo: maybe add email of speaker if present (not yet in database), format is emailadress (name) and then use author instead ?>
+		<dc:creator><?php echo $this->make_xml_safe($item->name); ?></dc:creator>
+		<description><?php echo $notes; ?></description>
+		<pubDate><?php echo JHtml::Date($item->sermon_date, 'r', true); ?></pubDate>
+<?php if ($enclosure = $this->getEnclosure($item)) : ?>
+		<enclosure url="<?php echo $enclosure['url']; ?>" length="<?php echo $enclosure['length']; ?>" type="<?php echo $enclosure['type']; ?>"></enclosure>
+<?php endif;
+// iTunes specific: per item
+if ($item->picture) : ?>
+		<itunes:image href="<?php echo SermonspeakerHelperSermonspeaker::makeLink($item->picture, true); ?>" />
+<?php elseif ($params->get('itImage')) : ?>
+		<itunes:image href="<?php echo SermonspeakerHelperSermonspeaker::makeLink($params->get('itImage'), true); ?>" />
+<?php endif; ?>
+		<itunes:author><?php echo $this->make_xml_safe($item->name); ?></itunes:author>
+		<itunes:duration><?php echo SermonspeakerHelperSermonSpeaker::insertTime($item->sermon_time); ?></itunes:duration>
 		<itunes:explicit>no</itunes:explicit>
-		<itunes:keywords>'.$item->itKeywords.'</itunes:keywords>
-		<itunes:subtitle>'.$item->itSubtitle.'</itunes:subtitle>
-		<itunes:summary>'.$item->itSummary.'</itunes:summary>
-	</item>';
-}
-echo '
+		<itunes:keywords><?php echo $this->getKeywords($item); ?></itunes:keywords>
+		<itunes:subtitle><?php echo (strlen($notes) > 255) ? mb_substr($notes, 0, 252, 'UTF-8').'...' : $notes; ?></itunes:subtitle>
+		<itunes:summary><?php echo (strlen($notes) > 4000) ? mb_substr($notes, 0, 3997, 'UTF-8').'...' : $notes; ?></itunes:summary>
+	</item>
+<?php endforeach; ?>
   </channel>
-</rss>';
+</rss>
--- SermonSpeaker4.0/com_sermonspeaker/site/views/feed/view.raw.php
+++ SermonSpeaker4.0/com_sermonspeaker/site/views/feed/view.raw.php
@@ -5,12 +5,11 @@ class SermonspeakerViewFeed extends JViewLegacy
 	function display($tpl = null)
 	{
 		$app 	= JFactory::getApplication();
-		$params	= $app->getParams();
-		$jinput	= $app->input;
+		$this->params	= $app->getParams();
 
 		// Get the log in credentials.
 		$credentials = array();
-		$credentials['username'] = $jinput->get->get('username', '', 'username');
+		$credentials['username'] = $app->input->get->get('username', '', 'username');
 		// Todo: How do I get ALLOWRAW with JInput or how does the com_users do it?
 		$credentials['password'] = JRequest::getString('password', '', 'get', JREQUEST_ALLOWRAW);
 		// Perform the log in.
@@ -18,186 +17,144 @@ class SermonspeakerViewFeed extends JViewLegacy
 			$app->login($credentials);
 		}
 		// check if access is not public
-		$user = JFactory::getUser();
+		$user	= JFactory::getUser();
 		$groups	= $user->getAuthorisedViewLevels();
-		if (!in_array($params->get('access'), $groups)) {
+		if (!in_array($this->params->get('access'), $groups)) {
 			$app->redirect('', JText::_('JERROR_ALERTNOAUTHOR'), 'error');
 		}
-		$type	= $jinput->get('type', 'auto');
-		$prio	= $params->get('fileprio', 0);
+
 		$this->document->setMimeEncoding('application/rss+xml'); 
-		$link = JURI::root();
-		// Loading Joomla Filefunctions
+
+		// Loading Joomla Filefunctions for enclosures
 		jimport('joomla.filesystem.file');
-		// Channel
-		// Save Parameters and stuff xmlsafe into $channel
-		$channel = new stdClass;
-		$channel->title 		= $this->make_xml_safe($params->get('sc_title'));
-		$channel->link 			= $link;
-		$channel->atomlink		= $this->make_xml_safe($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
-		$channel->description	= $this->make_xml_safe($params->get('description'));
-		$channel->copyright 	= $this->make_xml_safe($params->get('copyright'));
-		if ($params->get('editorEmail')){
-			$channel->manEditor 	= '<managingEditor>'.$this->make_xml_safe($params->get('editorEmail')).' ('.$params->get('editor').')</managingEditor>';
-		} else {
-			$channel->manEditor 	= '<dc:creator>'.$this->make_xml_safe($params->get('editor')).'</dc:creator>';
-		}
-		$channel->language 		= $this->make_xml_safe($params->get('itLanguage'));
-		
-		// iTunes specific tags
-		if($params->get('itOwnerEmail') != "" || $params->get('itOwnerName') != "") {
-			$channel->itOwner = array();
-			$channel->itOwner['email']	= $this->make_xml_safe($params->get('itOwnerEmail'));
-			$channel->itOwner['name'] 	= $this->make_xml_safe($params->get('itOwnerName'));
-		}
-		$channel->itKeywords 	= $this->make_xml_safe($params->get('itKeywords'));
-		$itImage = $params->get('itImage');
-		if ($itImage && !strstr($itImage, 'http://') && !strstr($itImage, 'https://')) {
-			$channel->itImage = $link.$itImage;
-		} elseif ($itImage){
-			$channel->itImage = $itImage;
-		} else {
-			$channel->itImage = '';
-		}
-		$channel->itCategories	= $this->make_itCat($params->get('itCategory1'))
-							.$this->make_itCat($params->get('itCategory2'))
-							.$this->make_itCat($params->get('itCategory3'));
-		$channel->itSubtitle	= $this->make_xml_safe($params->get('itSubtitle'));
-		$channel->itSummary		= $channel->description;
-		$channel->itAuthor		= $this->make_xml_safe($params->get('editor'));
-		$channel->itNewfeedurl	= $this->make_xml_safe($params->get('itRedirect'));
-		// Custom Code
-		$channel->custom		= $params->get('sc_custom');
+
 		// get Data from Model (/models/feed.php)
-        $rows = $this->get('Data');
-		// Items
-		$items = array();
-		foreach($rows as $row) {
-			// Trigger Event for `notes` and `scripture`
-			$scriptures	= SermonspeakerHelperSermonspeaker::insertScriptures($row->scripture, '-/*', false);
-			$row->notes		= JHtml::_('content.prepare', $row->notes);
-			$row->scripture	= JHtml::_('content.prepare', $scriptures);
-			$item = new stdClass;
-			// todo: ItemId des Predigten Menupunkts suchen und an Link anhängen, maybe use HelperRoute (check if feed will be valid then)
-			$item_link = $link.'index.php?option=com_sermonspeaker&amp;view=sermon&amp;id='.$row->id;
-			// limits notes text to x words for itDescription and RSS (if set)
-			$item_notes = str_replace(array("\r","\n",'  '), ' ', $this->make_xml_safe($row->notes));
-			$text_length = $params->get('text_length', '10');
-			$item_notes_array = explode(' ', $item_notes, $text_length + 1);
-			if (isset($item_notes_array[$text_length])) {
-				$item_notes_array[$text_length] = '...';
-			}
-			$item_notes_short = implode(' ', $item_notes_array);
-			if($params->get('limit_text')) {
-				$item->description = $item_notes_short;
-			} else {
-				$item->description = $item_notes;
-			}
-			$item->title	= $this->make_xml_safe($row->sermon_title);
-			$item->link		= $item_link; // todo: maybe make this link with JRoute to have a SEF link
-			$item->guid		= $item_link;
-			$item->date		= JHtml::Date($row->sermon_date, 'r', true);
-			$item->author 	= '<dc:creator>'.$this->make_xml_safe($row->name).'</dc:creator>'; // todo: maybe add email of speaker if present (not yet in database), format is emailadress (name) and then use author instead
-			$item->category = $this->make_xml_safe($row->series_title); // using the series title as an item category
-			// iTunes item specific tags
-			$item->itAuthor		= $this->make_xml_safe($row->name); // only speaker name here for iTunes
-			$item->itDuration 	= SermonspeakerHelperSermonSpeaker::insertTime($row->sermon_time);
-			if (strlen($item_notes_short) > 255){
-				$item->itSubtitle 	= mb_substr($item_notes_short, 0, 252, 'UTF-8').'...';
-			} else {
-				$item->itSubtitle 	= $item_notes_short;
-			}
-			if (strlen($item_notes) > 4000){
-				$item->itSummary = mb_substr($item_notes, 0, 3997, 'UTF-8').'...';
-			} else {
-				$item->itSummary = $item_notes;
-			}
-			if ($row->picture){
-				$row->picture	= trim($row->picture, '/');
-				$item->itImage	= $link.$row->picture;
-			} else {
-				$item->itImage	= $channel->itImage;
-			}
-			// create keywords from series_title and scripture (title and speaker are searchable anyway)
-			$keywords = array();
-			if($row->scripture){
-				$kw_script	= str_replace(',', ':', $row->scripture); // Make english scripture format
-				$kw_script	= str_replace("\n", '', $this->make_xml_safe($kw_script));
-				$keywords	= explode('-/*', $kw_script);
-			}
-			if($item->category){
-				$keywords[]	= $item->category;
-			}
-			$item->itKeywords 	= implode(',', $keywords);
-			// Create Enclosure
-			if (($type != 'video') && ($row->audiofile && (!$prio || ($type == 'audio') || !$row->videofile))){
-				$file = $row->audiofile;
-			} elseif (($type != 'audio') && ($row->videofile && ($prio || ($type == 'video') || !$row->audiofile))){
-				$file = $row->videofile;
-			} else {
-				$file = '';
-			}
-			if($file){
-				// MIME type for content
-				$item->enclosure['type'] = SermonspeakerhelperSermonspeaker::getMime(JFile::getExt($file));
-				if (strpos($file, 'http://') === 0) {
-					//external link
-					if ((strpos($file, 'http://vimeo.com') === 0) || (strpos($file, 'http://player.vimeo.com') === 0)){
-						// Vimeo
-						$id		= trim(strrchr($file, '/'), '/ ');
-						$file	= 'http://vimeo.com/moogaloop.swf?clip_id='.$id;
-						$item->enclosure['type'] = 'application/x-shockwave-flash';
-					}
-					$item->enclosure['url'] = $file;
-					$item->enclosure['length'] = 1;
-				} else {
-					//internal link
-					//url to play
-					$path = str_replace(array(' ', '%20'), array('%20', '%20'), $file); //fix for spaces in the filename
-					$path = trim($path, ' /');
-					$item->enclosure['url'] = $link.$path;
-					// Filesize for length
-					if(file_exists(JPATH_ROOT.$file)) {
-						$item->enclosure['length'] = filesize(JPATH_ROOT.$file);
-					} else {
-						$item->enclosure['length'] = 0;
-					}
-				}
-			} else {
-				$item->enclosure = '';
-			}
-			// Add sermonlink to the description
-			if($params->get('include_link')) {
-				$item->description = '&lt;a href=&quot;'.$item->enclosure['url'].'&quot;&gt;'.JText::_('COM_SERMONSPEAKER_DOWNLOADBUTTON_'.$type).'&lt;/a&gt;&lt;br&gt;'.$item->description;
-			}
-			// loads item info into items array
-			$items[] = $item;
-		}
-		// push data into the template
-		$this->assignRef('items',$items);
-		$this->assignRef('channel',$channel);
+        $this->items	= $this->get('Data');
+
 		parent::display($tpl);
 	}
-	
-	function make_xml_safe ($string){
-		$string = strip_tags($string);
-		$string = html_entity_decode($string, ENT_NOQUOTES, 'UTF-8');
-		$string = htmlspecialchars($string, ENT_QUOTES, 'UTF-8', FALSE);
+
+	function make_xml_safe($string)
+	{
+		$string	= strip_tags($string);
+		$string	= html_entity_decode($string, ENT_NOQUOTES, 'UTF-8');
+		$string	= htmlspecialchars($string, ENT_QUOTES, 'UTF-8', FALSE);
+
 		return $string;
 	}
-	function make_itCat ($cat){
-		if($cat == '') {
-			return '';
+
+	function make_itCat($cat)
+	{
+		$cat_array	= explode(' > ', $cat);
+		if (!isset($cat_array[1]))
+		{
+			$tags = htmlspecialchars($cat_array[0])."\" />\n";
 		}
-		$cat_array = explode(' > ', $cat);
-		if(!isset($cat_array[1])){
-			$tags = "\n".'	<itunes:category text="'.htmlspecialchars($cat_array[0]).'" />';
-		} else {
-			$tags = "\n".'	<itunes:category text="'.htmlspecialchars($cat_array[0]).'">'."\n";
-			$tags .= '		<itunes:category text="'.htmlspecialchars($cat_array[1]).'" />'."\n";
-			$tags .= '	</itunes:category>';
+		else
+		{
+			$tags = htmlspecialchars($cat_array[0])."\">\n";
+			$tags .= '		<itunes:category text="'.htmlspecialchars($cat_array[1])."\" />\n";
+			$tags .= "	</itunes:category>\n";
 		}
-		
+
 		return $tags;
+	}
+
+	function getNotes($text)
+	{
+		$text	= JHtml::_('content.prepare', $text);
+		$text	= str_replace(array("\r","\n",'  '), ' ', $this->make_xml_safe($text));
+
+		if ($this->params->get('limit_text'))
+		{
+			$length	= $this->params->get('text_length');
+			$array	= explode(' ', $text, $length + 1);
+			if (isset($array[$length]))
+			{
+				$array[$length] = '...';
+			}
+			$text = implode(' ', $array);
+		}
+
+		return $text;
+	}
+
+	function getEnclosure($item)
+	{
+		$type	= JFactory::getApplication()->input->get('type', 'auto');
+		$prio	= $this->params->get('fileprio', 0);
+
+		// Create Enclosure
+		if ($type == 'video')
+		{
+			$file	= $item->videofile;
+		}
+		elseif ($type == 'audio')
+		{
+			$file	= $item->audiofile;
+		}
+		else
+		{
+			$file	= SermonspeakerHelperSermonspeaker::getFileByPrio($item, $prio);
+		}
+		if ($file)
+		{
+			// MIME type for content
+			$enclosure['type']	= SermonspeakerHelperSermonspeaker::getMime(JFile::getExt($file));
+			if (strpos($file, 'http://') === 0)
+			{
+				//external link
+				if ((strpos($file, 'http://vimeo.com') === 0) || (strpos($file, 'http://player.vimeo.com') === 0))
+				{
+					// Vimeo
+					$id					= trim(strrchr($file, '/'), '/ ');
+					$file				= 'http://vimeo.com/moogaloop.swf?clip_id='.$id;
+					$enclosure['type']	= 'application/x-shockwave-flash';
+				}
+				$enclosure['url']		= $file;
+				$enclosure['length']	= 1;
+			}
+			else
+			{
+				//internal link
+				//url to play
+				$path	= str_replace(array(' ', '%20'), array('%20', '%20'), $file); //fix for spaces in the filename
+				$path	= trim($path, ' /');
+				$enclosure['url'] = JURI::root().$path;
+				// Filesize for length TODO: Get from database if available
+				if (file_exists(JPATH_ROOT.$file))
+				{
+					$enclosure['length'] = filesize(JPATH_ROOT.$file);
+				}
+				else
+				{
+					$enclosure['length'] = 0;
+				}
+			}
+		} else {
+			$enclosure = '';
+		}
+
+		return $enclosure;
+	}
+
+	// create keywords from series_title and scripture (title and speaker are searchable anyway)
+	function getKeywords($item)
+	{
+		$keywords = array();
+		if ($item->scripture)
+		{
+			$scripture	= SermonspeakerHelperSermonspeaker::insertScriptures($item->scripture, '-/*', false);
+			$scripture	= JHtml::_('content.prepare', $scripture);
+			$scripture	= str_replace(',', ':', $scripture); // Make english scripture format
+			$scripture	= str_replace("\n", '', $this->make_xml_safe($scripture));
+			$keywords	= explode('-/*', $scripture);
+		}
+		if ($item->series_title)
+		{
+			$keywords[]	= $this->make_xml_safe($item->series_title);
+		}
+
+		return implode(',', $keywords);
 	}
 }
