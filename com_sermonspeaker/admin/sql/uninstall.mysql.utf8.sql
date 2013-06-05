57ac67318f9df0717f0188ad5fcdbfb0ca4d9ede
Bakual <werbemails@bakual.ch>
2013-06-05 11:02:40 +0200

    Properly uninstall tags and co

--- com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
+++ com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
@@ -2,3 +2,7 @@ DROP TABLE IF EXISTS `#__sermon_speakers`;
 DROP TABLE IF EXISTS `#__sermon_series`;
 DROP TABLE IF EXISTS `#__sermon_sermons`;
 DROP TABLE IF EXISTS `#__sermon_scriptures`;
+DELETE FROM `#__ucm_content` WHERE `core_type_alias` = 'com_sermonspeaker.sermon';
+DELETE FROM `#__contentitem_tag_map` WHERE `type_alias` = 'com_sermonspeaker.sermon';
+DELETE a FROM `#__ucm_base` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.sermon';
+DELETE FROM `#__content_types` WHERE `type_alias` = 'com_sermonspeaker.sermon';
