7c687ff66f72e9ee1769aadab9e1b8cf9634d37a
Bakual <werbemails@bakual.ch>
2013-06-22 21:34:07 +0200

    Adding Soundcloud player helper class Player dropdown updates automatically from player classes Adding some playlist support. However it builds a list from the SoundCloud owner of the first item

---
+++ com_sermonspeaker/site/helpers/player/soundcloud.php
@@ -0,0 +1,64 @@
+<?php
+defined('_JEXEC') or die;
+
+require_once(JPATH_SITE.'/components/com_sermonspeaker/helpers/player.php');
+
+/**
+ * Soundcloud
+ */
+class SermonspeakerHelperPlayerSoundcloud extends SermonspeakerHelperPlayer
+{
+	public function isSupported($item)
+	{
+		if (parse_url($item, PHP_URL_HOST) == 'soundcloud.com')
+		{
+			$this->mode	= 'audio';
+		}
+		else
+		{
+			$this->mode	= false;
+		}
+		return $this->mode;
+	}
+
+	public function getName()
+	{
+		return 'Soundcloud';
+	}
+
+	public function preparePlayer($item, $config)
+	{
+		$this->config	= $config;
+		$this->player	= 'Soundcloud';
+		$this->script	= '';
+		$this->toggle	= false;
+
+		if (is_array($item))
+		{
+			// get first item and work from that
+			$first	= reset($item);
+			$link	= $first->audiofile;
+			$link	= substr($link, 0, strrpos($link, '/'));
+		}
+		else
+		{
+			$link	= $item->audiofile;
+		}
+		$this->file		= $link;
+		$this->fb_file	= $link;
+		$this->setDimensions('305', '100%');
+		$this->setPopup('a');
+		$start = $this->config['autostart'] ? 'true' : 'false';
+		$url	= 'http://soundcloud.com/oembed?format=xml&url='.$link.'&auto_play='.$start.'&maxheight='.$this->config['aheight'];
+		if ($this->config['awidth'] != '100%')
+		{
+			$url .= '&maxwidth='.$this->config['awidth'];
+		}
+		$xml	= simplexml_load_file($url);
+		$string = str_replace('<![CDATA[', '', $xml->html);
+		$string = str_replace(']]>', '', $string);
+		$this->mspace = $string;
+
+		return;
+	}
+}
