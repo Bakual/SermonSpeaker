33c17884892b4c28597013f9bdcafb4aef60847c
Bakual <werbemails@bakual.ch>
2013-11-21 15:02:55 +0100

    Add layout for edit

---
+++ com_sermonspeaker/admin/layouts/joomla/edit/global.php
@@ -0,0 +1,53 @@
+<?php
+defined('_JEXEC') or die;
+
+$form = $displayData->getForm();
+
+$fields = $displayData->get('fields') ?: array(
+	array('category', 'catid'),
+	array('parent', 'parent_id'),
+	'tags',
+	array('published', 'state', 'enabled'),
+	'featured',
+	'podcast',
+	'sticky',
+	'access',
+	'language',
+	'note',
+	'version_note'
+);
+
+$hiddenFields = $displayData->get('hidden_fields') ?: array();
+/* if (!JLanguageMultilang::isEnabled())
+{
+	$hiddenFields[] = 'language';
+} */
+if (!JComponentHelper::getParams('com_sermonspeaker')->get('save_history', 0))
+{
+	$hiddenFields[] = 'version_note';
+}
+
+$html = array();
+$html[] = '<fieldset class="form-vertical">';
+
+foreach ($fields as $field)
+{
+	$field = is_array($field) ? $field : array($field);
+	foreach ($field as $f)
+	{
+		if ($form->getField($f))
+		{
+			if (in_array($f, $hiddenFields))
+			{
+				$form->setFieldAttribute($f, 'type', 'hidden');
+			}
+
+			$html[] = $form->getControlGroup($f);
+			break;
+		}
+	}
+}
+
+$html[] = '</fieldset>';
+
+echo implode('', $html);
