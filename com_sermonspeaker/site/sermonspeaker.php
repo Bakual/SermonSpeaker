44827e6734eea34066507b5d6f6d10cdb3215e9a
Bakual <werbemails@bakual.ch>
2014-05-03 16:00:25 +0200

    phpcs

--- com_sermonspeaker/site/sermonspeaker.php
+++ com_sermonspeaker/site/sermonspeaker.php
@@ -11,15 +11,17 @@ defined('_JEXEC') or die();
 
 $jinput = JFactory::getApplication()->input;
 
-// providing backward compatibilty to older SermonSpeaker versions
-if ($jinput->get('task') == 'podcast') {
+// Providing backward compatibilty to older SermonSpeaker versions
+if ($jinput->get('task') == 'podcast')
+{
 	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: '.JURI::root().'index.php?option=com_sermonspeaker&view=feed&format=raw');
+	header('Location: ' . JURI::root() . 'index.php?option=com_sermonspeaker&view=feed&format=raw');
+
 	return;
 }
 
-require_once(JPATH_COMPONENT.'/helpers/route.php');
-require_once(JPATH_COMPONENT.'/helpers/sermonspeaker.php');
+require_once JPATH_COMPONENT . '/helpers/route.php';
+require_once JPATH_COMPONENT . '/helpers/sermonspeaker.php';
 
 // Load languages and merge with fallbacks
 $jlang = JFactory::getLanguage();
