88c392313e0b8e1c72fcdbe19597b7bb2ab4edaa
Thomas Hunziker <werbemails@bakual.ch>
2014-06-12 22:25:49 +0200

    Batchfile is now working.

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,7 @@
+REM This will generate the zipfiles for SermonSpeaker in /build/packages
+REM This needs the zip binaries from Info-Zip installed. An installer can be found http://gnuwin32.sourceforge.net/packages/zip.htm
+setlocal
+SET PATH=%PATH%;C:\Program Files (x86)\GnuWin32\bin
 rmdir /q /s packages
 mkdir packages
 REM Component
