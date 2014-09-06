a9445304ccb730f41ca19fed145342ed33a4fa80
Thomas Hunziker <werbemails@bakual.ch>
2014-09-06 14:06:26 +0200

    Removing history data on uninstall

--- com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
+++ com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
@@ -11,6 +11,9 @@ DELETE FROM `#__contentitem_tag_map` WHERE `type_alias` = 'com_sermonspeaker.ser
 DELETE a FROM `#__ucm_base` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.sermon';
 DELETE a FROM `#__ucm_base` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.speaker';
 DELETE a FROM `#__ucm_base` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.serie';
+DELETE a FROM `#__ucm_history` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.sermon';
+DELETE a FROM `#__ucm_history` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.speaker';
+DELETE a FROM `#__ucm_history` AS a INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id WHERE b.type_alias = 'com_sermonspeaker.serie';
 DELETE FROM `#__content_types` WHERE `type_alias` = 'com_sermonspeaker.sermon';
 DELETE FROM `#__content_types` WHERE `type_alias` = 'com_sermonspeaker.speaker';
 DELETE FROM `#__content_types` WHERE `type_alias` = 'com_sermonspeaker.serie';
