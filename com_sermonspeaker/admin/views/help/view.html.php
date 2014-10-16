5875eed5b16882b0da96b70fcde610781f3e1366
Bakual <werbemails@bakual.ch>
2014-10-16 15:21:06 +0200

    Adjust layout for the toggle sidebar

--- com_sermonspeaker/admin/views/help/view.html.php
+++ com_sermonspeaker/admin/views/help/view.html.php
@@ -1,8 +1,39 @@
 <?php
+/**
+ * @package     SermonSpeaker
+ * @subpackage  Component.Administrator
+ * @author      Thomas Hunziker <admin@bakual.net>
+ * @copyright   (C) 2014 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
 defined('_JEXEC') or die;
+
+/**
+ * HTML View class for the SermonSpeaker Component
+ *
+ * @since  3.4
+ */
 class SermonspeakerViewHelp extends JViewLegacy
 {
-	function display( $tpl = null )
+	/**
+	 * The HTML code for the sidebar.
+	 *
+	 * @var string
+	 */
+	protected $sidebar;
+
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a Error object.
+	 *
+	 * @see     JViewLegacy::loadTemplate()
+	 * @since   3.4
+	 */
+	public function display($tpl = null)
 	{
 		SermonspeakerHelper::addSubmenu('help');
 
@@ -11,14 +42,19 @@ class SermonspeakerViewHelp extends JViewLegacy
 
 		parent::display($tpl);
 	}
+
 	/**
 	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolbar()
 	{
-		$canDo 	= SermonspeakerHelper::getActions();
+		$canDo = SermonspeakerHelper::getActions();
 		JToolBarHelper::title(JText::_('JHELP'), 'support sermonhelp');
-		if ($canDo->get('core.admin')) {
+
+		if ($canDo->get('core.admin'))
+		{
 			JToolbarHelper::divider();
 			JToolBarHelper::preferences('com_sermonspeaker', 650, 900);
 		}
--- com_sermonspeaker/admin/views/tools/view.html.php
+++ com_sermonspeaker/admin/views/tools/view.html.php
@@ -1,40 +1,73 @@
 <?php
+/**
+ * @package     SermonSpeaker
+ * @subpackage  Component.Administrator
+ * @author      Thomas Hunziker <admin@bakual.net>
+ * @copyright   (C) 2014 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
 defined('_JEXEC') or die;
+
+/**
+ * HTML View class for the SermonSpeaker Component
+ *
+ * @since  3.4
+ */
 class SermonspeakerViewTools extends JViewLegacy
 {
-	function display( $tpl = null )
+	/**
+	 * The HTML code for the sidebar.
+	 *
+	 * @var string
+	 */
+	protected $sidebar;
+
+	/**
+	 * Execute and display a template script.
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  mixed  A string if successful, otherwise a Error object.
+	 *
+	 * @see     JViewLegacy::loadTemplate()
+	 * @since   3.4
+	 */
+	public function display($tpl = null)
 	{
 		$layout = $this->getLayout();
+
 		if ($layout !== 'time')
 		{
 			SermonspeakerHelper::addSubmenu('tools');
 		}
 
 		// Check if PreachIt is installed
-		$app = JFactory::getApplication();
-		$db		= JFactory::getDbo();
-		$query	= "SHOW TABLES";
-		$db->setQuery($query);
-		$tables	= $db->loadColumn();
-		$prefix	= $app->get('dbprefix');
-		$this->pi	= (in_array($prefix.'pistudies', $tables)) ? true : false;
+		$db = JFactory::getDbo();
+		$this->pi = in_array($db->getPrefix() . 'pistudies', $db->getTableList());
 
 		// We don't need toolbar in the modal window.
-		if ($layout !== 'time') {
+		if ($layout !== 'time')
+		{
 			$this->addToolbar();
 			$this->sidebar = JHtmlSidebar::render();
 		}
 
 		parent::display($tpl);
 	}
+
 	/**
 	 * Add the page title and toolbar.
+	 *
+	 * @return  void
 	 */
 	protected function addToolbar()
 	{
-		$canDo 	= SermonspeakerHelper::getActions();
+		$canDo = SermonspeakerHelper::getActions();
 		JToolBarHelper::title(JText::_('COM_SERMONSPEAKER_MAIN_TOOLS'), 'tools');
-		if ($canDo->get('core.admin')) {
+
+		if ($canDo->get('core.admin'))
+		{
 			JToolbarHelper::divider();
 			JToolBarHelper::preferences('com_sermonspeaker', 650, 900);
 		}
