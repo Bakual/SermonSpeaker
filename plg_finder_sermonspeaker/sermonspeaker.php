f37429d2d436b4a8c4c4aba70e835eaeb2436c99
Thomas Hunziker <werbemails@bakual.ch>
2014-11-30 19:53:20 +0100

    Fix finder plugin for state change.

--- plg_finder_sermonspeaker/sermonspeaker.php
+++ plg_finder_sermonspeaker/sermonspeaker.php
@@ -262,6 +262,32 @@ class PlgFinderSermonspeaker extends FinderIndexerAdapter
 	}
 
 	/**
+	 * Method to get a SQL query to load the published and access states for
+	 * an article and category.
+	 *
+	 * @return  JDatabaseQuery  A database object.
+	 *
+	 * @since   5.0.3
+	 */
+	protected function getStateQuery()
+	{
+		$query = $this->db->getQuery(true);
+
+		// Item ID
+		$query->select('a.id');
+
+		// Item and category published state
+		$query->select('a.' . $this->state_field . ' AS state, c.published AS cat_state');
+
+		// Item and category access levels
+		$query->select('c.access AS access, c.access AS cat_access')
+			->from($this->table . ' AS a')
+			->join('LEFT', '#__categories AS c ON c.id = a.catid');
+
+		return $query;
+	}
+
+	/**
 	 * Method to get the SQL query used to retrieve the list of content items.
 	 *
 	 * @param   mixed  $sql  A JDatabaseQuery object or null.
