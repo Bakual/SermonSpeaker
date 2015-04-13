b98e750b6af4c0e8599b54134f03e8a144bcae87
Thomas Hunziker <werbemails@bakual.ch>
2015-04-13 16:23:27 +0200

    Always import SermonSpeaker plugin group in backend

--- com_sermonspeaker/admin/sermonspeaker.php
+++ com_sermonspeaker/admin/sermonspeaker.php
@@ -2,17 +2,20 @@
 defined('_JEXEC') or die;
 
 // Access check.
-if (!JFactory::getUser()->authorise('core.manage', 'com_sermonspeaker')) 
+if (!JFactory::getUser()->authorise('core.manage', 'com_sermonspeaker'))
 {
 	throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'), 404);
 }
- 
+
 // Joomla doesn't autoload JFile and JFolder
 JLoader::register('JFile', JPATH_LIBRARIES . '/joomla/filesystem/file.php');
 JLoader::register('JFolder', JPATH_LIBRARIES . '/joomla/filesystem/folder.php');
 
 // Register Helperclass for autoloading
 JLoader::register('SermonspeakerHelper', JPATH_COMPONENT . '/helpers/sermonspeaker.php');
+
+// Import Plugins
+JPluginHelper::importPlugin('sermonspeaker');
 
 JHtml::_('behavior.tabstate');
 JHtml::stylesheet('administrator/components/com_sermonspeaker/sermonspeaker.css');
