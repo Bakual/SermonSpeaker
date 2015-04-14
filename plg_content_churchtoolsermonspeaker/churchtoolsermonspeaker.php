d4ca5e7c0569552b0846230f77a9f98ef0ec707c
Thomas Hunziker <werbemails@bakual.ch>
2015-04-14 14:19:07 +0200

    Working on churchtool plugin

--- plg_content_churchtoolsermonspeaker/churchtoolsermonspeaker.php
+++ plg_content_churchtoolsermonspeaker/churchtoolsermonspeaker.php
@@ -10,23 +10,48 @@
 defined('_JEXEC') or die();
 
 /**
- * Plug-in to prepopulate the sermon form from the churchtool software
+ * Plug-in to prepopulate the sermon form from the Churchtool software
  *
  * @since  1.0.0
  */
-class PlgSermonspeakerChurchtool extends JPlugin
+class PlgContentChurchtoolsermonspeaker extends JPlugin
 {
 	/**
-	 * Creates the player
+	 * Runs on content preparation
 	 *
-	 * @param   string  $context  The context from where it's triggered
-	 * @param   object  $data     Player object
+	 * @param   string  $context  The context for the data
+	 * @param   object  $data     An object containing the data for the form.
 	 *
 	 * @return  void
+	 *
+	 * @since   1.0
 	 */
 	public function onContentPrepareData($context, $data)
 	{
-		echo 'test';
+		// Only act for SermonSpeaker sermons
+		if ($context != 'com_sermonspeaker.sermon')
+		{
+			return;
+		}
 
+		// Only act on new sermons
+		if ($data->id)
+		{
+			return;
+		}
+
+		// Taken from https://docs.joomla.org/Connecting_to_an_external_database
+		$option = array();
+
+		$option['driver']   = $this->params->get('db_type', 'mysqli');
+		$option['host']     = $this->params->get('db_host', 'localhost');
+		$option['database'] = $this->params->get('db_database');
+		$option['user']     = $this->params->get('db_user');
+		$option['password'] = $this->params->get('db_pass');
+		$option['prefix']   = '';
+
+		$db = JDatabaseDriver::getInstance($option);
+
+		
 	}
 }
