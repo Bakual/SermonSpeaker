17d847f5b782eed7acb79272331d33eb73fa89f2
Thomas Hunziker <werbemails@bakual.ch>
2015-04-15 08:30:16 +0200

    More work

--- plg_content_churchtoolsermonspeaker/churchtoolsermonspeaker.php
+++ plg_content_churchtoolsermonspeaker/churchtoolsermonspeaker.php
@@ -52,6 +52,106 @@ class PlgContentChurchtoolsermonspeaker extends JPlugin
 
 		$db = JDatabaseDriver::getInstance($option);
 
-		
+		$date = JFactory::getDate();
+
+		// Sanitise Eventgroups
+		$eventgroups = $this->params->get('eventgroups');
+		$eg_array    = explode(',', $eventgroups);
+		$eg_array    = Joomla\Utilities\ArrayHelper::toInteger($eg_array);
+		$eventgroups = implode(',', $eg_array);
+
+		// Build Query for events
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName(array('id', 'startzeit', 'kommentar')));
+		$query->from($db->quoteName('ko_event'));
+//		$query->where($db->quoteName('startdatum') . ' = ' . $db->quote($date->format('Y-m-d')));
+		$query->where($db->quoteName('startdatum') . ' = ' . $db->quote('2015-04-12'));
+		$query->where($db->quoteName('rota') . ' = 1');
+		$query->where($db->quoteName('eventgruppen_id') . ' IN (' . $eventgroups . ')');
+		$query->order($db->quoteName('startzeit') . ' ASC');
+
+		$db->setQuery($query);
+
+		try
+		{
+			$events = $db->loadObjectList();
+		}
+		catch (Exception $e)
+		{
+			return;
+		}
+dump($events, 'events');
+		if (!$events)
+		{
+			return;
+		}
+
+		if (count($events) > 1)
+		{
+			// Current time without colons
+			$time = $date->format('Hms');
+
+			// Get latest past event
+			foreach ($events as $key => $value)
+			{
+				$starttime = (int) $value->startzeit;
+
+				if ($starttime > $time)
+				{
+					break;
+				}
+
+				$event = $value;
+			}
+		}
+		else
+		{
+			$event = $events[0];
+		}
+dump($event, 'event');
+		// Build Query for rota
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName(array('rota.schedule')));
+		$query->from($db->quoteName('ko_rota_schedulling'));
+		$query->where($db->quoteName('event_id') . ' = ' . (int) $event->id);
+		$query->where($db->quoteName('team_id') . ' = ' . (int) $this->params->get('team_id'));
+
+		$db->setQuery($query);
+
+		try
+		{
+			$schedules = $db->loadObjectList();
+		}
+		catch (Exception $e)
+		{
+			return;
+		}
+dump($schedules, 'schedules');
+		// "find_in_set" may help
+		$subquery = $db->getQuery(true);
+		$subquery->select($db->quoteName('rota.schedule'));
+		$subquery->from($db->quoteName('ko_rota_schedulling'));
+		$subquery->where($db->quoteName('event_id') . ' = ' . (int) $event->id);
+		$subquery->where($db->quoteName('team_id') . ' = ' . (int) $this->params->get('team_id'));
+
+		$query = $db->getQuery(true);
+		$query->select($db->quoteName(array('leute.nachname', 'leute.vorname')));
+		$query->from($db->quoteName('ko_leute', 'leute'));
+		$query->where($db->quoteName('leute.id') . ' IN (' . $subquery . ')');
+
+		$db->setQuery($query);
+
+		try
+		{
+			$schedules = $db->loadObjectList();
+		}
+		catch (Exception $e)
+		{
+			return;
+		}
+		dump($schedules, 'schedules');
+
+
+
 	}
 }
