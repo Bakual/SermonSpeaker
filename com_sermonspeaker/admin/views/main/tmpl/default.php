74c1e67dd5ae736fe208512b8e0cb555de121ad4
Thomas Hunziker <werbemails@bakual.ch>
2017-04-18 16:13:40 +0200

    Adjusting layouts to BS4

--- com_sermonspeaker/admin/views/main/tmpl/default.php
+++ com_sermonspeaker/admin/views/main/tmpl/default.php
@@ -1,51 +1,59 @@
 <?php
+/**
+ * @package     SermonSpeaker
+ * @subpackage  Component.Administrator
+ * @author      Thomas Hunziker <admin@sermonspeaker.net>
+ * @copyright   © 2016 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
 defined('_JEXEC') or die;
 ?>
-<div id="j-main-container">
-	<ul class="thumbnails">
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=series">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/series.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_SERIES_TITLE'); ?></h3>
+<div class="row-fluid">
+	<div id="j-main-container">
+		<div class="card-deck">
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=series">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/series.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_SERIES_TITLE'); ?></h3>
+				</div>
 			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=speakers">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/speakers.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_SPEAKERS_TITLE'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=speakers">
+				<div class="card-block">
+					<img class="card-text" src="<?php echo JUri::base()."components/com_sermonspeaker/images/speakers.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_SPEAKERS_TITLE'); ?></h3>
+				</div>
 			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=sermons">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/sermon.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_SERMONS_TITLE'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=sermons">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/sermon.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_SERMONS_TITLE'); ?></h3>
+				</div>
 			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_categories&extension=com_sermonspeaker">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/category.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('JCATEGORIES'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_categories&extension=com_sermonspeaker">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/category.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('JCATEGORIES'); ?></h3>
+				</div>
 			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=tools">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/tools.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_MAIN_TOOLS'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=tools">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/tools.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_MAIN_TOOLS'); ?></h3>
+				</div>
 			</a>
-		</li>
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=languages">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/globe.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_MAIN_LANGUAGES'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=languages">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/globe.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_MAIN_LANGUAGES'); ?></h3>
+				</div>
 			</a>
-		</li>
-	</ul>
-	<ul class="thumbnails">
-		<li class="span2">
-			<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=help">
-				<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/help.png"; ?>"/>
-				<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_MENU_HELP'); ?></h3>
+			<a class="card text-center" href="index.php?option=com_sermonspeaker&view=help">
+				<div class="card-block">
+					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/help.png"; ?>">
+					<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_MENU_HELP'); ?></h3>
+				</div>
 			</a>
-		</li>
-	</ul>
+		</div>
+	</div>
 </div>
--- com_sermonspeaker/admin/views/tools/tmpl/default.php
+++ com_sermonspeaker/admin/views/tools/tmpl/default.php
@@ -10,74 +10,94 @@
 defined('_JEXEC') or die;
 
 JHtml::_('behavior.modal');
+JHtml::_('bootstrap.tooltip');
 
-$session = JFactory::getSession();
+$session = JFactory::getApplication()->getSession();
 $user    = JFactory::getUser();
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_sermonspeaker&view=tools'); ?>" method="post" name="adminForm" id="adminForm">
-	<?php if(!empty($this->sidebar)) : ?>
-		<div id="j-sidebar-container" class="span2">
+	<div class="row">
+		<div id="j-sidebar-container" class="col-md-2">
 			<?php echo $this->sidebar; ?>
 		</div>
-		<div id="j-main-container" class="span10">
-	<?php else : ?>
-		<div id="j-main-container">
-	<?php endif;?>
-		<ul class="thumbnails">
-			<li class="span2" rel="tooltip" data-placement="right" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3_DESC'); ?>">
-				<a class="thumbnail" href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName().'='.$session->getId().'&'.JSession::getFormToken(); ?>=1">
-					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/download.png"; ?>"/>
-					<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></h3>
-				</a>
-			</li>
-			<li class="span2" rel="tooltip" data-placement="right" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME_DESC'); ?>">
-				<a class="modal thumbnail" href="index.php?option=com_sermonspeaker&view=tools&layout=time&tmpl=component" rel="{handler: 'iframe', size: {x: 450, y: 170}}" >
-					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/clock.png"; ?>"/>
-					<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?></h3>
-				</a>
-			</li>
-			<li class="span2" rel="tooltip" data-placement="right" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?>">
-				<a class="modal thumbnail" href="index.php?option=com_sermonspeaker&view=files&layout=modal&tmpl=component" rel="{handler: 'iframe', size: {x: 700, y: 500}}">
-					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/find.png"; ?>"/>
-					<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND'); ?></h3>
-				</a>
-			</li>
-			<?php // Check Access
-			if (!$user->authorise('com_sermonspeaker.script', 'com_sermonspeaker')):
-				$link = 'href="#" onclick="alert(\''.JText::_('JERROR_ALERTNOAUTHOR').'\')"';
-				$class = ' disabled';
-			else:
-				$link = 'href="index.php?option=com_sermonspeaker&task=tools.createAutomatic"';
-				$class = '';
-			endif; ?>
-			<li class="span2<?php echo $class; ?>" rel="tooltip" data-placement="left" title="<?php echo JText::sprintf('COM_SERMONSPEAKER_TOOLS_AUTOMATIC_DESC', JUri::root()); ?>">
-				<a class="thumbnail" <?php echo $link; ?>>
-					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/run.png"; ?>"/>
-					<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?></h3>
-				</a>
-			</li>
-			<?php if($this->pi) : ?>
-				<li class="span2" rel="tooltip" data-placement="left" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_DESC'); ?>">
-					<a class="thumbnail" href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . JSession::getFormToken(); ?>=1">
-						<img src="<?php echo JUri::base() . "components/com_sermonspeaker/images/import.png"; ?>"/>
-						<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?></h3>
+		<div class="col-md-10">
+			<div id="j-main-container">
+				<div class="card-deck">
+					<a class="card text-center hasTooltip"
+						href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName().'='.$session->getId().'&'.JSession::getFormToken(); ?>=1"
+						title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3_DESC'); ?>"
+					>
+						<div class="card-block">
+							<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/download.png"; ?>">
+							<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></h3>
+						</div>
 					</a>
-				</li>
-			<?php endif; ?>
-			<?php if($this->bs) : ?>
-				<li class="span2" rel="tooltip" data-placement="left" title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS_DESC'); ?>">
-					<a class="thumbnail" href="index.php?option=com_sermonspeaker&task=tools.bsimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . JSession::getFormToken(); ?>=1">
-						<img src="<?php echo JUri::base() . "components/com_sermonspeaker/images/import.png"; ?>"/>
-						<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS'); ?></h3>
+					<a class="card text-center hasTooltip"
+						href="index.php?option=com_sermonspeaker&view=tools&layout=time&tmpl=component"
+						title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME_DESC'); ?>"
+					>
+						<div class="card-block">
+							<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/clock.png"; ?>"/>
+							<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?></h3>
+						</div>
 					</a>
-				</li>
-			<?php endif; ?>
-			<li class="span2">
-				<a class="thumbnail" href="index.php?option=com_sermonspeaker&view=statistics&format=raw">
-					<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/stats.png"; ?>"/>
-					<h3 class="center"><?php echo JText::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?></h3>
-				</a>
-			</li>
-		</ul>
+					<a class="card text-center hasTooltip"
+						href="index.php?option=com_sermonspeaker&view=files&layout=time&tmpl=component"
+						title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?>"
+					>
+						<div class="card-block">
+							<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/find.png"; ?>"/>
+							<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_FIND'); ?></h3>
+						</div>
+					</a>
+					<?php // Check Access
+					if (!$user->authorise('com_sermonspeaker.script', 'com_sermonspeaker')):
+						$link = 'href="#" onclick="alert(\''.JText::_('JERROR_ALERTNOAUTHOR').'\')"';
+						$class = ' disabled';
+					else:
+						$link = 'href="index.php?option=com_sermonspeaker&task=tools.createAutomatic"';
+						$class = '';
+					endif; ?>
+					<a class="card text-center hasTooltip<?php echo $class; ?>"
+						<?php echo $link; ?>
+						data-placement="left"
+						title="<?php echo JText::sprintf('COM_SERMONSPEAKER_TOOLS_AUTOMATIC_DESC', JUri::root()); ?>"
+					>
+						<div class="card-block">
+							<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/run.png"; ?>"/>
+							<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?></h3>
+						</div>
+					</a>
+					<?php if ($this->pi) : ?>
+						<a class="card text-center hasTooltip"
+							href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . JSession::getFormToken(); ?>=1"
+							title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_DESC'); ?>"
+						>
+							<div class="card-block">
+								<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/import.png"; ?>"/>
+								<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?></h3>
+							</div>
+						</a>
+					<?php endif; ?>
+					<?php if ($this->bs) : ?>
+						<a class="card text-center hasTooltip"
+							href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . JSession::getFormToken(); ?>=1"
+							title="<?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS_DESC'); ?>"
+						>
+							<div class="card-block">
+								<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/import.png"; ?>"/>
+								<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS'); ?></h3>
+							</div>
+						</a>
+					<?php endif; ?>
+					<a class="card text-center" href="index.php?option=com_sermonspeaker&view=statistics&format=raw">
+						<div class="card-block">
+							<img src="<?php echo JUri::base()."components/com_sermonspeaker/images/stats.png"; ?>"/>
+							<h3 class="card-title"><?php echo JText::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?></h3>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </form>
