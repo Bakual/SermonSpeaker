52d7c38c187cc2cb33623c32161593a75fa87395
Thomas Hunziker <werbemails@bakual.ch>
2017-05-08 23:55:25 +0200

    More adjusting to J4 layouts

--- com_sermonspeaker/admin/views/help/tmpl/default.php
+++ com_sermonspeaker/admin/views/help/tmpl/default.php
@@ -10,36 +10,51 @@
 defined('_JEXEC') or die;
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_sermonspeaker&view=help'); ?>" method="post" name="adminForm" id="adminForm">
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-		<h1>SermonSpeaker <?php echo $this->version; ?></h1>
-		<h2>Support</h2>
-		<div>If you need help with SermonSpeaker, the following links may be helpful for you:
-			<ul>
-				<li><a href="http://www.sermonspeaker.net/documentation.html">Documentation</a></li>
-				<li><a href="http://www.sermonspeaker.net/faq.html">FAQ</a></li>
-				<li><a href="http://www.sermonspeaker.net/forum.html">Forums</a></li>
-			</ul>
+	<div class="row">
+		<div id="j-sidebar-container" class="col-md-2">
+			<?php echo $this->sidebar; ?>
 		</div>
-		<h2>License</h2>
-		<div>SermonSpeaker is released under the <a href="http://www.gnu.org/licenses/gpl.html">GNU/GPL license</a></div>
-		<div>Please note that the included flash players have their own licenses:
-			<ul>
-				<li><a href="http://www.longtailvideo.com/players/">JW Player</a> from <a href="http://www.longtailvideo.com/">LongTail Video</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike</a> and thus free for non-commercial use. Commercial licenses are available.</li>
-				<li><a href="http://flowplayer.org/">FlowPlayer</a> is licensed under <a href="http://flowplayer.org/download/license_gpl.htm">GPL</a> and thus free for use. The copyright logo can be removed with a commercial license.</li>
-				<li><a href="http://wpaudioplayer.com/">WordPress Audio Player</a> from <a href="http://www.1pixelout.net/">1 Pixel Out</a> is licensed under <a href="http://wpaudioplayer.com/license/">MIT</a> and thus free for use. When using keep in mind that it only supports mp3 audiofiles.</li>
-			</ul>
-		</div>
-		<h2>Classes</h2>
-		<div>SermonSpeaker includes the following extern classes besides the players mentioned above:
-			<ul>
-				<li><a href="http://http://www.plupload.com/">Plupload</a> for the Uploader.</li>
-				<li><a href="http://getid3.sourceforge.net">GetID3</a> for the ID3 tags lookup.</li>
-				<li><a href="http://undesigned.org.za/2007/10/22/amazon-s3-php-class">Amazon S3 PHP Class</a> for the Amazon S3 support.</li>
-				<li><a href="https://github.com/eduardocereto/GA-Code-Samples">Google Analytics Code Samples</a> from <a href="http://www.cardinalpath.com">Cardinal Path</a> for the Vimeo GA support.</li>
-			</ul>
+		<div class="col-md-10">
+			<div id="j-main-container" class="j-main-container">
+				<div class="card">
+					<div class="card-header"><h1>SermonSpeaker <?php echo $this->version; ?></h1></div>
+					<div class="card-block">
+						<h2 class="card-title">Support</h2>
+						<p class="card-text">If you need help with SermonSpeaker, the following links may be helpful for you:</p>
+						<a href="http://www.sermonspeaker.net/documentation.html" class="card-link">Documentation</a>
+						<a href="http://www.sermonspeaker.net/faq.html" class="card-link">FAQ</a>
+						<a href="http://www.sermonspeaker.net/forum.html" class="card-link">Forums</a>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-block">
+						<h2 class="card-title">License</h2>
+						<p class="card-text">
+							SermonSpeaker is released under the <a href="http://www.gnu.org/licenses/gpl.html">GNU/GPL license</a><br>
+							Please note that the included flash players have their own licenses:
+							<ul>
+								<li><a href="http://www.longtailvideo.com/players/">JW Player</a> from <a href="http://www.longtailvideo.com/">LongTail Video</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike</a> and thus free for non-commercial use. Commercial licenses are available.</li>
+								<li><a href="http://flowplayer.org/">FlowPlayer</a> is licensed under <a href="http://flowplayer.org/download/license_gpl.htm">GPL</a> and thus free for use. The copyright logo can be removed with a commercial license.</li>
+								<li><a href="http://wpaudioplayer.com/">WordPress Audio Player</a> from <a href="http://www.1pixelout.net/">1 Pixel Out</a> is licensed under <a href="http://wpaudioplayer.com/license/">MIT</a> and thus free for use. When using keep in mind that it only supports mp3 audiofiles.</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+				<div class="card">
+					<div class="card-block">
+						<h2 class="card-title">Classes</h2>
+						<p class="card-text">
+							SermonSpeaker includes the following extern classes besides the players mentioned above:
+							<ul>
+								<li><a href="http://http://www.plupload.com/">Plupload</a> for the Uploader.</li>
+								<li><a href="http://getid3.sourceforge.net">GetID3</a> for the ID3 tags lookup.</li>
+								<li><a href="http://undesigned.org.za/2007/10/22/amazon-s3-php-class">Amazon S3 PHP Class</a> for the Amazon S3 support.</li>
+								<li><a href="https://github.com/eduardocereto/GA-Code-Samples">Google Analytics Code Samples</a> from <a href="http://www.cardinalpath.com">Cardinal Path</a> for the Vimeo GA support.</li>
+							</ul>
+						</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </form>
--- com_sermonspeaker/admin/views/languages/tmpl/default.php
+++ com_sermonspeaker/admin/views/languages/tmpl/default.php
@@ -1,86 +1,96 @@
 <?php
 defined('_JEXEC') or die;
+
+JHtml::_('bootstrap.tooltip');
 ?>
 <form enctype="multipart/form-data"  action="<?php echo JRoute::_('index.php?option=com_installer&view=install'); ?>" method="post" name="adminForm" id="adminForm">
-<?php if(!empty($this->sidebar)): ?>
-	<div id="j-sidebar-container" class="span2">
-		<?php echo $this->sidebar; ?>
-	</div>
-	<div id="j-main-container" class="span10">
-<?php else : ?>
-	<div id="j-main-container">
-<?php endif;?>
-<?php if ($this->xml) : ?>
-		<div class="well">
-			<h3><?php echo $this->xml->title; ?></h3>
-			<?php echo (string)$this->xml->description; ?>
+	<div class="row">
+		<div id="j-sidebar-container" class="col-md-2">
+			<?php echo $this->sidebar; ?>
 		</div>
-		<table class="table">
-			<?php foreach ($this->xml->language as $language) :
-				$class = 'btn';
-				if (isset($this->installed[$this->xml->extension_name.'-'.$language->lang_name])) :
-					// language pack is installed
-					$creationDate	= $this->installed[$this->xml->extension_name.'-'.$language->lang_name]->creationDate;
-					if (strtotime($language->created) > strtotime($creationDate)) :
-						$class .= ' btn-warning';
-						$text	= JText::_($this->prefix.'_NEWER_LANGUAGEPACK_THAN_INSTALLED');
-					else :
-						$class .= ' btn-success';
-						$text	= JText::_($this->prefix.'_NEWEST_LANGUAGE_INSTALLED');
-					endif;
-				elseif (isset($this->languages[str_replace('_', '-', $language->lang_name)])) :
-					// site language is installed
-					if (strtotime($language->created) > strtotime($this->manifest['creationDate'])) :
-						$class .= ' btn-primary';
-						$text	= JText::_($this->prefix.'_NEWER_LANGUAGEPACK_THAN_EXTENSION');
-					else :
-						$class .= ' btn-info';
-						$text	= JText::_($this->prefix.'_NEWEST_LANGUAGE_INSTALLED');
-					endif;
-				else :
-					// language pack available
-					$text	= JText::_($this->prefix.'_SITELANGUAGE_NOT_INSTALLED');
-				endif; ?>
-				<tr>
-					<td>
-						<input type="button" class="<?php echo $class; ?>" value="<?php echo JText::_($this->prefix.'_INSTALL_LANGUAGEPACK'); ?>" onclick="document.getElementById('install_url').value = '<?php echo $language->link; ?>'; Joomla.submitbutton();" title="<?php echo $text; ?>" />
-						<a href="<?php echo $language->link; ?>" title="<?php echo JText::sprintf($this->prefix.'_DOWNLOAD_LANGUAGEPACK', $this->site); ?>">
-							<?php if (isset($language->iso_lang_name)) : ?>
-							<?php echo $language->iso_lang_name; ?>
-							<?php if (isset($language->iso_country_name) && $language->iso_country_name != '') : ?>
-								(<?php echo $language->iso_country_name; ?>)
-							<?php endif; ?>
+		<div class="col-md-10">
+			<div id="j-main-container" class="j-main-container">
+				<?php if (!$this->xml) : ?>
+					<div class="card card-info">
+						<div class="card-block">
+							<h4 class="card-title"><?php echo JText::_('NOTICE'); ?></h4>
+							<p class="card-text"><?php echo JText::_('COM_SERMONSPEAKER_XML_ERROR'); ?></p>
+							<a href="http://www.sermonspeaker.net/download/language-packs.html" class="btn btn-primary" target="_blank">SermonSpeaker.net</a>
+						</div>
+					</div>
+				<?php else : ?>
+					<div class="card">
+						<div class="card-block">
+							<h3 class="card-title"><?php echo $this->xml->title; ?></h3>
+							<p class="card-text"><?php echo (string)$this->xml->description; ?></p>
+						</div>
+					</div>
+					<table class="table">
+						<?php foreach ($this->xml->language as $language) : ?>
+							<?php $class = 'btn'; ?>
+							<?php if (isset($this->installed[$this->xml->extension_name . '-' . $language->lang_name])) : ?>
+								<?php // Language pack is installed ?>
+								<?php $creationDate = $this->installed[$this->xml->extension_name . '-' . $language->lang_name]->creationDate; ?>
+								<?php if (strtotime($language->created) > strtotime($creationDate)) : ?>
+									<?php $class .= ' btn-warning'; ?>
+									<?php $text = JText::_($this->prefix . '_NEWER_LANGUAGEPACK_THAN_INSTALLED'); ?>
+								<?php else : ?>
+									<?php $class .= ' btn-success'; ?>
+									<?php $text = JText::_($this->prefix . '_NEWEST_LANGUAGE_INSTALLED'); ?>
+								<?php endif; ?>
+							<?php elseif (isset($this->languages[str_replace('_', '-', $language->lang_name)])) : ?>
+								<?php // Site language is installed ?>
+								<?php if (strtotime($language->created) > strtotime($this->manifest['creationDate'])) : ?>
+									<?php $class .= ' btn-primary'; ?>
+									<?php $text = JText::_($this->prefix . '_NEWER_LANGUAGEPACK_THAN_EXTENSION'); ?>
+								<?php else : ?>
+									<?php $class .= ' btn-info'; ?>
+									<?php $text = JText::_($this->prefix . '_NEWEST_LANGUAGE_INSTALLED'); ?>
+								<?php endif; ?>
 							<?php else : ?>
-								<?php echo $language->lang_name; ?>
+								<?php // Language pack available ?>
+								<?php $text = JText::_($this->prefix . '_SITELANGUAGE_NOT_INSTALLED'); ?>
 							<?php endif; ?>
-						</a>
-					</td>
-					<td width="40%">
-						<div class="progress progress-striped" style="margin-bottom: 0;">
-							<div class="bar" style="width: <?php echo $language->completed; ?>%;"></div>
-						</div>
-					</td>
-					<td>
-						<?php echo $language->completed; ?>%
-					</td>
-					<td>
-						<div>
-							<?php echo JHtml::date($language->created, JText::_('DATE_FORMAT_LC4')); ?>
-							<?php if((string)$this->xml->contribute && $language->completed != 100) : ?>
-								<a href="http://transifex.com/projects/p/<?php echo $this->xml->transifex_slug; ?>/language/<?php echo $language->lang_name; ?>" class="btn" target="_blank">
-									<?php echo JText::_($this->prefix.'_CONTRIBUTE_NOW'); ?>
-								</a>
-							<?php endif; ?>
-						</div>
-					</td>
-				</tr>
-			<?php endforeach; ?>
-		</table>
-<?php else : ?>
-		<div class="well">
-			<?php echo JText::_($this->prefix.'_XML_ERROR'); ?>
+							<tr>
+								<td>
+									<input type="button" class="hasTooltip <?php echo $class; ?>" value="<?php echo JText::_($this->prefix.'_INSTALL_LANGUAGEPACK'); ?>" onclick="document.getElementById('install_url').value = '<?php echo $language->link; ?>'; Joomla.submitbutton();" title="<?php echo $text; ?>" />
+									<a href="<?php echo $language->link; ?>" title="<?php echo JText::sprintf($this->prefix.'_DOWNLOAD_LANGUAGEPACK', $this->site); ?>">
+										<?php if (isset($language->iso_lang_name)) : ?>
+											<?php echo $language->iso_lang_name; ?>
+											<?php if (isset($language->iso_country_name) && $language->iso_country_name != '') : ?>
+												(<?php echo $language->iso_country_name; ?>)
+											<?php endif; ?>
+										<?php else : ?>
+											<?php echo $language->lang_name; ?>
+										<?php endif; ?>
+									</a>
+								</td>
+								<td width="40%">
+									<div class="progress">
+										<div class="progress-bar progress-bar-striped" role="progressbar"
+											 style="width: <?php echo $language->completed; ?>%"
+											 aria-valuenow="<?php echo $language->completed; ?>"
+											 aria-valuemin="0" aria-valuemax="100">
+												<?php echo $language->completed; ?>%
+										</div>
+									</div>
+								</td>
+								<td>
+									<div>
+										<?php echo JHtml::date($language->created, JText::_('DATE_FORMAT_LC4')); ?>
+										<?php if ((string) $this->xml->contribute && $language->completed != 100) : ?>
+											<a href="http://transifex.com/projects/p/<?php echo $this->xml->transifex_slug; ?>/language/<?php echo $language->lang_name; ?>" class="btn btn-secondary" target="_blank">
+												<?php echo JText::_($this->prefix.'_CONTRIBUTE_NOW'); ?>
+											</a>
+										<?php endif; ?>
+									</div>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					</table>
+				<?php endif; ?>
+			</div>
 		</div>
-<?php endif; ?>
 	</div>
 	<input type="hidden" id="install_url" name="install_url" />
 	<input type="hidden" name="installtype" value="url" />
