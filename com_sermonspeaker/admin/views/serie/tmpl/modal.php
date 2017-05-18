4fbc2f05be133f371fc0858c8ad879db4e77225e
Thomas Hunziker <werbemails@bakual.ch>
2017-05-18 22:54:14 +0200

    Adjust serie/speaker modal forms to new way. Breaks the modals in the sermon form

--- com_sermonspeaker/admin/views/serie/tmpl/modal.php
+++ com_sermonspeaker/admin/views/serie/tmpl/modal.php
@@ -1,201 +1,33 @@
 <?php
-// no direct access
+/**
+ * @package     SermonSpeaker
+ * @subpackage  Component.Administrator
+ * @author      Thomas Hunziker <admin@sermonspeaker.net>
+ * @copyright   © 2016 - Thomas Hunziker
+ * @license     http://www.gnu.org/licenses/gpl.html
+ **/
+
 defined('_JEXEC') or die;
 
-JHtml::_('bootstrap.tooltip');
-JHtml::_('behavior.formvalidation');
-JHtml::_('behavior.keepalive');
-JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('bootstrap.tooltip', '.hasTooltip', array('placement' => 'bottom'));
 
-$input = JFactory::getApplication()->input;
-?>
-<script type="text/javascript">
-	Joomla.submitbutton = function(task) {
-		if (task == 'serie.cancel' || document.formvalidator.isValid(document.id('adminForm'))) {
-			<?php echo $this->form->getField('series_description')->save(); ?>
-			Joomla.submitform(task, document.getElementById('adminForm'));
-		} else {
-			alert('<?php echo $this->escape(JText::_('JGLOBAL_VALIDATION_FORM_FAILED'));?>');
+// @deprecated 4.0 the function parameter, the inline js and the buttons are not needed since 3.7.0.
+$function  = JFactory::getApplication()->input->getCmd('function', 'jEditSerie_' . (int) $this->item->id);
+
+// Function to update input title when changed
+JFactory::getDocument()->addScriptDeclaration('
+	function jEditSerieModal() {
+		if (window.parent && document.formvalidator.isValid(document.getElementById("item-form"))) {
+			return window.parent.' . $this->escape($function) . '(document.getElementById("jform_title").value);
 		}
 	}
-</script>
+');
+?>
+<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.apply'); jEditSerieModal();"></button>
+<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.save'); jEditSerieModal();"></button>
+<button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('article.cancel');"></button>
 
-<form action="<?php echo JRoute::_('index.php?option=com_sermonspeaker&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-	<div class="well">
-		<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('serie.save');">
-			<?php echo JText::_('JSAVE') ?>
-		</button>
-		<button type="button" class="btn" onclick="window.parent.SqueezeBox.close();">
-			<?php echo JText::_('JCANCEL') ?>
-		</button>
-	</div>
-	<div class="row-fluid">
-		<!-- Begin Content -->
-		<div class="span10 form-horizontal">
-			<ul class="nav nav-tabs">
-				<li class="active"><a href="#general" data-toggle="tab"><?php echo JText::_('JDETAILS');?></a></li>
-				<li><a href="#publishing" data-toggle="tab"><?php echo JText::_('JGLOBAL_FIELDSET_PUBLISHING');?></a></li>
-				<li><a href="#metadata" data-toggle="tab"><?php echo JText::_('JGLOBAL_FIELDSET_METADATA_OPTIONS');?></a></li>
-			</ul>
-
-			<div class="tab-content">
-				<!-- Begin Tabs -->
-				<div class="tab-pane active" id="general">
-					<fieldset class="adminform">
-						<div class="control-group form-inline">
-							<?php echo $this->form->getLabel('title'); ?> <?php echo $this->form->getInput('title'); ?> <?php echo $this->form->getLabel('catid'); ?> <?php echo $this->form->getInput('catid'); ?>
-						</div>
-						<?php echo $this->form->getInput('series_description'); ?>
-					</fieldset>
-					<div class="row-fluid">
-						<div class="span6">
-							<h4><?php echo JText::_('JDETAILS');?></h4>
-							<?php foreach($this->form->getFieldset('detail') as $field): ?>
-								<div class="control-group">
-									<?php if (!$field->hidden): ?>
-										<div class="control-label">
-											<?php echo $field->label; ?>
-										</div>
-									<?php endif; ?>
-									<div class="controls">
-										<?php echo $field->input; ?>
-									</div>
-								</div>
-							<?php endforeach; ?>
-						</div>
-					</div>
-				</div>
-
-				<div class="tab-pane" id="publishing">
-					<div class="row-fluid">
-						<div class="span6">
-							<div class="control-group">
-								<div class="control-label">
-									<?php echo $this->form->getLabel('alias'); ?>
-								</div>
-								<div class="controls">
-									<?php echo $this->form->getInput('alias'); ?>
-								</div>
-							</div>
-							<div class="control-group">
-								<div class="control-label">
-									<?php echo $this->form->getLabel('id'); ?>
-								</div>
-								<div class="controls">
-									<?php echo $this->form->getInput('id'); ?>
-								</div>
-							</div>
-							<div class="control-group">
-								<div class="control-label">
-									<?php echo $this->form->getLabel('created_by'); ?>
-								</div>
-								<div class="controls">
-									<?php echo $this->form->getInput('created_by'); ?>
-								</div>
-							</div>
-							<div class="control-group">
-								<div class="control-label">
-									<?php echo $this->form->getLabel('created'); ?>
-								</div>
-								<div class="controls">
-									<?php echo $this->form->getInput('created'); ?>
-								</div>
-							</div>
-						</div>
-						<div class="span6">
-							<?php if ($this->item->modified_by) : ?>
-								<div class="control-group">
-									<div class="control-label">
-										<?php echo $this->form->getLabel('modified_by'); ?>
-									</div>
-									<div class="controls">
-										<?php echo $this->form->getInput('modified_by'); ?>
-									</div>
-								</div>
-								<div class="control-group">
-									<div class="control-label">
-										<?php echo $this->form->getLabel('modified'); ?>
-									</div>
-									<div class="controls">
-										<?php echo $this->form->getInput('modified'); ?>
-									</div>
-								</div>
-							<?php endif;
-							if ($this->item->version) : ?>
-								<div class="control-group">
-									<div class="control-label">
-										<?php echo $this->form->getLabel('version'); ?>
-									</div>
-									<div class="controls">
-										<?php echo $this->form->getInput('version'); ?>
-									</div>
-								</div>
-							<?php endif;
-							if ($this->item->hits) : ?>
-								<div class="control-group">
-									<div class="control-label">
-										<?php echo $this->form->getLabel('hits'); ?>
-									</div>
-									<div class="controls">
-										<?php echo $this->form->getInput('hits'); ?>
-									</div>
-								</div>
-							<?php endif; ?>
-						</div>
-					</div>
-				</div>
-				<div class="tab-pane" id="metadata">
-					<fieldset>
-						<?php foreach($this->form->getFieldset('metadata') as $field): ?>
-							<div class="control-group">
-								<?php if (!$field->hidden): ?>
-									<div class="control-label">
-										<?php echo $field->label; ?>
-									</div>
-								<?php endif; ?>
-								<div class="controls">
-									<?php echo $field->input; ?>
-								</div>
-							</div>
-						<?php endforeach; ?>
-					</fieldset>
-				</div>
-			</div>
-			<input type="hidden" name="task" value="" />
-			<input type="hidden" name="return" value="<?php echo $input->getCmd('return');?>" />
-			<?php echo JHtml::_('form.token'); ?>
-		</div>
-		<!-- End Content -->
-		<!-- Begin Sidebar -->
-		<div class="span2">
-			<h4><?php echo JText::_('JDETAILS');?></h4>
-			<hr />
-			<fieldset class="form-vertical">
-				<div class="control-group">
-					<div class="controls">
-						<?php echo $this->form->getValue('title'); ?>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('state'); ?>
-					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('state'); ?>
-					</div>
-				</div>
-
-				<div class="control-group">
-					<div class="control-label">
-						<?php echo $this->form->getLabel('language'); ?>
-					</div>
-					<div class="controls">
-						<?php echo $this->form->getInput('language'); ?>
-					</div>
-				</div>
-			</fieldset>
-		</div>
-		<!-- End Sidebar -->
-	</div>
-</form>
+<div class="container-popup">
+	<?php $this->setLayout('edit'); ?>
+	<?php echo $this->loadTemplate(); ?>
+</div>
