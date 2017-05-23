7645a06aaff94e9bb0d7946d2099827bc372e047
Thomas Hunziker <werbemails@bakual.ch>
2017-05-23 16:20:13 +0200

    remove deprecated JS

--- com_sermonspeaker/admin/views/sermon/tmpl/modal.php
+++ com_sermonspeaker/admin/views/sermon/tmpl/modal.php
@@ -9,22 +9,9 @@
 
 defined('_JEXEC') or die;
 
-JHtml::_('bootstrap.tooltip', '.hasTooltip', array('placement' => 'bottom'));
-
-// @deprecated 4.0 the function parameter, the inline js and the buttons are not needed since 3.7.0.
-$function  = JFactory::getApplication()->input->getCmd('function', 'jEditSermon_' . (int) $this->item->id);
-
-// Function to update input title when changed
-JFactory::getDocument()->addScriptDeclaration('
-	function jEditSermonModal() {
-		if (window.parent && document.formvalidator.isValid(document.getElementById("item-form"))) {
-			return window.parent.' . $this->escape($function) . '(document.getElementById("jform_title").value);
-		}
-	}
-');
 ?>
-<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('sermon.apply'); jEditSermonModal();"></button>
-<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('sermon.save'); jEditSermonModal();"></button>
+<button id="applyBtn" type="button" class="hidden" onclick="Joomla.submitbutton('sermon.apply');"></button>
+<button id="saveBtn" type="button" class="hidden" onclick="Joomla.submitbutton('sermon.save');"></button>
 <button id="closeBtn" type="button" class="hidden" onclick="Joomla.submitbutton('sermon.cancel');"></button>
 
 <div class="container-popup">
