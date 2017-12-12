ec9e41fd82672dbb1682f0ed33297530ce613205
Thomas Hunziker <werbemails@bakual.ch>
2017-12-12 16:27:24 +0100

    Specific category classes in helper

--- com_sermonspeaker/site/helpers/category.php
+++ com_sermonspeaker/site/helpers/category.php
@@ -9,19 +9,21 @@
 
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Categories\Categories;
+
 /**
- * SermonSpeaker Component Category Tree
+ * SermonSpeaker Component Sermons Category Tree
  *
- * @since  5
+ * @since  6.0.0
  */
-class SermonspeakerCategories extends JCategories
+class SermonspeakerSermonsCategories extends Categories
 {
 	/**
 	 * Constructor
 	 *
 	 * @param   array $options Obtions
 	 *
-	 * @since ?
+	 * @since 6.0.0
 	 */
 	public function __construct($options = array())
 	{
@@ -30,7 +32,61 @@ class SermonspeakerCategories extends JCategories
 			$options['table'] = '#__sermon_sermons';
 		}
 
-		$options['extension'] = 'com_sermonspeaker';
+		$options['extension'] = 'com_sermonspeaker.sermons';
+
+		parent::__construct($options);
+	}
+}
+
+/**
+ * SermonSpeaker Component Series Category Tree
+ *
+ * @since  6.0.0
+ */
+class SermonspeakerSeriesCategories extends Categories
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   array $options Obtions
+	 *
+	 * @since 6.0.0
+	 */
+	public function __construct($options = array())
+	{
+		if (!isset($options['table']))
+		{
+			$options['table'] = '#__sermon_series';
+		}
+
+		$options['extension'] = 'com_sermonspeaker.series';
+
+		parent::__construct($options);
+	}
+}
+
+/**
+ * SermonSpeaker Component Speakers Category Tree
+ *
+ * @since  6.0.0
+ */
+class SermonspeakerSpeakersCategories extends Categories
+{
+	/**
+	 * Constructor
+	 *
+	 * @param   array $options Obtions
+	 *
+	 * @since 6.0.0
+	 */
+	public function __construct($options = array())
+	{
+		if (!isset($options['table']))
+		{
+			$options['table'] = '#__sermon_speakers';
+		}
+
+		$options['extension'] = 'com_sermonspeaker.speakers';
 
 		parent::__construct($options);
 	}
