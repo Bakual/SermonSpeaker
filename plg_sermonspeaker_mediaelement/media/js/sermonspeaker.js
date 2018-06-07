fefd42396fff6879f770a0ac3e22c921a5684fd6
Thomas Hunziker <werbemails@bakual.ch>
2018-06-07 14:39:48 +0200

    Updating medialementjs to 4.2.9 (playlist 2.5.0)

--- plg_sermonspeaker_mediaelement/media/js/sermonspeaker.js
+++ plg_sermonspeaker_mediaelement/media/js/sermonspeaker.js
@@ -8,6 +8,10 @@ function ss_eventListeners() {
         element.addEventListener('click', ss_playListItem);
     });
     document.getElementById("mediaspace1").addEventListener('play', ss_changeListItem);
+    let toggleplayer = document.getElementById("mediaspace1-other");
+    if (toggleplayer) {
+        toggleplayer.addEventListener('play', ss_changeListItem);
+    }
 }
 
 function ss_play() {
@@ -17,6 +21,9 @@ function ss_playListItem() {
     let id = this.dataset.id;
     let playerid = this.dataset.player;
     let player = document.getElementById(playerid);
+    if (player.lastElementChild.classList.contains('hidden')) {
+        player = document.getElementById(playerid + '-other');
+    }
     let activeplayer = player.lastChild.player;
     activeplayer.currentPlaylistItem = id;
     player.setSrc(activeplayer.playlist[id].src);
@@ -25,7 +32,12 @@ function ss_playListItem() {
 }
 
 function ss_changeListItem(event) {
-    let activeplayer = document.getElementById('mediaspace1').lastChild.player;
+    let activeplayer;
+    if (document.getElementById('mediaspace1').lastElementChild.classList.contains('hidden')) {
+        activeplayer = document.getElementById('mediaspace1-other').lastChild.player;
+    } else {
+        activeplayer = document.getElementById('mediaspace1').lastChild.player;
+    }
     let entry = activeplayer.playlist[activeplayer.currentPlaylistItem];
     for (var i = 0; document.getElementById("sermon" + i); i++) {
         document.getElementById("sermon" + i).classList.remove("ss-current");
@@ -50,4 +62,39 @@ function ss_changeListItem(event) {
     }
     document.getElementById('playing-title').innerHTML = entry.title;
     document.getElementById('playing-desc').innerHTML = entry.description;
+}
+
+function Video() {
+    let player1 = document.getElementById('mediaspace1').lastChild;
+    let player2 = document.getElementById('mediaspace1-other').lastChild;
+    if (player1.nodeName == 'AUDIO') {
+        player1.pause();
+        player1.classList.add('hidden');
+        player1.parentNode.parentNode.parentNode.parentNode.classList.add('hidden');
+        player2.classList.remove('hidden');
+        player2.parentNode.parentNode.parentNode.parentNode.classList.remove('hidden');
+    } else {
+        player1.classList.remove('hidden');
+        player1.parentNode.parentNode.parentNode.parentNode.classList.remove('hidden');
+        player2.pause();
+        player2.classList.add('hidden');
+        player2.parentNode.parentNode.parentNode.parentNode.classList.add('hidden');
+    }
+ }
+function Audio() {
+    let player1 = document.getElementById('mediaspace1').firstElementChild;
+    let player2 = document.getElementById('mediaspace1-other').firstElementChild;
+    if (player1.nodeName == 'VIDEO') {
+        player1.pause();
+        player1.classList.add('hidden');
+        player1.parentNode.parentNode.parentNode.parentNode.classList.add('hidden');
+        player2.classList.remove('hidden');
+        player2.parentNode.parentNode.parentNode.parentNode.classList.remove('hidden');
+    } else {
+        player1.classList.remove('hidden');
+        player1.parentNode.parentNode.parentNode.parentNode.classList.remove('hidden');
+        player2.pause();
+        player2.classList.add('hidden');
+        player2.parentNode.parentNode.parentNode.parentNode.classList.add('hidden');
+    }
 }
