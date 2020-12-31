cda508886371fdda7de6dfe1a0119f8c84e74c02
Thomas Hunziker <werbemails@bakual.ch>
2020-12-31 10:35:20 +0100

    Curly braces are deprecated

--- com_sermonspeaker/admin/controllers/tools.php
+++ com_sermonspeaker/admin/controllers/tools.php
@@ -600,7 +600,7 @@ class SermonspeakerControllerTools extends BaseController
 		}
 
 		// Get the speakers if the teacher is stored as json string.
-		if ($studies[0]->teacher{0} == '{')
+		if ($studies[0]->teacher[0] == '{')
 		{
 			$query = $db->getQuery(true);
 
