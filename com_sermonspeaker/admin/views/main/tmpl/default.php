a38fe2fa25720c38ae013d85a2a5d13c1fbfc39d
Thomas Hunziker <werbemails@bakual.ch>
2021-04-11 21:22:47 +0200

    Better design for cards

--- com_sermonspeaker/admin/views/main/tmpl/default.php
+++ com_sermonspeaker/admin/views/main/tmpl/default.php
@@ -13,82 +13,116 @@ use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
 
 ?>
-<div class="row-fluid">
-	<div id="j-main-container">
-		<div class="row row-cols-1 row-cols-md-3 g-2">
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_sermonspeaker&view=sermons">
-					<div class="card-body">
-						<span class="fas fa-list-alt fa-4x"></span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SERMONS_TITLE'); ?></h3>
+<div class="row">
+	<div class="col-md-12">
+		<div id="j-main-container" class="j-main-container">
+			<div class="row row-cols-1 row-cols-md-3 g-2">
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fas fa-list-alt fa-4x"></span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=sermons">
+								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SERMONS_TITLE'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_sermonspeaker&view=series">
-					<div class="card-body">
-						<span class="fas fa-object-group fa-4x"></span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SERIES_TITLE'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fas fa-object-group fa-4x"></span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=series">
+							<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SERIES_TITLE'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_sermonspeaker&view=speakers">
-					<div class="card-body">
-						<span class="fas fa-comment fa-4x"></span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SPEAKERS_TITLE'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fas fa-comment fa-4x"></span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=speakers">
+								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_SPEAKERS_TITLE'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_categories&extension=com_sermonspeaker.sermons">
-					<div class="card-body">
-						<span class="fa-stack fa-2x">
-							<span class="fas fa-folder-o fa-stack-2x"></span>
-							<span class="fas fa-list-alt fa-stack-1x"></span>
-						</span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SERMONS'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fa-stack fa-2x">
+								<span class="fas fa-folder fa-stack-2x"></span>
+								<span class="fas fa-list-alt fa-stack-1x fa-inverse"></span>
+							</span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_categories&extension=com_sermonspeaker.sermons">
+								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SERMONS'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_categories&extension=com_sermonspeaker.series">
-					<div class="card-body">
-						<span class="fa-stack fa-2x">
-							<span class="fas fa-folder-o fa-stack-2x fa-2x"></span>
-							<span class="fas fa-object-group fa-stack-1x"></span>
-						</span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SERIES'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fa-stack fa-2x">
+								<span class="fas fa-folder fa-stack-2x fa-2x"></span>
+								<span class="fas fa-object-group fa-stack-1x fa-inverse"></span>
+							</span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_categories&extension=com_sermonspeaker.series">
+								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SERIES'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_categories&extension=com_sermonspeaker.speakers">
-					<div class="card-body">
-						<span class="fa-stack fa-2x">
-							<span class="fas fa-folder-o fa-stack-2x"></span>
-							<span class="fas fa-comment fa-stack-1x"></span>
-						</span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SPEAKERS'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fa-stack fa-2x">
+								<span class="fas fa-folder fa-stack-2x"></span>
+								<span class="fas fa-comment fa-stack-1x fa-inverse"></span>
+							</span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_categories&extension=com_sermonspeaker.speakers">
+							<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_CATEGORIES_SPEAKERS'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_sermonspeaker&view=tools">
-					<div class="card-body">
-						<span class="fas fa-wrench fa-4x"></span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MAIN_TOOLS'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fas fa-wrench fa-4x"></span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=tools">
+							<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MAIN_TOOLS'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
-			</div>
-			<div class="col">
-				<a class="card text-center" href="index.php?option=com_sermonspeaker&view=help">
-					<div class="card-body">
-						<span class="fas fa-question-circle fa-4x"></span>
-						<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_HELP'); ?></h3>
+				</div>
+				<div class="col">
+					<div class="card text-center">
+						<div class="card-header">
+							<span class="fas fa-question-circle fa-4x"></span>
+						</div>
+						<div class="card-body">
+							<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=help">
+							<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_MENU_HELP'); ?></h3>
+							</a>
+						</div>
 					</div>
-				</a>
+				</div>
 			</div>
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
--- com_sermonspeaker/admin/views/tools/tmpl/default.php
+++ com_sermonspeaker/admin/views/tools/tmpl/default.php
@@ -23,30 +23,38 @@ $user    = Factory::getUser();
 <form action="<?php echo Route::_('index.php?option=com_sermonspeaker&view=tools'); ?>" method="post" name="adminForm" id="adminForm">
 	<div class="row">
 		<div class="col-md-12">
-			<div id="j-main-container">
+			<div id="j-main-container" class="j-main-container">
 				<div class="row row-cols-1 row-cols-md-3 g-2">
 					<div class="col">
-						<a class="card text-center hasTooltip"
-						   href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
-						   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_ID3_DESC'); ?>"
-						>
-							<div class="card-body">
+						<div class="card text-center">
+							<div class="card-header">
 								<span class="fas fa-download fa-4x"></span>
-								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></h3>
 							</div>
-						</a>
+							<div class="card-body">
+								<a class="stretched-link hasTooltip"
+								   href="index.php?option=com_sermonspeaker&task=tools.write_id3&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
+								   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_ID3_DESC'); ?>"
+								>
+									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_ID3'); ?></h3>
+								</a>
+							</div>
+						</div>
 					</div>
 					<div class="col">
-						<a class="card text-center hasTooltip"
-						   href="#tools-time-modal"
-						   data-toggle="modal"
-						   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_TIME_DESC'); ?>"
-						>
-							<div class="card-body">
+						<div class="card text-center">
+							<div class="card-header">
 								<span class="fas fa-clock fa-4x"></span>
-								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?></h3>
 							</div>
-						</a>
+							<div class="card-body">
+								<a class="stretched-link hasTooltip"
+								   href="#tools-time-modal"
+								   data-toggle="modal"
+								   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_TIME_DESC'); ?>"
+								>
+								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_TIME'); ?></h3>
+								</a>
+							</div>
+						</div>
 						<?php echo JHtml::_(
 							'bootstrap.renderModal',
 							'tools-time-modal',
@@ -57,16 +65,20 @@ $user    = Factory::getUser();
 						); ?>
 					</div>
 					<div class="col">
-						<a class="card text-center hasTooltip"
-						   href="#tools-files-modal"
-						   data-toggle="modal"
-						   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?>"
-						>
-							<div class="card-body">
+						<div class="card text-center">
+							<div class="card-header">
 								<span class="fas fa-binoculars fa-4x"></span>
-								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?></h3>
 							</div>
-						</a>
+							<div class="card-body">
+								<a class="stretched-link hasTooltip"
+								   href="#tools-files-modal"
+								   data-toggle="modal"
+								   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?>"
+								>
+									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_FIND_DESC'); ?></h3>
+								</a>
+							</div>
+						</div>
 						<?php echo JHtml::_(
 							'bootstrap.renderModal',
 							'tools-files-modal',
@@ -79,7 +91,8 @@ $user    = Factory::getUser();
 						); ?>
 						<?php // Check Access
 						if (!$user->authorise('com_sermonspeaker.script', 'com_sermonspeaker')):
-							$link  = 'href="#" onclick="alert(\'' . Text::_('JERROR_ALERTNOAUTHOR') . '\')"';
+							Text::script('JERROR_ALERTNOAUTHOR');
+							$link  = 'href="#" onclick="alert(Joomla.Text._(\'JERROR_ALERTNOAUTHOR\'))"';
 							$class = ' disabled';
 						else:
 							$link  = 'href="index.php?option=com_sermonspeaker&task=tools.createAutomatic"';
@@ -87,56 +100,72 @@ $user    = Factory::getUser();
 						endif; ?>
 					</div>
 					<div class="col">
-						<a class="card text-center hasTooltip<?php echo $class; ?>"
-							<?php echo $link; ?>
-						   data-placement="left"
-						   title="<?php echo Text::sprintf('COM_SERMONSPEAKER_TOOLS_AUTOMATIC_DESC', Uri::root()); ?>"
-						>
-							<div class="card-body">
+						<div class="card text-center">
+							<div class="card-header">
 								<span class="fas fa-cogs fa-4x"></span>
-								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?></h3>
 							</div>
-						</a>
+							<div class="card-body">
+								<a class="stretched-link hasTooltip<?php echo $class; ?>"
+										<?php echo $link; ?>
+								   data-placement="left"
+								   title="<?php echo Text::sprintf('COM_SERMONSPEAKER_TOOLS_AUTOMATIC_DESC', Uri::root()); ?>"
+								>
+									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_AUTOMATIC'); ?></h3>
+								</a>
+							</div>
+						</div>
 					</div>
 					<?php if ($this->pi) : ?>
 						<div class="col">
-							<a class="card text-center hasTooltip"
-							   href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
-							   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_DESC'); ?>"
-							>
-								<div class="card-body">
+							<div class="card text-center">
+								<div class="card-header">
 									<span class="fa-stack fa-2x">
 										<span class="fas fa-file fa-stack-2x"></span>
-										<span class="fas fa-arrow-left fa-stack-1x"></span>
+										<span class="fas fa-arrow-left fa-stack-1x fa-inverse"></span>
 									</span>
-									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?></h3>
 								</div>
-							</a>
+								<div class="card-body">
+									<a class="stretched-link hasTooltip"
+									   href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
+									   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_DESC'); ?>"
+									>
+										<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT'); ?></h3>
+									</a>
+								</div>
+							</div>
 						</div>
 					<?php endif; ?>
 					<?php if ($this->bs) : ?>
 						<div class="col">
-							<a class="card text-center hasTooltip"
-							   href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
-							   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS_DESC'); ?>"
-							>
-								<div class="card-body">
+							<div class="card text-center">
+								<div class="card-header">
 									<span class="fa-stack fa-2x">
 										<span class="fas fa-file fa-stack-2x"></span>
-										<span class="fas fa-arrow-left fa-stack-1x"></span>
+										<span class="fas fa-arrow-left fa-stack-1x fa-inverse"></span>
 									</span>
-									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS'); ?></h3>
 								</div>
-							</a>
+								<div class="card-body">
+									<a class="stretched-link hasTooltip"
+									   href="index.php?option=com_sermonspeaker&task=tools.piimport&<?php echo $session->getName() . '=' . $session->getId() . '&' . Session::getFormToken(); ?>=1"
+									   title="<?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS_DESC'); ?>"
+									>
+									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_TOOLS_IMPORT_BS'); ?></h3>
+									</a>
+								</div>
+							</div>
 						</div>
 					<?php endif; ?>
 					<div class="col">
-						<a class="card text-center" href="index.php?option=com_sermonspeaker&view=statistics&format=raw">
-							<div class="card-body">
+						<div class="card text-center">
+							<div class="card-header">
 								<span class="fas fa-chart-bar fa-4x"></span>
-								<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?></h3>
 							</div>
-						</a>
+							<div class="card-body">
+								<a class="stretched-link" href="index.php?option=com_sermonspeaker&view=statistics&format=raw">
+									<h3 class="card-title"><?php echo Text::_('COM_SERMONSPEAKER_STATISTICS_TITLE'); ?></h3>
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
