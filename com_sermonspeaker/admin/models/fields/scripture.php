26d7c3622a4a502b4cf41194326f1e2f00f93c8b
Thomas Hunziker <werbemails@bakual.ch>
2021-05-09 21:10:37 +0200

    Fixing scripture field

--- com_sermonspeaker/admin/models/fields/scripture.php
+++ com_sermonspeaker/admin/models/fields/scripture.php
@@ -9,6 +9,10 @@
 
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
 jimport('joomla.form.formfield');
 
 /**
@@ -36,9 +40,10 @@ class JFormFieldScripture extends JFormField
 	 */
 	protected function getInput()
 	{
+		$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+
 		// Add the modal field script to the document head.
-		JHtml::_('jquery.framework');
-		JHtml::_('script', 'system/fields/modal-fields.js', array('version' => 'auto', 'relative' => true));
+		$wa->useScript('field.modal-fields');
 
 		// Initialize some field attributes.
 		$size      = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
@@ -46,13 +51,14 @@ class JFormFieldScripture extends JFormField
 		$readonly  = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
 		$disabled  = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 
-		$app        = JFactory::getApplication();
-		$document   = JFactory::getDocument();
-		$javascript = "function delete_scripture(id){
+		$app = Factory::getApplication();
+
+		$wa->addInlineScript("
+		function delete_scripture(id){
 			var child = document.getElementById('scripture_span_'+id)
 			document.getElementById('scripture_span').removeChild(child);
-		}";
-		$document->addScriptDeclaration($javascript);
+		}");
+
 		$admin = $app->isClient('administrator');
 
 		if ($admin)
@@ -80,13 +86,13 @@ class JFormFieldScripture extends JFormField
 				if (!$value['book'])
 				{
 					$class = ' old hasTooltip';
-					$title = ' title="' . JText::_('COM_SERMONSPEAKER_SCRIPTURE_NOT_SEARCHABLE') .'"';
+					$title = ' title="' . Text::_('COM_SERMONSPEAKER_SCRIPTURE_NOT_SEARCHABLE') .'"';
 				}
 			}
 			else
 			{
-				$separator = JText::_('COM_SERMONSPEAKER_SCRIPTURE_SEPARATOR');
-				$text      = JText::_('COM_SERMONSPEAKER_BOOK_' . $value['book']);
+				$separator = Text::_('COM_SERMONSPEAKER_SCRIPTURE_SEPARATOR');
+				$text      = Text::_('COM_SERMONSPEAKER_BOOK_' . $value['book']);
 
 				if ($value['cap1'])
 				{
@@ -122,29 +128,29 @@ class JFormFieldScripture extends JFormField
 						<input id="' . $this->id . '_' . $i . '" type="hidden" value="' . implode('|', $value) . '" name="' . $this->name . '[' . $i . ']">
 						<div class="input-group">
 							<span class="input-group-btn">
-								<button class="btn btn-secondary" onclick="delete_scripture(' . $i . ')"><span class="fas fas-trash"></span></button>
+								<button class="btn btn-secondary" onclick="delete_scripture(' . $i . ')"><span class="fas fa-trash"></span></button>
 							</span>
 							<input id="' . $this->id . '_text_' . $i . '" type="text" class="readonly form-control scripture' . $class . '"' . $title
-							. 'data-toggle="modal" data-target="#' . $modalId . $i . '" '
+							. 'data-bs-toggle="modal" data-bs-target="#' . $modalId . $i . '" '
 							. $size . $disabled . $readonly . $maxLength . ' value="' . $text . '" name="jform[' . $this->fieldname . '_text][' . $i . ']" />
 						</div>';
 
-			$html .= JHtml::_(
+			$html .= HTMLHelper::_(
 				'bootstrap.renderModal',
 				$modalId . $i,
 				array(
-					'title'       => JText::_('COM_SERMONSPEAKER_EDIT_SCRIPTURE'),
+					'title'       => Text::_('COM_SERMONSPEAKER_EDIT_SCRIPTURE'),
 					'url'         => $url . '&id=' . $i,
 					'height'      => '400px',
 					'width'       => '100%',
 					'modalWidth'  => 50,
-					'footer'      => '<a role="button" class="btn btn-secondary" aria-hidden="true"'
+					'footer'      => '<button type="button" class="btn btn-secondary"'
 						. ' onclick="jQuery(\'#' . $modalId . $i . '\').modal(\'hide\'); return false;">'
-						. JText::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</a>'
-						. '<a role="button" class="btn btn-primary" aria-hidden="true"'
+						. Text::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</button>'
+						. '<button type="button" class="btn btn-primary"'
 						. ' onclick="jQuery(\'#' . $modalId . $i . ' .iframe\')[0].contentWindow.AddScripture();'
 						. ' jQuery(\'#' . $modalId . $i . '\').modal(\'hide\'); return false;">'
-						. JText::_('JSAVE') . '</a>',
+						. Text::_('JSAVE') . '</button>',
 				)
 			);
 
@@ -159,31 +165,31 @@ class JFormFieldScripture extends JFormField
 
 		$html .= '<input type="hidden" id="scripture_id" value="' . $i . '" /></span>';
 
-		$html .= '<a href="#' . $modalId . '"
+		$html .= '<button href="#' . $modalId . '"
 						class="btn btn-secondary hasTooltip"
-						title="' . JText::_('COM_SERMONSPEAKER_NEW_SCRIPTURE') . '"
-						data-toggle="modal"
-						role="button"
+						title="' . Text::_('COM_SERMONSPEAKER_NEW_SCRIPTURE') . '"
+						data-bs-toggle="modal"
+						type="button"
 					>
 						<span class="fas fa-plus"></span>
-					</a>';
+					</button>';
 
-		$html .= JHtml::_(
+		$html .= HTMLHelper::_(
 			'bootstrap.renderModal',
 			$modalId,
 			array(
-				'title'       => JText::_('COM_SERMONSPEAKER_NEW_SCRIPTURE'),
+				'title'       => Text::_('COM_SERMONSPEAKER_NEW_SCRIPTURE'),
 				'url'         => $url,
 				'height'      => '400px',
 				'width'       => '100%',
 				'modalWidth'  => 50,
-				'footer'      => '<a role="button" class="btn btn-secondary" aria-hidden="true"'
+				'footer'      => '<button type="button" class="btn btn-secondary"'
 					. ' onclick="jQuery(\'#' . $modalId . '\').modal(\'hide\'); return false;">'
-					. JText::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</a>'
-					. '<a role="button" class="btn btn-primary" aria-hidden="true"'
+					. Text::_('JLIB_HTML_BEHAVIOR_CLOSE') . '</button>'
+					. '<button type="button" class="btn btn-primary"'
 					. ' onclick="jQuery(\'#' . $modalId . ' .iframe\')[0].contentWindow.AddScripture();'
 						. ' jQuery(\'#' . $modalId . '\').modal(\'hide\'); return false;">'
-					. JText::_('JSAVE') . '</a>',
+					. Text::_('JSAVE') . '</button>',
 			)
 		);
 
