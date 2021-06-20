a156087371066a4addd38f631ef38c0fa39bf351
Thomas Hunziker <werbemails@bakual.ch>
2021-06-20 18:18:56 +0200

    Fixing speaker modals (again)

--- com_sermonspeaker/site/layouts/titles/speaker.php
+++ com_sermonspeaker/site/layouts/titles/speaker.php
@@ -20,31 +20,31 @@ use Joomla\CMS\Router\Route;
 extract($displayData);
 ?>
 <?php if (!$item->speaker_state) : ?>
-    <span itemprop="name"><?php echo $item->speaker_title; ?></span>
+	<span itemprop="name"><?php echo $item->speaker_title; ?></span>
 <?php else : ?>
 	<?php if ($params->get('speakerpopup', 1)) : ?>
 		<?php echo HtmlHelper::_(
-			'bootstrap.renderModal',
-			'sermonspeaker-modal-speaker-' . $item->speaker_id,
-			array(
-				'title'  => $item->speaker_title,
-				'footer' => $this->sublayout('modal_footer', $item),
-			),
-			$this->sublayout('modal_body', $item)
-        ); ?>
-        <a href="#sermonspeaker-modal-speaker-<?php echo $item->speaker_id; ?>" data-toggle="modal">
+				'bootstrap.renderModal',
+				'sermonspeaker-modal-speaker-' . $item->speaker_id,
+				array(
+						'title'  => $item->speaker_title,
+						'footer' => $this->sublayout('modal_footer', $item),
+				),
+				$this->sublayout('modal_body', $item)
+		); ?>
+		<a href="#sermonspeaker-modal-speaker-<?php echo $item->speaker_id; ?>" data-bs-toggle="modal">
 	<?php else : ?>
-        <a href="<?php echo Route::_(SermonspeakerHelperRoute::getSpeakerRoute($item->speaker_slug, $item->speaker_catid, $item->speaker_language)); ?>" itemprop="url">
+		<a href="<?php echo Route::_(SermonspeakerHelperRoute::getSpeakerRoute($item->speaker_slug, $item->speaker_catid, $item->speaker_language)); ?>" itemprop="url">
 	<?php endif; ?>
 	<?php if ($item->pic) : ?>
-	    <?php HTMLHelper::_('bootstrap.popover'); ?>
-        <meta itemprop="image" content="<?php echo SermonspeakerHelperSermonspeaker::makeLink($item->pic, true); ?>"/>
-        <span class="hasPopover" data-html="true" data-placement="top" data-title="<?php echo $item->speaker_title; ?>"
-              data-content="<?php echo htmlspecialchars('<img src="' . SermonspeakerHelperSermonspeaker::makeLink($item->pic) . '" />'); ?>">
-            <span itemprop="name"><?php echo $item->speaker_title; ?></span>
-        </span>
-    <?php else : ?>
-        <span itemprop="name"><?php echo $item->speaker_title; ?></span>
-    <?php endif; ?>
-    </a>
+		<?php HTMLHelper::_('bootstrap.popover'); ?>
+		<meta itemprop="image" content="<?php echo SermonspeakerHelperSermonspeaker::makeLink($item->pic, true); ?>"/>
+		<span class="hasPopover" data-bs-html="true" data-bs-placement="top" title="<?php echo $item->speaker_title; ?>"
+			  data-bs-content="<?php echo htmlspecialchars('<img src="' . SermonspeakerHelperSermonspeaker::makeLink($item->pic) . '" />'); ?>">
+			<span itemprop="name"><?php echo $item->speaker_title; ?></span>
+		</span>
+	<?php else : ?>
+		<span itemprop="name"><?php echo $item->speaker_title; ?></span>
+	<?php endif; ?>
+	</a>
 <?php endif;
