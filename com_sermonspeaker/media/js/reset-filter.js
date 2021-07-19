874d23dfe0b1fc9999bc286bfc245720ffb7621f
Thomas Hunziker <werbemails@bakual.ch>
2021-07-19 21:47:49 +0200

    Don't reset order

--- com_sermonspeaker/media/js/reset-filter.js
+++ com_sermonspeaker/media/js/reset-filter.js
@@ -7,42 +7,36 @@
  **/
 (document => {
 
-    const onClick = () => {
-        const form = document.getElementById('adminForm');
-        if (document.getElementById('filter-search')) {
-            document.getElementById('filter-search').value = '';
-        }
-        if (document.getElementById('filter-tag')) {
-            document.getElementById('filter-tag').value = '';
-        }
-        if (document.getElementById('filter-book')) {
-            document.getElementById('filter-book').value = '';
-        }
-        if (document.getElementById('filter-month')) {
-            document.getElementById('filter-month').value = '';
-        }
-        if (document.getElementById('filter-year')) {
-            document.getElementById('filter-year').value = '';
-        }
-        if (document.getElementById('filter_order')) {
-            document.getElementById('filter_order').value = '';
-        }
-        if (document.getElementById('filter_order_Dir')) {
-            document.getElementById('filter_order_Dir').value = '';
-        }
-        form.submit();
-    };
+  const onClick = () => {
+    const form = document.getElementById('adminForm');
+    if (document.getElementById('filter-search')) {
+      document.getElementById('filter-search').value = '';
+    }
+    if (document.getElementById('filter-tag')) {
+      document.getElementById('filter-tag').value = '';
+    }
+    if (document.getElementById('filter-book')) {
+      document.getElementById('filter-book').value = '';
+    }
+    if (document.getElementById('filter-month')) {
+      document.getElementById('filter-month').value = '';
+    }
+    if (document.getElementById('filter-year')) {
+      document.getElementById('filter-year').value = '';
+    }
+    form.submit();
+  };
 
-    const onBoot = () => {
-        const form = document.getElementById('adminForm');
-        const element = form.querySelector('button[type="reset"]');
+  const onBoot = () => {
+    const form = document.getElementById('adminForm');
+    const element = form.querySelector('button[type="reset"]');
 
-        if (element) {
-            element.addEventListener('click', onClick);
-        }
+    if (element) {
+      element.addEventListener('click', onClick);
+    }
 
-        document.removeEventListener('DOMContentLoaded', onBoot);
-    };
+    document.removeEventListener('DOMContentLoaded', onBoot);
+  };
 
-    document.addEventListener('DOMContentLoaded', onBoot);
+  document.addEventListener('DOMContentLoaded', onBoot);
 })(document);
