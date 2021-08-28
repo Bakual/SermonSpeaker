a716583040477b162ad2db34050e2ed8d7fd7d28
Thomas Hunziker <werbemails@bakual.ch>
2021-08-28 22:06:21 +0200

    editor plugin J4 ready

--- plg_editors_xtd_sermonspeaker/sermonspeaker.php
+++ plg_editors_xtd_sermonspeaker/sermonspeaker.php
@@ -10,15 +10,17 @@
 defined('_JEXEC') or die();
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Session\Session;
 
 /**
  * Editorbutton for the SermonSpeaker content plugin
  *
  * @since  1.0
  */
-class PlgButtonSermonspeaker extends JPlugin
+class PlgButtonSermonspeaker extends CMSPlugin
 {
 	/**
 	 * Load the language file on instantiation.
@@ -33,7 +35,9 @@ class PlgButtonSermonspeaker extends JPlugin
 	 *
 	 * @param   string  $name  Name of the element
 	 *
-	 * @return  $button  The HTML for the button
+	 * @return  string  The HTML for the button
+	 *
+	 * @since ?
 	 */
 	public function onDisplay($name)
 	{
@@ -44,25 +48,31 @@ class PlgButtonSermonspeaker extends JPlugin
 			} else {
 				var tag = '{sermonspeaker '+id+'}';
 			}
-			jInsertEditorText(tag, '" . $name . "');
-			SqueezeBox.close();
+			window.parent.Joomla.editors.instances['" . $name . "'].replaceSelection(tag);
+			if (window.parent.Joomla.Modal) {
+				window.parent.Joomla.Modal.getCurrent().close();
+			}
 		}";
 
 		$doc = Factory::getDocument();
 		$doc->addScriptDeclaration($js);
 
-		HTMLHelper::_('behavior.modal');
-
 		// Use the built-in element view to select the sermon.
-		$link = 'index.php?option=com_sermonspeaker&amp;view=sermons&amp;layout=modal&amp;tmpl=component&amp;' . JSession::getFormToken() . '=1';
+		$link = 'index.php?option=com_sermonspeaker&amp;view=sermons&amp;layout=modal&amp;tmpl=component&amp;'
+			. Session::getFormToken() . '=1&amp;editor=' . $name;
 
-		$button = new JObject;
+		$button          = new CMSObject;
 		$button->modal   = true;
-		$button->class   = 'btn';
 		$button->link    = $link;
 		$button->text    = Text::_('PLG_EDITORS-XTD_SERMONSPEAKER_BUTTON_SERMONSPEAKER');
-		$button->name    = 'broadcast';
-		$button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+		$button->name    = $this->_type . '_' . $this->_name;
+		$button->icon    = 'comment';
+		$button->options = [
+			'height'     => '300px',
+			'width'      => '800px',
+			'bodyHeight' => '70',
+			'modalWidth' => '80',
+		];
 
 		return $button;
 	}
