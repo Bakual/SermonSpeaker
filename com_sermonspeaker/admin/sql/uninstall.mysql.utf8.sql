40139f1becf460519329ef57ed4887fc086155fb
Thomas Hunziker <werbemails@bakual.ch>
2021-09-06 21:29:24 +0200

    Optimise Uninstall SQL

--- com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
+++ com_sermonspeaker/admin/sql/uninstall.mysql.utf8.sql
@@ -4,52 +4,19 @@ DROP TABLE IF EXISTS `#__sermon_sermons`;
 DROP TABLE IF EXISTS `#__sermon_scriptures`;
 DELETE
 FROM `#__ucm_content`
-WHERE `core_type_alias` = 'com_sermonspeaker.sermon';
-DELETE
-FROM `#__ucm_content`
-WHERE `core_type_alias` = 'com_sermonspeaker.speaker';
-DELETE
-FROM `#__ucm_content`
-WHERE `core_type_alias` = 'com_sermonspeaker.serie';
+WHERE `core_type_alias` LIKE 'com_sermonspeaker.%';
 DELETE
 FROM `#__contentitem_tag_map`
-WHERE `type_alias` = 'com_sermonspeaker.sermon';
-DELETE
-FROM `#__contentitem_tag_map`
-WHERE `type_alias` = 'com_sermonspeaker.speaker';
-DELETE
-FROM `#__contentitem_tag_map`
-WHERE `type_alias` = 'com_sermonspeaker.serie';
+WHERE `type_alias` = 'com_sermonspeaker.%';
 DELETE a
 FROM `#__ucm_base` AS a
          INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.sermon';
-DELETE a
-FROM `#__ucm_base` AS a
-         INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.speaker';
-DELETE a
-FROM `#__ucm_base` AS a
-         INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.serie';
-DELETE a
-FROM `#__ucm_history` AS a
-         INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.sermon';
-DELETE a
-FROM `#__ucm_history` AS a
-         INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.speaker';
-DELETE a
-FROM `#__ucm_history` AS a
-         INNER JOIN `#__content_types` AS b ON a.ucm_type_id = b.type_id
-WHERE b.type_alias = 'com_sermonspeaker.serie';
+WHERE b.type_alias LIKE 'com_sermonspeaker.%';
+DELETE FROM `#__history`
+WHERE `item_id` LIKE 'com_sermonspeaker.%';
 DELETE
-FROM `#__content_types`
-WHERE `type_alias` = 'com_sermonspeaker.sermon';
+FROM `#__categories`
+WHERE `extension` LIKE 'com_sermonspeaker.%';
 DELETE
-FROM `#__content_types`
-WHERE `type_alias` = 'com_sermonspeaker.speaker';
-DELETE
-FROM `#__content_types`
-WHERE `type_alias` = 'com_sermonspeaker.serie';
+FROM `#__assets`
+WHERE `name` LIKE 'com_sermonspeaker.%';
