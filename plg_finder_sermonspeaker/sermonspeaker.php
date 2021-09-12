9acdf4ae2bd210f6bafa46ff56a2944ce8d18e68
Thomas Hunziker <werbemails@bakual.ch>
2021-09-12 14:50:41 +0200

    Finder Plugin can now index series and speakers as well

--- plg_finder_sermonspeaker/sermonspeaker.php
+++ plg_finder_sermonspeaker/sermonspeaker.php
@@ -14,6 +14,7 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Table\Table;
 use Joomla\Component\Finder\Administrator\Indexer\Adapter;
 use Joomla\Component\Finder\Administrator\Indexer\Helper;
+use Joomla\Component\Finder\Administrator\Indexer\Indexer;
 use Joomla\Component\Finder\Administrator\Indexer\Result;
 use Joomla\Database\DatabaseQuery;
 use Joomla\Registry\Registry;
@@ -59,7 +60,7 @@ class PlgFinderSermonspeaker extends Adapter
 	 *
 	 * @since ?
 	 */
-	protected $type_title = 'Sermon';
+	protected $type_title = 'SermonSpeaker';
 
 	/**
 	 * The table name.
@@ -85,6 +86,33 @@ class PlgFinderSermonspeaker extends Adapter
 	 * @since  6.0.1
 	 */
 	protected $autoloadLanguage = true;
+
+	/**
+	 * valid SermonSpeaker Contexts
+	 *
+	 * @var    array
+	 * @since  6.0.1
+	 */
+	private $validContexts = [
+		'com_sermonspeaker.sermon',
+		'com_sermonspeaker.frontendupload',
+		'com_sermonspeaker.serie',
+		'com_sermonspeaker.serieform',
+		'com_sermonspeaker.speaker',
+		'com_sermonspeaker.speakerform',
+	];
+
+	/**
+	 * SermonSpeaker Types
+	 *
+	 * @var    array
+	 * @since  6.0.1
+	 */
+	private $sermonspeakerTypes = [
+		'Sermons'  => 'sermon',
+		'Series'   => 'serie',
+		'Speakers' => 'speaker',
+	];
 
 	/**
 	 * Method to update the item link information when the item category is
@@ -154,10 +182,10 @@ class PlgFinderSermonspeaker extends Adapter
 	public function onFinderAfterSave($context, $row, $isNew): void
 	{
 		// We only want to handle sermons here. We need to handle front end and back end editing.
-		if ($context == 'com_sermonspeaker.sermon' || $context == 'com_sermonspeaker.frontendupload')
+		if (in_array($context, $this->validContexts))
 		{
 			// Check if the access levels are different.
-			if (!$isNew && $this->old_catid != $row->catid)
+			if (!$isNew && $this->old_category != $row->catid)
 			{
 				// Process the change.
 				$this->itemAccessChange($row);
@@ -186,7 +214,7 @@ class PlgFinderSermonspeaker extends Adapter
 	 * to queue the item to be indexed later.
 	 *
 	 * @param   string   $context  The context of the content passed to the plugin.
-	 * @param   Table   $row      A Table object
+	 * @param   Table    $row      A Table object
 	 * @param   boolean  $isNew    If the content is just about to be created
 	 *
 	 * @return  boolean  True on success.
@@ -197,8 +225,10 @@ class PlgFinderSermonspeaker extends Adapter
 	public function onFinderBeforeSave($context, $row, $isNew)
 	{
 		// Check for access levels from the category
-		if ($context == 'com_sermonspeaker.sermon' || $context == 'com_sermonspeaker.frontendupload' )
+		if (in_array($context, $this->validContexts))
 		{
+			$this->adjustType($context);
+
 			// Query the database for the old access level if the item isn't new.
 			if (!$isNew)
 			{
@@ -217,6 +247,37 @@ class PlgFinderSermonspeaker extends Adapter
 		}
 
 		return true;
+	}
+
+	/**
+	 * @param   string  $context
+	 *
+	 *
+	 * @since version
+	 */
+	private function adjustType(string $context): void
+	{
+		// Adjust to context
+		if ($context == 'com_sermonspeaker.sermon' || $context == 'com_sermonspeaker.frontendupload')
+		{
+			$this->layout     = 'sermon';
+			$this->table      = '#__sermon_sermons';
+			$this->type_title = 'Sermons';
+		}
+
+		if ($context == 'com_sermonspeaker.serie' || $context == 'com_sermonspeaker.serieform')
+		{
+			$this->layout     = 'serie';
+			$this->table      = '#__sermon_series';
+			$this->type_title = 'Series';
+		}
+
+		if ($context == 'com_sermonspeaker.speaker' || $context == 'com_sermonspeaker.speakerform')
+		{
+			$this->layout     = 'speaker';
+			$this->table      = '#__sermon_speakers';
+			$this->type_title = 'Speakers';
+		}
 	}
 
 	/**
@@ -254,9 +315,11 @@ class PlgFinderSermonspeaker extends Adapter
 	 */
 	public function onFinderChangeState($context, $pks, $value)
 	{
-		// We only want to handle sermons here
-		if ($context == 'com_sermonspeaker.sermon' || $context == 'com_sermonspeaker.frontendupload')
+		// We only want to handle SermonSpeaker items here
+		if (in_array($context, $this->validContexts))
 		{
+			$this->adjustType($context);
+
 			$this->itemStateChange($pks, $value);
 		}
 
@@ -268,9 +331,143 @@ class PlgFinderSermonspeaker extends Adapter
 	}
 
 	/**
+	 * Method to get the adapter state and push it into the indexer.
+	 *
+	 * @return  void
+	 *
+	 * @throws  Exception on error.
+	 * @since   2.5
+	 */
+	public function onStartIndex()
+	{
+		// Get the indexer state.
+		$iState = Indexer::getState();
+
+		foreach ($this->sermonspeakerTypes as $title => $type)
+		{
+			$this->adjustType('com_sermonspeaker.' . $type);
+
+			// Get the number of content items.
+			$total = (int) $this->getContentCount();
+
+			// Add the content count to the total number of items.
+			$iState->totalItems += $total;
+
+			// Populate the indexer state information for the adapter.
+			$iState->pluginState[$this->context . '.' . $title]['total']  = $total;
+			$iState->pluginState[$this->context . '.' . $title]['offset'] = 0;
+		}
+
+		// Set the indexer state.
+		Indexer::setState($iState);
+	}
+
+	/**
+	 * Method to prepare for the indexer to be run. This method will often
+	 * be used to include dependencies and things of that nature.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @throws  Exception on error.
+	 * @since   2.5
+	 */
+	public function onBeforeIndex()
+	{
+		// Get the indexer and adapter state.
+		$iState = Indexer::getState();
+		$count  = 0;
+
+		foreach ($this->sermonspeakerTypes as $title => $type)
+		{
+			$aState = $iState->pluginState[$this->context . '.' . $title];
+
+			// Check the progress of the indexer and the adapter.
+			if ($iState->batchOffset == $iState->batchSize || $aState['offset'] == $aState['total'])
+			{
+				$count++;
+			}
+		}
+
+		if ($count === 3)
+		{
+			return true;
+		}
+
+		// Run the setup method.
+		return $this->setup();
+	}
+
+	/**
+	 * Method to setup the indexer to be run.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since ?
+	 */
+	protected function setup()
+	{
+		return true;
+	}
+
+	/**
+	 * Method to index a batch of content items. This method can be called by
+	 * the indexer many times throughout the indexing process depending on how
+	 * much content is available for indexing. It is important to track the
+	 * progress correctly so we can display it to the user.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @throws  Exception on error.
+	 * @since   2.5
+	 */
+	public function onBuildIndex()
+	{
+		// Get the indexer and adapter state.
+		$iState = Indexer::getState();
+
+		foreach ($this->sermonspeakerTypes as $title => $type)
+		{
+			$aState = $iState->pluginState[$this->context . '.' . $title];
+
+			// Check the progress of the indexer and the adapter.
+			if ($iState->batchOffset == $iState->batchSize || $aState['offset'] == $aState['total'])
+			{
+				continue;
+			}
+
+			// Get the batch offset and size.
+			$offset = (int) $aState['offset'];
+			$limit  = (int) ($iState->batchSize - $iState->batchOffset);
+
+			// Get the content items to index.
+			$this->adjustType('com_sermonspeaker.' . $type);
+			$items = $this->getItems($offset, $limit);
+
+			// Iterate through the items and index them.
+			for ($i = 0, $n = count($items); $i < $n; $i++)
+			{
+				// Index the item.
+				$this->index($items[$i]);
+
+				// Adjust the offsets.
+				$offset++;
+				$iState->batchOffset++;
+				$iState->totalItems--;
+			}
+
+			// Update the indexer state.
+			$aState['offset']                                   = $offset;
+			$iState->pluginState[$this->context . '.' . $title] = $aState;
+			Indexer::setState($iState);
+		}
+
+		return true;
+	}
+
+	/**
 	 * Method to index an item. The item must be a FinderIndexerResult object.
 	 *
-	 * @param   FinderIndexerResult  $item    The item to index as an FinderIndexerResult object.
+	 * @param   FinderIndexerResult  $item  The item to index as an FinderIndexerResult object.
 	 *
 	 * @return  void
 	 *
@@ -285,7 +482,10 @@ class PlgFinderSermonspeaker extends Adapter
 			return;
 		}
 
-		$item->context = 'com_sermonspeaker.sermon';
+		// Load dependent classes.
+		require_once JPATH_SITE . '/components/com_sermonspeaker/helpers/route.php';
+
+		$item->context = 'com_sermonspeaker.' . $this->layout;
 
 		$item->metadata = new Registry($item->metadata);
 
@@ -296,18 +496,25 @@ class PlgFinderSermonspeaker extends Adapter
 		$item->url = $this->getURL($item->id, $this->extension, $this->layout);
 
 		// Build the necessary route and path information.
-		$item->route = SermonspeakerHelperRoute::getSermonRoute($item->slug, $item->catid, $item->language);
+		$method      = 'get' . ucfirst($this->layout) . 'Route';
+		$item->route = SermonspeakerHelperRoute::$method($item->slug, $item->catid, $item->language);
 
 		// Add the metadata processing instructions.
 		$item->addInstruction(FinderIndexer::META_CONTEXT, 'metakey');
 		$item->addInstruction(FinderIndexer::META_CONTEXT, 'metadesc');
 
 		// Add the type taxonomy data.
-		$item->addTaxonomy('Type', 'Sermon');
+		$item->addTaxonomy('Type', $this->type_title);
+
+		// Add the author taxonomy data.
+		if (!empty($item->author))
+		{
+			$item->addTaxonomy('Author', $item->author, $item->state);
+		}
 
 		// Add the category taxonomy data.
-		$categories = Categories::getInstance('com_sermonspeaker.sermons', ['published' => false, 'access' => false]);
-		$category = $categories->get($item->catid);
+		$categories = Categories::getInstance('com_sermonspeaker.' . $this->layout . 's', ['published' => false, 'access' => false]);
+		$category   = $categories->get($item->catid);
 		$item->addNestedTaxonomy('Category', $category, $category->published, $category->access, $category->language);
 
 		// Add the language taxonomy data.
@@ -318,21 +525,6 @@ class PlgFinderSermonspeaker extends Adapter
 
 		// Index the item.
 		$this->indexer->index($item);
-	}
-
-	/**
-	 * Method to setup the indexer to be run.
-	 *
-	 * @return  boolean  True on success.
-	 *
-	 * @since ?
-	 */
-	protected function setup()
-	{
-		// Load dependent classes.
-		require_once JPATH_SITE . '/components/com_sermonspeaker/helpers/route.php';
-
-		return true;
 	}
 
 	/**
@@ -378,12 +570,24 @@ class PlgFinderSermonspeaker extends Adapter
 
 		// Check if we can use the supplied SQL query.
 		$query = $query instanceof DatabaseQuery ? $query : $this->db->getQuery(true)
-			->select('a.id, a.title, a.alias, a.notes AS summary')
-			->select('a.picture')
+			->select('a.id, a.title, a.alias')
 			->select('a.state, a.catid, a.created AS start_date, a.created_by')
 			->select('a.metakey, a.metadesc, ' . (int) $access . ' AS access, a.version, a.ordering')
 			->select('a.publish_up AS publish_start_date, a.publish_down AS publish_end_date')
 			->select('c.title AS category, c.published AS cat_state, c.access AS cat_access');
+
+		switch ($this->layout)
+		{
+			case 'sermon':
+				$query->select('a.picture AS image, a.notes AS summary');
+				break;
+			case 'serie':
+				$query->select('a.avatar AS image, a.series_description AS summary');
+				break;
+			case 'speaker':
+				$query->select('a.pic AS image, a.intro AS summary, a.bio AS body');
+				break;
+		}
 
 		// Handle the alias CASE WHEN portion of the query
 		$case_when_item_alias = ' CASE WHEN ';
@@ -405,7 +609,7 @@ class PlgFinderSermonspeaker extends Adapter
 		$query->select($case_when_category_alias);
 
 		$query->select('u.name AS author')
-			->from('#__sermon_sermons AS a')
+			->from($this->db->quoteName($this->table) . ' AS a')
 			->join('LEFT', '#__categories AS c ON c.id = a.catid')
 			->join('LEFT', '#__users AS u ON u.id = a.created_by');
 
