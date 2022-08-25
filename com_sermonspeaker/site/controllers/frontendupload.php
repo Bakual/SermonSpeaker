9761bcd4f15726ca5929a526c2593199ad98339a
Thomas Hunziker <werbemails@bakual.ch>
2022-08-25 14:23:50 +0200

    Don't require autoloaded composer files (wrong path anyway)

--- com_sermonspeaker/site/controllers/frontendupload.php
+++ com_sermonspeaker/site/controllers/frontendupload.php
@@ -367,10 +367,8 @@ class SermonspeakerControllerFrontendupload extends JControllerForm
 		{
 			$files[] = $item->audiofile;
 			$files[] = $item->videofile;
-			require_once JPATH_COMPONENT_SITE . '/id3/getid3/getid3.php';
 			$getID3 = new getID3;
 			$getID3->setOption(array('encoding' => 'UTF-8'));
-			require_once JPATH_COMPONENT_SITE . '/id3/getid3/write.php';
 			$writer             = new getid3_writetags;
 			$writer->tagformats = array('id3v2.3');
 
