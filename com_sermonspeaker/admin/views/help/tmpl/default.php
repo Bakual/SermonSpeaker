79755f6cda4be878074af90d4882ba982e0c9a6c
Thomas Hunziker <github@bakual.ch>
2023-06-05 10:09:57 +0200

    Remove no longer used references

--- com_sermonspeaker/admin/views/help/tmpl/default.php
+++ com_sermonspeaker/admin/views/help/tmpl/default.php
@@ -32,40 +32,18 @@ use Joomla\CMS\Router\Route;
 				<div class="card-text">
 					SermonSpeaker is released under the <a href="http://www.gnu.org/licenses/gpl.html">GNU/GPL
 						license</a><br>
-					Please note that the included flash players have their own licenses:
-					<ul>
-						<li><a href="http://www.longtailvideo.com/players/">JW Player</a> from <a
-									href="http://www.longtailvideo.com/">LongTail Video</a> is licensed under <a
-									href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons
-								Attribution-NonCommercial-ShareAlike</a> and thus free for non-commercial use.
-							Commercial licenses are available.
-						</li>
-						<li><a href="http://flowplayer.org/">FlowPlayer</a> is licensed under <a
-									href="http://flowplayer.org/download/license_gpl.htm">GPL</a> and thus free for use.
-							The copyright logo can be removed with a commercial license.
-						</li>
-						<li><a href="http://wpaudioplayer.com/">WordPress Audio Player</a> from <a
-									href="http://www.1pixelout.net/">1 Pixel Out</a> is licensed under <a
-									href="http://wpaudioplayer.com/license/">MIT</a> and thus free for use. When using
-							keep in mind that it only supports mp3 audiofiles.
-						</li>
-					</ul>
 				</div>
 			</div>
 		</div>
 		<div class="card m-3">
 			<div class="card-body">
 				<h2 class="card-title">Classes</h2>
-				<div class="card-text">SermonSpeaker includes the following extern classes besides the players mentioned
-					above:
+				<div class="card-text">SermonSpeaker includes the following extern classes:
 					<ul>
 						<li><a href="http://http://www.plupload.com/">Plupload</a> for the Uploader.</li>
 						<li><a href="http://getid3.sourceforge.net">GetID3</a> for the ID3 tags lookup.</li>
-						<li><a href="http://undesigned.org.za/2007/10/22/amazon-s3-php-class">Amazon S3 PHP Class</a>
+						<li><a href="https://aws.amazon.com/de/sdk-for-php/">Amazon AWS SDK for PHP</a>
 							for the Amazon S3 support.
-						</li>
-						<li><a href="https://github.com/eduardocereto/GA-Code-Samples">Google Analytics Code Samples</a>
-							from <a href="http://www.cardinalpath.com">Cardinal Path</a> for the Vimeo GA support.
 						</li>
 					</ul>
 				</div>
