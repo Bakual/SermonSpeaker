3e8ccc45adb4fdab31aeab8eb32bba4a56c246fb
Thomas Hunziker <github@bakual.ch>
2025-11-04 14:39:30 +0100

    Declare the variables

--- mod_related_sermons/tmpl/default.php
+++ mod_related_sermons/tmpl/default.php
@@ -11,15 +11,22 @@ defined('_JEXEC') or die();
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+
+/**
+ * @var array                     $list
+ * @var \Joomla\Registry\Registry $params
+ * @var stdClass                  $module
+ * @var bool                      $showDate
+ */
 ?>
 <ul class="mod-relateditems relateditems mod-list">
-<?php foreach ($list as $item) : ?>
-<li>
-	<a href="<?php echo $item->route; ?>">
-		<?php if ($showDate) : ?>
-			<?php echo HTMLHelper::_('date', $item->created, Text::_('DATE_FORMAT_LC4')) . ' - '; ?>
-		<?php endif; ?>
-		<?php echo $item->title; ?></a>
-</li>
-<?php endforeach; ?>
+	<?php foreach ($list as $item) : ?>
+		<li>
+			<a href="<?php echo $item->route; ?>">
+				<?php if ($showDate) : ?>
+					<?php echo HTMLHelper::_('date', $item->created, Text::_('DATE_FORMAT_LC4')) . ' - '; ?>
+				<?php endif; ?>
+				<?php echo $item->title; ?></a>
+		</li>
+	<?php endforeach; ?>
 </ul>
