23b9ea51faf263821b4c82c47818a8cc4512a87a
Thomas Hunziker <github@bakual.ch>
2025-11-05 15:21:39 +0100

    Codestyle

--- mod_sermonarchive/services/provider.php
+++ mod_sermonarchive/services/provider.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     SermonSpeaker
- * @subpackage  Module.LatestSermons
+ * @subpackage  Module.Sermonarchive
  * @author      Thomas Hunziker <admin@sermonspeaker.net>
  * @copyright   © 2022 - Thomas Hunziker
  * @license     http://www.gnu.org/licenses/gpl.html
