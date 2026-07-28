c768bc8e08f72960c48dd0620e4487ee595cb2b2
Thomas Hunziker <github@bakual.ch>
2026-07-28 16:11:38 +0200

    Working on using an Ajax ednpoint instead of component for the upload Using the media manager API model for the upload to support all adapters

--- mod_sermonupload/Helper/SermonuploadHelper.php
+++ mod_sermonupload/Helper/SermonuploadHelper.php
@@ -9,12 +9,18 @@
 
 namespace Sermonspeaker\Module\Sermonupload\Site\Helper;
 
+use Exception;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Filter\OutputFilter;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Session\Session;
 use Joomla\CMS\Uri\Uri;
+use Joomla\Component\Media\Administrator\Exception\FileExistsException;
+use Joomla\Component\Media\Administrator\Model\ApiModel;
 use Joomla\Database\DatabaseAwareInterface;
 use Joomla\Database\DatabaseAwareTrait;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Path;
 
 defined('_JEXEC') or die();
 
@@ -26,6 +32,180 @@ defined('_JEXEC') or die();
 class SermonuploadHelper implements DatabaseAwareInterface
 {
 	use DatabaseAwareTrait;
+
+	/**
+	 * Get Losung from XML
+	 *
+	 * @return  void
+	 *
+	 * @throws \Exception
+	 * @since   1.0
+	 */
+	public static function fileUploadAjax(): void
+	{
+		// Check for request forgeries
+		if (!Session::checkToken('request'))
+		{
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('JINVALID_TOKEN'),
+			);
+			echo json_encode($response);
+
+			return;
+		}
+
+		// Authorize User
+		$user = Factory::getApplication()->getIdentity();
+
+		if (!$user->authorise('core.create', 'com_sermonspeaker'))
+		{
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('JGLOBAL_AUTH_ACCESS_DENIED'),
+			);
+			echo json_encode($response);
+
+			return;
+		}
+
+		// Initialise variables.
+		$app    = Factory::getApplication();
+		$cparams = $app->getParams('com_sermonspeaker');
+		$jinput = $app->input;
+
+		// Get some data from the request
+		$file = $jinput->files->get('file');
+		$type = $jinput->get('type', 'audio', 'word');
+		$type = (in_array($type, array('audio', 'video', 'addfile'))) ? $type : 'audio';
+
+		if (!$file['name'])
+		{
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('COM_SERMONSPEAKER_FU_FAILED'),
+			);
+			echo json_encode($response);
+
+			return;
+		}
+
+		// Get file extension
+		$ext = File::getExt($file['name']);
+
+		// Optionally sanitising filenames
+		if ($cparams->get('sanitise_filename', 1))
+		{
+			// Make filename URL safe. Eg replaces ä with ae.
+			$file['name'] = OutputFilter::stringURLSafe(File::stripExt($file['name'])) . '.' . $ext;
+
+			// Make the filename safe
+			$file['name'] = File::makeSafe($file['name']);
+
+			// Replace spaces in filename as long as makeSafe doesn't do this.
+			$file['name'] = str_replace(' ', '_', $file['name']);
+
+			// Check if filename has more chars than only dashes, making a new filename based on current date/time if not
+			if (count_chars(File::stripExt($file['name']), 3) == '-')
+			{
+				$file['name'] = Factory::getDate()->format("Y-m-d-H-i-s") . '.' . $ext;
+			}
+		}
+
+		// Check for file extension
+		$types = strtolower($cparams->get($type . '_filetypes'));
+		$types = array_map('trim', explode(',', $types));
+
+		if (!in_array(strtolower($ext), $types))
+		{
+			$response = array(
+				'status' => '0',
+				'error'  => Text::sprintf('COM_SERMONSPEAKER_FILETYPE_NOT_ALLOWED', $ext),
+			);
+			echo json_encode($response);
+
+			return;
+		}
+
+		$path   = $cparams->get('path_' . $type, 'local-images:/');
+		$path   = trim($path, '/');
+		$date   = $jinput->get('date', '', 'string');
+		$time   = ($date) ? strtotime($date) : time();
+		$append = ($cparams->get('append_path_user', 0)) ? '/' . Factory::getApplication()->getIdentity()->id : '';
+		$append .= ($cparams->get('append_path', 0)) ? '/' . date('Y', $time) . '/' . date('m', $time) : '';
+
+		if ($cparams->get('append_path_lang', 0))
+		{
+			$lang = $jinput->get('language');
+
+			if (!$lang || $lang == '*')
+			{
+				$jlang = Factory::getApplication()->getLanguage();
+				$lang  = $jlang->getTag();
+			}
+
+			$append .= '/' . $lang;
+		}
+
+		$folder = JPATH_ROOT . '/' . $path . $append;
+
+		$filename = $file['name'];
+
+		if ($cparams->get('sanitise_filename', 1))
+		{
+			$filename = strtolower($filename);
+		}
+
+		$filepath         = Path::clean($folder . '/' . $filename);
+		$file['filepath'] = $filepath;
+
+		// TODO: Check with mediamanager
+		if (file_exists($filepath))
+		{
+			// File exists
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('COM_SERMONSPEAKER_FU_ERROR_EXISTS'),
+			);
+			echo json_encode($response);
+
+			return;
+		}
+
+		$pathinfo = explode(':/', $path);
+
+		$mediaManager = new ApiModel();
+
+		try
+		{
+			$result = $mediaManager->createFile($pathinfo[0], $filename, $pathinfo[1], $file['tmp_name'],false);
+
+			$response = array(
+				'status'   => '1',
+				'filename' => $filename,
+				'path'     => str_replace('\\', '/', '/' . $path . $append . '/' . $filename),
+				'error'    => Text::sprintf('COM_SERMONSPEAKER_FU_FILENAME', substr($file['filepath'], strlen(JPATH_ROOT))),
+			);
+		}
+		catch (FileExistsException $e)
+		{
+			// Error in upload
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('COM_SERMONSPEAKER_FU_ERROR_EXISTS'),
+			);
+		}
+		catch (Exception $e)
+		{
+			// Error in upload
+			$response = array(
+				'status' => '0',
+				'error'  => Text::_('COM_SERMONSPEAKER_FU_ERROR_UNABLE_TO_UPLOAD_FILE'),
+			);
+		}
+
+		echo json_encode($response);
+	}
 
 	/**
 	 * Loads JavaScript for the uploader into Document Header
@@ -43,6 +223,8 @@ class SermonuploadHelper implements DatabaseAwareInterface
 		$identifier = $identifier . $type;
 		$uploadURL  = Uri::base() . 'index.php?option=com_sermonspeaker&task=file.upload&'
 			. Session::getFormToken() . '=1&format=json';
+		$uploadURL  = Uri::base() . 'index.php?option=com_ajax&module=sermonupload&method=fileUpload&'
+			. Session::getFormToken() . '=1&format=json&mediatypes=1';
 
 		$plupload_script = '
 			jQuery(document).ready(function() {
